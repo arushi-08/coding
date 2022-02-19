c4d60a6aa22c2361d8c872097fd8c401ba736c30
Arushi Sharma <arushi.net@outlook.com>
2022-02-19 10:21:29 +0000

    [LeetCode Sync] Runtime - 36 ms (21.68%), Memory - 13.8 MB (100.00%)

--- my-folder/0069-sqrtx/solution.py
+++ my-folder/0069-sqrtx/solution.py
@@ -1,24 +1,22 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        ans = 0
+        
         if x == 1:
             return 1
+        
         left = 1
         right = x
-        while(left <= right):
-            mid = (right + left) // 2
-            # print(mid)
-            if mid*mid <= x and (mid+1)*(mid+1)>x:
+        
+        while(left<=right):
+            mid = (left + right) // 2
+            
+            if mid*mid <= x and (mid+1)*(mid+1) > x:
                 return mid
+            
             if mid*mid < x:
                 left = mid + 1
+            
             else:
-                # print("enter")
-                right = mid - 1
+                right = mid -1
         
         return 0
-        
-#         for i in range(1, x):
-#             if i*i <= x:
-#                 ans = i
-#         return ans
