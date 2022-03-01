07a9c874fc3002c2f3b5c3b6424e87adf6d9df77
Arushi Sharma <arushi.net@outlook.com>
2022-03-01 12:05:00 +0000

    [LeetCode Sync] Runtime - 44 ms (5.27%), Memory - 14.5 MB (100.00%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,48 +1,59 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left , right = 0, len(nums) - 1
-        pivot = -1
-        if not len(nums):
-            return -1
-        if len(nums) == 1:
-            if nums[0] != target:
-                return -1
+            
+    def search_array(self, nums, target):
+        
+        left = 0
+        right = len(nums) - 1
+        print(nums[0])
+        if nums[0] == target:
+            return 0
+        
+        while(left <= right):
+            
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] > target:
+                right = mid - 1
             else:
+                left = mid + 1
+        return -1
+    
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+        pivot = -1
+        if len(nums) == 1:
+            if nums[0] == target:
                 return 0
+            return -1
         
         while(left <= right):
             mid = (left + right) // 2
-            if nums[mid] == target:
-                # print('ans', mid)
-                return mid
-            if mid + 1 <= right and nums[mid] > nums[mid+1]:
+            # print(mid, nums[mid] , nums[mid+1] )
+            if mid + 1 < len(nums) and nums[mid] > nums[mid+1]:
                 pivot = mid
                 break
             else:
-                # 
-                right = mid - 1
+                left = mid + 1
                 if left > right and pivot == -1:
-                    right = len(nums) - 1
-                    left = mid + 1
-        
+                    left = 0
+                    right = mid - 1
+        print("pivot", pivot)
         if pivot > -1:
-            print(pivot)
-            count = 0
-            for arr in [nums[:pivot+1], nums[pivot+1:]]:
-                if not count:
-                    left, right = 0, pivot
-                else:
-                    left, right = pivot+1, len(nums)-1
-                while(left <= right):
-                    mid = (left + right) // 2
-                    if nums[mid] == target:
-                        return mid 
-                    if nums[mid] < target:
-                        left = mid + 1
-                    else:
-                        right = mid - 1
-                count += 1
-            return -1
-            
+            nums2 = nums[:pivot+1]
+            nums1 = nums[pivot+1:]
+            if nums2[0] > target:
+                ans = self.search_array(nums1, target)
+                if ans >= 0:
+                    return ans + pivot + 1
+                return ans
+            else:
+                ans = self.search_array(nums2, target)
+                return ans
+        
         else:
-            return -1
+            return  self.search_array(nums, target)
