c4820a3fbf815d111785b17c2c4c87d5a3a34964
Arushi Sharma <arushi.net@outlook.com>
2022-04-12 05:56:11 +0000

    [LeetCode Sync] Runtime - 48 ms (100.00%), Memory - 13.9 MB (100.00%)

---
+++ my-folder/0070-climbing-stairs/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int, memo = {}) -> int:
+        
+        if n in memo:
+            return memo[n]
+        
+        if n <= 1:
+            return n
+        
+        if n == 2:
+            return 2
+        
+        memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        return memo[n]
+        
+        
+        
