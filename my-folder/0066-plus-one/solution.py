03c8f341c523e75794a259a2f214a366dcd3d2cd
Arushi Sharma <arushi.net@outlook.com>
2022-06-12 02:41:05 +0000

    [LeetCode Sync] Runtime - 59 ms (6.43%), Memory - 13.8 MB (100.00%)

--- my-folder/0066-plus-one/solution.py
+++ my-folder/0066-plus-one/solution.py
@@ -1,32 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
-        for i in range(len(digits)-1, -1, -1):
-            if digits[i] < 9:
+        n = len(digits)
+        digits[n-1] += 1
+        carry = digits[n-1] / 10
+        digits[n-1] = digits[n-1] % 10
+        
+        for i in range(n-2, -1, -1):
+            
+            if carry == 1:
                 digits[i] += 1
-                return digits
-            else:
-                digits[i] = 0
+                carry = digits[i] / 10
+                digits[i] = digits[i] % 10
         
-        
-        if digits[0] == 0:
+        if carry == 1:
             digits.insert(0, 1)
-            
+        
         return digits
-        
-#         integer = 0
-#         place = len(digits) - 1
-#         for i in digits:
-#             integer += i*pow(10, place)
-#             place -= 1
-        
-#         integer += 1
-#         print(integer)
-        
-#         digits = []
-#         while(integer > 0):
-#             digits.append(integer%10)
-#             integer = int(integer/ 10)
-            
-#         # print(digits)
-#         return [digits[i] for i in range(len(digits)-1, -1, -1)]
