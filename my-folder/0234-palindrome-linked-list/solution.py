7b900710ec90d5606057245f000386e2ef3d0e36
Arushi Sharma <arushi.net@outlook.com>
2022-06-15 07:26:01 +0000

    [LeetCode Sync] Runtime - 1328 ms (5.01%), Memory - 39.1 MB (9.44%)

--- my-folder/0234-palindrome-linked-list/solution.py
+++ my-folder/0234-palindrome-linked-list/solution.py
@@ -5,55 +5,43 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if head.next == None:
-            return True
         
-#         get to the middle of LL
-#         reverse the second half LL
-#         compare the 2 halfs
-        
+        if not head: return False
+        if not head.next: return True
         slow = head
         fast = head
-        while fast.next and fast.next.next:
-            fast = fast.next.next
-            slow = slow.next
-        
-        prev = None
-        curr2 = slow
-        while curr2:
-            temp = curr2.next
-            curr2.next = prev
-            # print("curr2", curr2)
-            prev = curr2
-            curr2 = temp
-        
-        while prev and head:
-            # print(head, prev)
-            if head.val != prev.val:
-                return False
+        head2 = None
+        while slow and fast and fast.next:
             
-            head = head.next
-            prev = prev.next
-
+            fast = fast.next.next
+            
+            if not fast or not fast.next:
+                head2 = slow.next
+                slow.next = None
+                
+            slow = slow.next
+#         2 -> 1 -> 0
+        curr = head2
+        prev = None
+        while curr:
+            
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        head2 = prev
+        
+        curr1 = head
+        curr2 = head2
+        
+        while curr1 and curr2:
+            if curr1.val != curr2.val:
+                return False
+            curr1 = curr1.next
+            curr2 = curr2.next
+        
         return True
+            
+            
         
-        
-        
-        
-        
-#         list1 = []
-#         curr = head
-#         while curr!= None:
-#             list1.append(curr.val)
-#             curr = curr.next
-        
-#         l = 0
-#         r = len(list1) - 1
-#         while(l < r):
-#             if list1[l] == list1[r]:
-#                 l += 1
-#                 r -= 1
-#             else:
-#                 return False
-        
-#         return True
