d85c10b0770be1c525523c02a21235315fb73574
Arushi Sharma <arushi.net@outlook.com>
2022-06-16 16:58:14 +0000

    [LeetCode Sync] Runtime - 59 ms (7.71%), Memory - 14 MB (100.00%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,17 +1,37 @@
 class Solution:
-        
+    def __init__(self):
+#         nums = [1, 2, 3, 1]
+#         memo = [1, 2, 4]
+        self.memo = {}
     def rob(self, nums: List[int]) -> int:
         
-        return self.rob_helper(nums, 0)
-    
-    def rob_helper(self, nums, i, memo={}):
-        if i >= len(nums):
+        if not nums:
             return 0
         
+        memo = [0] * (len(nums))
         
-        if tuple(nums[i:]) in memo:
-            return memo[tuple(nums[i:])]
+        for i in range(len(nums)):
+            if i == 0:
+                memo[i] = nums[i]
+            elif i == 1:
+                memo[i] = max(memo[i-1], nums[i])
+            else:
+                memo[i] = max(memo[i-2] + nums[i], memo[i-1])
+            # memo[i] = max(sum(list(range(0, i, 2))) + nums[i], sum(list(range(1, i, 2))))
+            
+        return memo[-1]
         
+#         if not nums:
+#             return 0
         
-        memo[tuple(nums[i:])]= max(self.rob_helper(nums, i + 2) + nums[i], self.rob_helper(nums, i + 1))
-        return memo[tuple(nums[i:])]
+#         if len(nums) == 1:
+#             return nums[0]
+        
+#         if len(nums) in self.memo:
+#             return self.memo[len(nums)]
+            
+#         self.memo[len(nums)] = max(self.rob(nums[2:]) + nums[0], self.rob(nums[1:]))
+        
+#         return self.memo[len(nums)]
+    
+    
