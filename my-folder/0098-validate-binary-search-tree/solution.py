614eb0f36fff1dd40494b4c8f3ca55ebe7f9127e
Arushi Sharma <arushi.net@outlook.com>
2022-06-19 17:50:50 +0000

    [LeetCode Sync] Runtime - 542 ms (5.90%), Memory - 17.3 MB (100.00%)

--- my-folder/0098-validate-binary-search-tree/solution.py
+++ my-folder/0098-validate-binary-search-tree/solution.py
@@ -5,16 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode], floor=float("-inf"), ceiling=float("inf")) -> bool:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        if not root:
-            return True
+        if not root : return True
+        if not root.left and not root.right: return True
         
-        if root.val <= floor or root.val >= ceiling:
-            return False
+        max_left = self.findmax(root.left)
+        min_right = self.findmin(root.right)
+        left = self.isValidBST(root.left)
+        right = self.isValidBST(root.right)
         
-        return self.isValidBST(root.left, floor, root.val) and self.isValidBST(root.right, root.val, ceiling)
+        return root.val < min_right and root.val > max_left and left and right
         
         
+    def findmin(self, root):
         
+        if not root: return float("inf")
         
+        left = self.findmin(root.left)
+        right = self.findmin(root.right)
+        
+        return min(root.val, left, right)
+    
+    def findmax(self, root):
+        
+        if not root: return float("-inf")
+        
+        left = self.findmax(root.left)
+        right = self.findmax(root.right)
+        
+        return max(root.val, left, right)
