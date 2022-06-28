a8a17df2325bdb43969ac0499fa64999dc9469d4
Arushi Sharma <arushi.net@outlook.com>
2022-06-28 12:10:55 +0000

    [LeetCode Sync] Runtime - 129 ms (13.63%), Memory - 14.3 MB (100.00%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,21 +1,18 @@
-from collections import deque
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
-        queue = deque()
-        max_len = 0
-        for i in s:
-            if i not in queue:
-                queue.append(i)
-            else:
-                max_len = max(max_len, len(queue))
-                while i != queue[0]:
-                    del queue[0]
-                if len(queue) >= 1 and i == queue[0]:
-                    del queue[0]
-                    queue.append(i)
+        if not s: return 0
+        longest = 0
+        j = 1
+        temp = [s[0]]
+        while j < len(s):
+            while s[j] in temp:
+                temp.pop(0)
+            temp.append(s[j])
+            j += 1
+            
+            longest = max(longest, len(temp))
+        longest = max(longest, len(temp))
         
-        print(max_len, len(queue), queue)
-        max_len = max(max_len, len(queue))
-        return max_len
-        
+        return longest
+            
