bb8c32127cad970e3952c69ecb89d0cf695f94f6
Arushi Sharma <arushi.net@outlook.com>
2022-06-28 10:42:31 +0000

    [LeetCode Sync] Runtime - 98 ms (5.30%), Memory - 13.9 MB (100.00%)

---
+++ my-folder/0080-remove-duplicates-from-sorted-array-ii/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        i = 2
+        j = 2
+        while j < len(nums):
+            if nums[j] == nums[i-2]:
+                j += 1
+                continue
+            else:
+                nums[i] = nums[j]
+                i += 1
+                j += 1
+        return i
