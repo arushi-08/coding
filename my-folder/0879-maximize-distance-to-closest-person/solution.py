83419b1e59a9e6b0b6ae0f535c79fef45bec1914
Arushi Sharma <arushi.net@outlook.com>
2022-07-18 13:53:26 +0000

    [LeetCode Sync] Runtime - 235 ms (5.68%), Memory - 14.6 MB (100.00%)

--- my-folder/0879-maximize-distance-to-closest-person/solution.py
+++ my-folder/0879-maximize-distance-to-closest-person/solution.py
@@ -1,31 +1,14 @@
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
         
-        N = len(seats)
-        distance = [0] * N
+        ones = iter(i for i,seat in enumerate(seats) if seat)
+        prev = front_zeros = next(ones)
+        mid_zeros = 0
+        for i in ones:
+            mid_zeros = max(mid_zeros, i - prev - 1)
+            prev = i
         
-        last_occupied = 0
-        for i in range(N):
-            if seats[i] == 0:
-                if seats[last_occupied] == 1:
-                    distance[i] = i - last_occupied
-                else:
-                    pass
-            else:
-                last_occupied = i
-        # print(distance)
-        last_occupied = N-1
-        for i in range(N-1, -1 ,-1):
-            if seats[i] == 0:
-                if seats[last_occupied] == 1:
-                    if distance[i] == 0:
-                        distance[i] = last_occupied - i
-                    else:
-                        distance[i] = min(distance[i], last_occupied - i)
-                else:
-                    pass
-            else:
-                last_occupied = i
+        rear_zeros = len(seats) - prev - 1
         
-        return max(distance)
-        
+        return max(front_zeros, (mid_zeros + 1) >> 1, rear_zeros)
+                
