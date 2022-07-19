2c7213edc705636476208bc33b2283b707eb896d
Arushi Sharma <arushi.net@outlook.com>
2022-07-19 16:23:25 +0000

    [LeetCode Sync] Runtime - 39 ms (100.00%), Memory - 13.8 MB (100.00%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,14 +1,24 @@
 class Solution:
-    def __init__(self):
-        self.memo = []
     def climbStairs(self, n: int) -> int:
-        self.memo = [0]*(n + 1)
-        return self.helper(n)
-    
-    def helper(self, n):
-        if n in [0, 1]: return n
-        if n == 2: return 2
+        # recursive
+#         if n < 0: return 0
+#         if n <= 2: return n
         
-        if self.memo[n] : return self.memo[n]
-        self.memo[n] =  self.helper(n-1) + self.helper(n-2)
-        return self.memo[n]
+#         return self.climbStairs(n-1) + self.climbStairs(n-2) 
+        
+        # n = 2 ans = 2: f(n) = f(n-1) + 1 + f(n-2) + 2
+        # base condns = if n == 0 or 1 or 2: return n
+        # if n == 3 return 3, 1 + 1 + 1, 2 + 1, 1 + 2
+        if n <= 2: 
+            return n
+        
+        dp = [0]*(n+1)
+        for i in [0,1,2]:
+            dp[i] = i
+        
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
+        
