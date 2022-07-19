47ced09f9c6e64a3fdb9d0a5fa208dc7c9bff713
Arushi Sharma <arushi.net@outlook.com>
2022-07-19 19:04:00 +0000

    [LeetCode Sync] Runtime - 54 ms (7.71%), Memory - 13.9 MB (100.00%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,37 +1,14 @@
 class Solution:
-    def __init__(self):
-#         nums = [1, 2, 3, 1]
-#         memo = [1, 2, 4]
-        self.memo = {}
     def rob(self, nums: List[int]) -> int:
+        # don't take adjacent vals, maximize nums sum
+        if not nums: return 0
+        n = len(nums)
+        if n == 1: return nums[0]
+        dp = [0]*n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
         
-        if not nums:
-            return 0
+        for i in range(2, n):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
         
-        memo = [0] * (len(nums))
-        
-        for i in range(len(nums)):
-            if i == 0:
-                memo[i] = nums[i]
-            elif i == 1:
-                memo[i] = max(memo[i-1], nums[i])
-            else:
-                memo[i] = max(memo[i-2] + nums[i], memo[i-1])
-            # memo[i] = max(sum(list(range(0, i, 2))) + nums[i], sum(list(range(1, i, 2))))
-            
-        return memo[-1]
-        
-#         if not nums:
-#             return 0
-        
-#         if len(nums) == 1:
-#             return nums[0]
-        
-#         if len(nums) in self.memo:
-#             return self.memo[len(nums)]
-            
-#         self.memo[len(nums)] = max(self.rob(nums[2:]) + nums[0], self.rob(nums[1:]))
-        
-#         return self.memo[len(nums)]
-    
-    
+        return max(dp[-1], dp[-2])
