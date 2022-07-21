ae4a0b3cfde6737bf99acb65eca63e983a5051d0
Arushi Sharma <arushi.net@outlook.com>
2022-07-21 11:07:58 +0000

    [LeetCode Sync] Runtime - 619 ms (33.24%), Memory - 21.9 MB (90.30%)

--- my-folder/1250-longest-common-subsequence/solution.py
+++ my-folder/1250-longest-common-subsequence/solution.py
@@ -1,36 +1,19 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-        
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        if not text1 or not text2: return 0
-        if text1 == text2: return len(text1)
+        n = len(text1)
+        m = len(text2)
+        dp = [[0]*(m+1) for _ in range(n+1)]
         
-        dp = [[0]*(len(text2)+1) for _ in range(len(text1)+1)]
+        lcs = 0
         
-        for i in range(len(text1)-1, -1, -1):
-            for j in range(len(text2)-1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
-                else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-        
-        return dp[0][0]
-        
-        # for i in range(len(text1)):
-        #     for j in range(len(text2)):
-        #         if dp[i][j] == 1:
-                    
+        for i in range(1, n+1):
+            for j in range(1,m+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
                 
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    
+        return dp[i][j]
         
-#         if (text1, text2) in self.memo:
-#             return self.memo[(text1, text2)]
-        
-#         if text1[0] == text2[0]: 
-#             self.memo[(text1, text2)] = self.longestCommonSubsequence(text1[1:], text2[1:]) + 1
-#         else:
-#             self.memo[(text1, text2)] = max(self.longestCommonSubsequence(text1, text2[1:]),
-#                       self.longestCommonSubsequence(text1[1:], text2))
-        
-#         return self.memo[(text1, text2)]
