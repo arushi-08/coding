a122e57757fffc5964cbd79c04928b6c616ad002
Arushi Sharma <arushi.net@outlook.com>
2022-07-22 05:07:33 +0000

    [LeetCode Sync] Runtime - 4955 ms (13.25%), Memory - 22 MB (17.95%)

--- my-folder/0005-longest-palindromic-substring/solution.py
+++ my-folder/0005-longest-palindromic-substring/solution.py
@@ -1,21 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1: return s
+        n = len(s)
+        dp = [[0]*(n) for _ in range(n)]
+        max_len_pal = ''
+        for i in range(n):
+                dp[i][i] = True
+                max_len_pal = s[i]
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    if j - i == 1 or dp[i+1][j-1] == True:
+                        dp[i][j] = True
+                        
+                        if len(s[i:j+1]) > len(max_len_pal):
+                            max_len_pal = s[i:j+1]
         
-        if len(s) <= 1:
-            return s
-        
-        ans = ""
-        for i in range(len(s)):
-            temp1 = self.helper(s, i, i)
-            temp2 = self.helper(s, i, i+1)
-            ans = max(temp1, temp2, ans, key=len)
-        
-        return ans
+        return max_len_pal
     
-    def helper(self, s, l, r):
-        
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-        
-        return s[l+1:r]
