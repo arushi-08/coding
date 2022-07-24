8ff1ffa7b6daf3454504b4281fe45e3f79eb7fd5
Arushi Sharma <arushi.net@outlook.com>
2022-07-24 11:23:19 +0000

    [LeetCode Sync] Runtime - 157 ms (6.82%), Memory - 14.1 MB (100.00%)

--- my-folder/0733-flood-fill/solution.py
+++ my-folder/0733-flood-fill/solution.py
@@ -1,43 +1,37 @@
 from collections import deque
 class Solution:
+    def isSafe(self, x, y, visited, image, current_color):
+        return (x >= 0 
+                and y >= 0 
+                and x < len(image) 
+                and y < len(image[0]) 
+                and visited[x][y] == False
+                and image[x][y] == current_color)
+        
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
+        current_color = image[sr][sc]
+        
         visited = [[False]*len(image[0]) for _ in range(len(image))]
-        self.bfs(image, sr, sc, color, visited)
-        return image
-    
-    def bfs(self, image, sr, sc, color, visited):
+        visited[sr][sc] = True
         
         queue = deque()
         queue.append((sr, sc))
-        visited[sr][sc] = True
-        rows_list = [0, 1, 0, -1]
-        cols_list = [1, 0, -1, 0]
-        curr_color = image[sr][sc]
         
-        while len(queue):
-            cur_row, cur_col = queue.popleft()
-            print(cur_row, cur_col)
-            image[cur_row][cur_col] = color
+        while queue:
+            curr_x, curr_y = queue.popleft()
+            image[curr_x][curr_y] = color
             
-            for i in range(len(rows_list)):
-                if self.isSafe(image, 
-                               cur_row + rows_list[i],
-                               cur_col + cols_list[i],
-                               visited,
-                               curr_color
-                              ):
-                    queue.append((cur_row + rows_list[i], cur_col + cols_list[i]))
-                    print(queue)
-                    visited[cur_row + rows_list[i]][cur_col + cols_list[i]] = True
-    
-    def isSafe(self, image, row, col, visited, color):
-        return (
-            row >= 0 and
-            col >= 0 and
-            row < len(image) and
-            col < len(image[0]) and
-            not visited[row][col] and
-            image[row][col] == color
-        )
-                
+            possible_moves_x = [1, 0, -1, 0]
+            possible_moves_y = [0, -1, 0, 1]
+            
+            for x, y in zip(possible_moves_x, possible_moves_y):
+                if self.isSafe(curr_x + x, 
+                               curr_y + y, 
+                               visited, 
+                               image, 
+                               current_color):
+                    visited[curr_x + x][curr_y + y] = True
+                    queue.append((curr_x + x, curr_y + y))
+                    
+        return image
