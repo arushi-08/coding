e156364a5e8ff12514ed37e88ed5d6643f4ff6a3
Arushi Sharma <arushi.net@outlook.com>
2022-07-24 16:59:15 +0000

    [LeetCode Sync] Runtime - 265 ms (5.32%), Memory - 18.6 MB (100.00%)

--- my-folder/0378-kth-smallest-element-in-a-sorted-matrix/solution.py
+++ my-folder/0378-kth-smallest-element-in-a-sorted-matrix/solution.py
@@ -1,31 +1,19 @@
-import heapq
+from heapq import heappush, heappop, heapify
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        """
-         BFS traversal 
-        1. insert right and bottom elements in min heap, 
-        2. pop k times. Last popped value is result
-        """
+        # O(N + klogn)
         heap = []
-
-        result = 0
+        for i in range(len(matrix)): #  O(N)
+            heap.append((matrix[i][0], i, 0))
+            
+        heapify(heap) # O(N)
+        
         i = 0
-        n = len(matrix)
-        visited = [[False]*n for _ in range(n)]
-        heapq.heappush(heap, (matrix[0][0], 0, 0))
-
-        while i < k:
-            val, row, col = heapq.heappop(heap)
-            result = val
-
-            if col + 1 < n and not visited[row][col + 1]:
-                heapq.heappush(heap,(matrix[row][col + 1], row, col + 1))
-                visited[row][col + 1] = True
-
-            if row + 1 < n and not visited[row + 1][col]:
-                heapq.heappush(heap,(matrix[row + 1][col], row + 1, col))
-                visited[row + 1][col] = True
-
+        while i < k: # O(klogN)
+            curr, row, col = heappop(heap)
+            if col + 1 < len(matrix[row]):
+                heappush(heap, (matrix[row][col + 1], row, col + 1))
             i += 1
-
-        return result
+        
+        return curr
+            
