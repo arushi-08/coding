936f3c88feac86067f92ca63f9d73e62f8b0256d
Arushi Sharma <arushi.net@outlook.com>
2022-07-25 17:39:58 +0000

    [LeetCode Sync] Runtime - 4767 ms (5.00%), Memory - 16 MB (100.00%)

--- my-folder/0322-coin-change/solution.py
+++ my-folder/0322-coin-change/solution.py
@@ -1,57 +1,30 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-        
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        dp = [float("inf")] * (amount + 1)
-        dp[0] = 0
-        
-        for i in range(len(coins)):
-            for j in range(coins[i], amount+1):
-                dp[j] = min(dp[j], dp[j-coins[i]] + 1)
-        
-        
-        return dp[-1] if dp[-1] < float("inf") else -1
-            
+        """
+        nums[i]  amount 
+            0, 1, 2, 3
+         0  0  1  0. 0    1
+         1  0  0  1  0    2
+         2  0  0  0  0    5
+        """
         
         
-#         def helper(coins, amount):
-#             if amount == 0:
-#                 return 0
-
-#             if amount < 0:
-#                 return float("inf")
-
-#             if amount in self.memo:
-#                 return self.memo[amount]
-
-#             values = []
-#             for i in range(len(coins)):
-#                 recursion = helper(coins, amount-coins[i])
-#                 # print(recursion, amount)
-#                 values.append(recursion+1)
-
-#             self.memo[amount] =  min(values)
-#             return self.memo[amount]
+        dp = [[float("inf")]*(amount+1) for _ in range(len(coins))]
         
-#         values = helper(coins, amount)
-#         return values if values < float("inf") else -1
-# ----------------
-#         memo = {}
+        for i in range(len(dp)):
+            dp[i][0] = 0
+        for i in range(len(dp)):
+            for j in range(len(dp[0])):
+                if coins[i] == j:
+                    dp[i][j] = 1
+                
+                if j >= coins[i]:
+                    dp[i][j] = min(dp[i-1][j],
+                                  1 + dp[i][j-coins[i]])
+                else:
+                    dp[i][j] = dp[i-1][j]
         
-#         def check(amount):
-#             if amount in memo: return memo[amount]
-#             if amount == 0: return 0
-#             if amount < 0: return float("inf")
-#             temp = []
-#             for coin in coins:
-#                 val = check(amount - coin)
-#                 print(val)
-#                 temp.append(1 + val)
-#             memo[amount] = min(temp)
-#             return memo[amount]
+        if dp[len(coins)-1][amount] == float("inf"):
+            return -1
+        return dp[len(coins)-1][amount] 
         
-#         minimum = check(amount)
-#         return minimum if minimum < float("inf") else -1
-
