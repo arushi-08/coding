79539d65c65b82f8d1d7ed59b5045d0d831f0ddf
Arushi Sharma <arushi.net@outlook.com>
2022-07-25 19:31:11 +0000

    [LeetCode Sync] Runtime - 2265 ms (5.04%), Memory - 16 MB (100.00%)

--- my-folder/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.py
+++ my-folder/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.py
@@ -1,21 +1,18 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
+        # LCS 
         
-        s2 = list(s).copy()
-        s2.reverse()
-        s2 = "".join(s2)
+        s_reverse = list(s)
+        s_reverse.reverse()
+        s_reverse = "".join(s_reverse)
         
-        n = len(s)
-        dp = [[0]*(n+1) for _ in range(n+1)]
+        dp = [[0] * (len(s) + 1) for _ in range(len(s) + 1)]
         
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                if s[i-1] == s2[j-1]:
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                if s[i-1] == s_reverse[j-1]:
                     dp[i][j] = dp[i-1][j-1] + 1
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         
-        pal_len = dp[n][n]
-        # print(dp)
-        return n-pal_len
-                    
+        return len(s) - dp[len(s)][len(s)]
