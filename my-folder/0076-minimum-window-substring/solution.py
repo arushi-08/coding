24e3e3139f649ba46c2882b60b509afdeea2aaed
Arushi Sharma <arushi.net@outlook.com>
2022-07-25 09:18:34 +0000

    [LeetCode Sync] Runtime - 188 ms (21.19%), Memory - 14.8 MB (100.00%)

--- my-folder/0076-minimum-window-substring/solution.py
+++ my-folder/0076-minimum-window-substring/solution.py
@@ -1,29 +1,52 @@
-from collections import Counter
+from collections import Counter, defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        # find min substring in s that has all chars of t
+        # use freq dictionary of t as window
         
-        t_counter = Counter(t)
-        t_len = len(t)
-        i = 0
-        start, end = 0, 0
-        for j, char in enumerate(s, 1):
+        # keep expanding window by moving right pointer.
+        # when window has all desired characters (all T chars), 
+        # contract left pointer
+        # save smallest window
+        
+        
+        t_freq_dict = Counter(t)
+        left_idx = 0
+        right_idx = 0
+        window = defaultdict(int)
+        maxstring = ""
+        desired_chars_length = 0 
+        
+        while right_idx < len(s):
             
-            if t_counter[char] > 0:
-                t_len -= 1
-            t_counter[char] -= 1
+            # desired_chars_length = 0
+            # keep expanding window by moving right pointer.
+            new_char = s[right_idx]
+            window[new_char] += 1
             
-            if t_len == 0:
-                while t_counter[s[i]] < 0:
-                    t_counter[s[i]] += 1
-                    i += 1
+            if new_char in t_freq_dict and t_freq_dict[new_char] == window[new_char]:
+                desired_chars_length += 1
+            
+            # when window has all desired characters
+            if desired_chars_length >= len(t_freq_dict):
+                #contract
+                old_char = s[left_idx]
+                while old_char not in t_freq_dict or window[old_char] > t_freq_dict[old_char]:
+                    window[old_char] -= 1
                     
-                if end ==0 or end - start > j - i:
-                    start, end = i, j
+                    if old_char in t_freq_dict and window[old_char] < t_freq_dict[old_char]:
+                        desired_chars_length -= 1
+                    left_idx += 1
+                    old_char = s[left_idx]
                     
-                t_counter[s[i]] += 1  #why?
-                t_len += 1
-                i += 1
-        
-        return s[start:end]
-        
+                currstring = s[left_idx:right_idx+1]
 
+                if not maxstring:
+                    maxstring = currstring
+                maxstring = min(maxstring, currstring, key=len)
+            
+
+            right_idx += 1
+            
+        return maxstring
+            
