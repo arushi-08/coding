93c81d97bb2d0936d8c02832b32ee5c32d0be27a
Arushi Sharma <arushi.net@outlook.com>
2022-10-09 09:41:27 +0000

    [LeetCode Sync] Runtime - 59 ms (7.21%), Memory - 14 MB (100.00%)

--- my-folder/0278-first-bad-version/solution.py
+++ my-folder/0278-first-bad-version/solution.py
@@ -4,16 +4,16 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         
-        l = 1
-        r = n
-        
-        while l < r:
-            mid = (l + r)//2
-            
+        left = 1
+        right = n
+        while left < right:
+            mid = (left + right) // 2
             if isBadVersion(mid):
-                r = mid
-            
+                right = mid
             else:
-                l = mid + 1
-                
-        return r
+                left = mid + 1
+        
+        if isBadVersion(left):
+            return left
+        
+        return -1
