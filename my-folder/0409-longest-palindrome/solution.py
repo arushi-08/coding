6c492c317eb9f4543404543af984da03ad97a9f7
Arushi Sharma <arushi.net@outlook.com>
2022-10-09 09:06:25 +0000

    [LeetCode Sync] Runtime - 74 ms (5.29%), Memory - 13.9 MB (100.00%)

--- my-folder/0409-longest-palindrome/solution.py
+++ my-folder/0409-longest-palindrome/solution.py
@@ -1,26 +1,17 @@
+from collections import Counter
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        
-        repeat_elements = {}
-        for i in range(len(s)):
-            repeat_elements[s[i]] = repeat_elements.get(s[i],0)+1
-        
-        print(repeat_elements)
-        
-        is_odd = 0
+        freq = Counter(s)
         ans = 0
-        for key in repeat_elements:
-            if repeat_elements[key] % 2 == 1:
-                # max_odd = max(max_odd, repeat_elements[key])
-                ans += repeat_elements[key] - 1
-                is_odd = 1
+        is_odd = 0
+        for i in freq.values():
+            if i % 2 == 0:
+                ans += i
             else:
-                ans += repeat_elements[key]
+                ans += i - 1
+                is_odd = 1
         
         if is_odd:
-            return 1 + ans
+            ans += 1
+        
         return ans
-            
-            
-            
-            
