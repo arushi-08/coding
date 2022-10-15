5ab48061208f4090ec0545236bbae29a47afdc6a
Arushi Sharma <arushi.net@outlook.com>
2022-10-15 18:00:56 +0000

    [LeetCode Sync] Runtime - 190 ms (5.71%), Memory - 18.8 MB (99.96%)

--- my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -8,45 +8,15 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        if not root or not p or not q: return root
-        
+        if not root or not p or not q:
+            return root
         if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
         elif p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
         
-        return root
         
-#         parent_p = []
-#         self.dfs(parent_p, p, root)
-#         parent_q = []
-#         self.dfs(parent_q, q, root)
-        
-#         ancestor = None
-    
-#         for i,j in zip(parent_p, parent_q):
-#             if i != j:
-#                 break
-#             ancestor = i
-        
-#         return ancestor
-        
-#     def dfs(self, parent, node, root):
-        
-#         if not root: return False
-        
-#         if root.val == node.val: 
-#             # print("inside", [p1.val for p1 in parent])
-#             parent.append(node)
-#             return True
-        
-#         parent.append(root)
-#         left = self.dfs(parent, node, root.left)
-#         if left:
-#             return True
-#         right = self.dfs(parent, node, root.right)
-#         if right: 
-#             return True
-#         parent.pop()
         
         
