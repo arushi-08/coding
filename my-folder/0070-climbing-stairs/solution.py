541392ba1079b42724fdab62a93c854eb8d5d969
Arushi Sharma <arushi.net@outlook.com>
2022-10-15 18:52:15 +0000

    [LeetCode Sync] Runtime - 52 ms (100.00%), Memory - 13.9 MB (100.00%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,24 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # recursive
-#         if n < 0: return 0
-#         if n <= 2: return n
         
-#         return self.climbStairs(n-1) + self.climbStairs(n-2) 
-        
-        # n = 2 ans = 2: f(n) = f(n-1) + 1 + f(n-2) + 2
-        # base condns = if n == 0 or 1 or 2: return n
-        # if n == 3 return 3, 1 + 1 + 1, 2 + 1, 1 + 2
-        if n <= 2: 
+        if n in range(3):
             return n
-        
-        dp = [0]*(n+1)
-        for i in [0,1,2]:
+        dp = [0] * (n + 1)
+        for i in range(3):
             dp[i] = i
-        
-        for i in range(3,n+1):
+        for i in range(3, n+1):
             dp[i] = dp[i-1] + dp[i-2]
-        
-        return dp[n]
-        
-        
+        return dp[-1]
