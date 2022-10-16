3650f7fb2a7651bff29144f37cb50cffd4e2e0f2
Arushi Sharma <arushi.net@outlook.com>
2022-10-16 17:24:31 +0000

    [LeetCode Sync] Runtime - 56 ms (8.42%), Memory - 14 MB (100.00%)

--- my-folder/0062-unique-paths/solution.py
+++ my-folder/0062-unique-paths/solution.py
@@ -1,47 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-        
     def uniquePaths(self, m: int, n: int) -> int:
         
-        dp = [[0]*(n+1)]*(m+1)
+        if n == 1 or m == 1:
+            return 1
         
-        for i in range(m + 1):
-            for j in range(n + 1):
-                    
-                if i < 1 or j < 1:
-                    continue
-                    
-                elif i == 1 or j == 1:
-                    dp[i][j] = 1
-                    
-                elif i == j and i == 2:
-                    dp[i][j] = 2
-                    
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        
-        # print(dp, m-1, n)
-        # dp[m][n] = dp[m-1][n] + dp[m][n-1]       
-        
-        return dp[m][n]
+        dp = [[1]*n for i in range(m)]
         
         
-#         if m == n and m < 1:
-#             return 0
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
-#         if m != n and (m < 1 or n < 1):
-#             return 0
-        
-#         if m == 1 or n == 1:
-#             return 1
-        
-#         if m == n and m == 2:
-#             return 2
-        
-#         if (m,n) in self.memo:
-#             return self.memo[(m,n)]
-        
-#         self.memo[(m,n)] =  self.uniquePaths(m - 1, n) + self.uniquePaths(m, n-1)
-#         return self.memo[(m,n)]
-    
+        return dp[m-1][n-1]
