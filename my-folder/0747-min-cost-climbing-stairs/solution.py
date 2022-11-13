69b041c3169845712a058a4ad2b8e80817367282
Arushi Sharma <arushi.net@outlook.com>
2022-11-13 13:20:42 +0000

    [LeetCode Sync] Runtime - 103 ms (5.68%), Memory - 14 MB (100.00%)

--- my-folder/0747-min-cost-climbing-stairs/solution.py
+++ my-folder/0747-min-cost-climbing-stairs/solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        if len(cost) < 3:
+        if len(cost) <= 2:
             return min(cost)
         
-        dp = [0] * (len(cost))
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-        for i in range(2, len(cost)):
-            if i != len(cost)-1:
-                dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
-            else:
-                dp[i] = min(dp[i-1], dp[i-2] + cost[i])
-        
+        dp = [0] * len(cost)
+        dp[0] = 0
+        dp[1] = min(cost[:2])
+        for i in range(2, len(dp)):
+            dp[i] = min(dp[i-2]+cost[i-1], dp[i-1]+cost[i])
+         
         return dp[-1]
