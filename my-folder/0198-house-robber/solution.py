0c126e463d4485219840e04e874bd79df0a071ae
Arushi Sharma <arushi.net@outlook.com>
2022-11-13 14:16:00 +0000

    [LeetCode Sync] Runtime - 54 ms (7.71%), Memory - 13.9 MB (100.00%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,21 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # adjacent not
-        # [1,2,3,1]
-        # 1 + [2,3,1] 0 + [2,3,1]
-        # 3 + [3,1] 1 + [3,1]  2 + [3,1] 0 + [3,1]
-        # idx -> len(nums) 
         
+        if len(nums) <= 2:
+            return max(nums)
         
-        dp = [0] * (len(nums))
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[:2])
         
-        for i in range(len(nums)):
-            if i == 0:
-                dp[i] = nums[i]
-                continue
-            if i == 1:
-                dp[i] = max(nums[i], nums[i-1])
-                continue
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        for i in range(2, len(dp)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         
-        return dp[len(nums)-1]
+        return dp[-1]
