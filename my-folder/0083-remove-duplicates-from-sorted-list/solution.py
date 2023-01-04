bba8d8b8060e2e69f89a3ec8d4d45b6c173e8f59
Arushi Sharma <arushi.net@outlook.com>
2023-01-04 23:32:11 +0000

    [LeetCode Sync] Runtime - 84 ms (9.21%), Memory - 13.9 MB (100.00%)

--- my-folder/0083-remove-duplicates-from-sorted-list/solution.py
+++ my-folder/0083-remove-duplicates-from-sorted-list/solution.py
@@ -5,20 +5,20 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+
+        ptr1 = head
         if not head or not head.next:
             return head
-        
-        ptr1 = head
         ptr2 = head.next
-        
-        while ptr2:
+
+        while ptr1 and ptr2:
             if ptr1.val != ptr2.val:
                 ptr1.next = ptr2
-                ptr1 = ptr1.next
-            
-            ptr2 = ptr2.next
+                ptr1 = ptr2
+                ptr2 = ptr2.next
+            else:
+                if not ptr2.next:
+                    ptr1.next = ptr2.next
+                ptr2 = ptr2.next
         
-        ptr1.next = None
         return head
-                
