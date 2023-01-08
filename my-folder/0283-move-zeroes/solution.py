45c4564f5f5f3694a0268dfca9a0932e8476ef58
Arushi Sharma <arushi.net@outlook.com>
2023-01-08 04:02:47 +0000

    [LeetCode Sync] Runtime - 177 ms (12.99%), Memory - 15.6 MB (100.00%)

--- my-folder/0283-move-zeroes/solution.py
+++ my-folder/0283-move-zeroes/solution.py
@@ -3,11 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        lastnonzerofoundat = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[lastnonzerofoundat], nums[i] = nums[i], nums[lastnonzerofoundat]
+                lastnonzerofoundat += 1
         
-        i = 0
-        j = 0
-        while i < len(nums) and j < len(nums):
-            if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-            j += 1
+        return nums
