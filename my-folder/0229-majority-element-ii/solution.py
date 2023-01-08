2b08139f60fbff36ceae70c38d946864495fe341
Arushi Sharma <arushi.net@outlook.com>
2023-01-08 14:06:26 +0000

    [LeetCode Sync] Runtime - 109 ms (5.00%), Memory - 15.3 MB (100.00%)

--- my-folder/0229-majority-element-ii/solution.py
+++ my-folder/0229-majority-element-ii/solution.py
@@ -1,28 +1,30 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        
-#.         [2,  3,   4,  2,     1,      3] 4/3 = 1
-# counter1  1        1   1      
-# memory1   2        4   2 
-# counter2      1         you know that only 2 elements will be > n/3      
-# memory2       3           
-        counter1, counter2, memory1, memory2 = 0, 0, 0, 0
-    
-        for i in range(len(nums)):
-            if nums[i] == memory1:
+
+        # define maj_cand1, maj_cand2, counter1, counter2
+        maj_cand1, maj_cand2, counter1, counter2 = None, None, 0, 0
+
+        for n in nums:
+            if maj_cand1 == n:
                 counter1 += 1
-            elif nums[i] == memory2:
+            elif maj_cand2 == n:
                 counter2 += 1
             elif counter1 == 0:
-                memory1 = nums[i]
+                maj_cand1 = n
                 counter1 += 1
             elif counter2 == 0:
-                memory2 = nums[i]
+                maj_cand2 = n
                 counter2 += 1
             else:
                 counter1 -= 1
                 counter2 -= 1
-        
-        return [n for n in set((memory1, memory2)) if nums.count(n) > len(nums)/3]
             
+        ans = []
+        for i in [maj_cand1, maj_cand2]:
+            if nums.count(i) > len(nums)/ 3:
+                ans.append(i)
+        
+        return list(set(ans))
+
+
