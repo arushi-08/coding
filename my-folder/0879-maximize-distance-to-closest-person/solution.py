35c52dcce2207798ffcc48208751fb6a57a1ca15
Arushi Sharma <arushi.net@outlook.com>
2023-01-13 21:56:36 +0000

    [LeetCode Sync] Runtime - 160 ms (5.68%), Memory - 14.7 MB (100.00%)

--- my-folder/0879-maximize-distance-to-closest-person/solution.py
+++ my-folder/0879-maximize-distance-to-closest-person/solution.py
@@ -1,36 +1,30 @@
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
-        
-        prev = None
-        people = []
-        for i, seat in enumerate(seats):
-            if seat:
-                people.append(i)
-        
-        people = iter(people)
-        future = next(people, None)
-        ans = 0
+
+        # 1, 0, 0, 0, 1, 0, 1
+        # _, 1, 2, 3, _, 1, _
+        # _, 3, 2, 1, _, 1, _
+
+        left_dist = []
+        closest_1_idx = float('-inf')
         for i in range(len(seats)):
-            if seats[i]:
-                prev = i
+            if seats[i] == 1:
+                left_dist.append(0)
+                closest_1_idx = i
             else:
-                while future is not None and future < i:
-                    future = next(people, None)
-                
-                if prev is None:
-                    left_distance = float("inf")
-                else:
-                    left_distance = i - prev
-                
-                if future is None:
-                    right_distance = float("inf")
-                else:
-                    right_distance = future - i
-                
-                ans = max(ans, min(left_distance, right_distance))
+                left_dist.append(i - closest_1_idx)
+
+        closest_1_idx = float('inf')
+        right_dist = [0]*len(seats)
+        for i in range(len(seats)-1, -1, -1):
+            if seats[i] == 1:
+                right_dist.append(0)
+                closest_1_idx = i
+            else:
+                right_dist[i] = closest_1_idx - i
+        max_dist = 0
+        for i in range(len(seats)):
+            max_dist = max(max_dist, min(left_dist[i], right_dist[i]))
         
-        return ans
-                    
-                    
-                    
-                    
+        return max_dist
+
