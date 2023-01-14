040527fe956c0feffec0d84a13982d873634ff23
Arushi Sharma <arushi.net@outlook.com>
2023-01-14 14:33:33 +0000

    [LeetCode Sync] Runtime - 47 ms (5.36%), Memory - 15.9 MB (100.00%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -1,15 +1,23 @@
-import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+
+        s = "".join(s.lower().split(" "))
+        s = "".join([i for i in s if i.isalpha() or i.isdigit()])
         
-        if not s : return True
-        s = s.lower()
-        s = re.sub("[^a-z0-9]", "", s)
+        if not s or not s.strip():
+            return True
         i = 0
         j = len(s) - 1
+        
         while i < j:
             if s[i] != s[j]:
                 return False
+            
             i += 1
             j -= 1
-        return True
+        # print(i, j)
+        if i >= j:
+            return True
+
+        return False
+
