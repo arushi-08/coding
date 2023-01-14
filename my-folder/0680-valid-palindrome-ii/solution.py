7b0eeef2fce7a4534c47ecc759aefb32fe04c652
Arushi Sharma <arushi.net@outlook.com>
2023-01-14 16:30:59 +0000

    [LeetCode Sync] Runtime - 160 ms (5.14%), Memory - 14.5 MB (100.00%)

--- my-folder/0680-valid-palindrome-ii/solution.py
+++ my-folder/0680-valid-palindrome-ii/solution.py
@@ -1,18 +1,40 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        
-        strike_one = 0
-        def verify(s, i, j, strike_one):
-            
-            while i < j:
-                if s[i] == s[j]:
-                    i += 1
-                    j -= 1
-                else:
-                    if strike_one:
-                        return False
-                    strike_one = 1
-                    return verify(s, i+1, j, strike_one) or verify(s, i, j-1, strike_one)
-            return True
 
-        return verify(s, 0, len(s)-1, strike_one)
+        # s can be palindrome after deleting at most one character from it.
+        # abacca  a a - bc -  
+        i = 0
+        j = len(s) - 1
+        found_non_match_char = False
+        non_match_idx = []
+        ans = False
+        while i <= j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+                continue
+            else:
+                found_non_match_char = True
+                if j < len(s) :
+                    ans = (self.check_palindrome(s, i+1, j) | 
+                    self.check_palindrome(s, i, j-1))
+                break
+
+        if ans: return True
+
+        if not ans and not found_non_match_char: return True
+
+        return False
+
+    def check_palindrome(self, s, i, j):
+        print(i,j,s[i:j+1])
+        while i<j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
+
+        
+
