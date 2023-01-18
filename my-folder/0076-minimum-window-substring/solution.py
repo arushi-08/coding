e58b8814e6ed277ba22fa4c8c2f9375585d3fea6
Arushi Sharma <arushi.net@outlook.com>
2023-01-18 16:09:26 +0000

    [LeetCode Sync] Runtime - 302 ms (16.63%), Memory - 14.7 MB (100.00%)

--- my-folder/0076-minimum-window-substring/solution.py
+++ my-folder/0076-minimum-window-substring/solution.py
@@ -1,52 +1,51 @@
-from collections import Counter, defaultdict
+from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # find min substring in s that has all chars of t
-        # use freq dictionary of t as window
-        
-        # keep expanding window by moving right pointer.
-        # when window has all desired characters (all T chars), 
-        # contract left pointer
-        # save smallest window
-        
-        
-        t_freq_dict = Counter(t)
-        left_idx = 0
-        right_idx = 0
-        window = defaultdict(int)
-        maxstring = ""
-        desired_chars_length = 0 
-        
-        while right_idx < len(s):
-            
-            # desired_chars_length = 0
-            # keep expanding window by moving right pointer.
-            new_char = s[right_idx]
-            window[new_char] += 1
-            
-            if new_char in t_freq_dict and t_freq_dict[new_char] == window[new_char]:
-                desired_chars_length += 1
-            
-            # when window has all desired characters
-            if desired_chars_length >= len(t_freq_dict):
-                #contract
-                old_char = s[left_idx]
-                while old_char not in t_freq_dict or window[old_char] > t_freq_dict[old_char]:
-                    window[old_char] -= 1
-                    
-                    if old_char in t_freq_dict and window[old_char] < t_freq_dict[old_char]:
-                        desired_chars_length -= 1
-                    left_idx += 1
-                    old_char = s[left_idx]
-                    
-                currstring = s[left_idx:right_idx+1]
+        # min window substring of s so that every char in t
+        # including dups is included in window
+        # s = "ADOBECODEBANC", t = "ABC"
+        #       first get all chars between chars of t
+        #      iiiiii, got all chars of t, store local_ans = adobec
+        #            iiii, found b another char of t, store local_ans=adobecodeb, 
+        #                i, found a another char of t, left_ptr == a, move left_ptr till you get b,(but b is already in window, move left_ptr till next char of t), local_answer = codeba,
+        #                 ii, found c another char of t, left_ptr == c, move left_ptr till you get b, local_answer = banc
+        i = 0
+        j = 0
+        freq_t = Counter(t)
+        window = {}
+        t_counter = 0
+        local_answer = s
+        global_answer = s
+        while j < len(s):
+            if s[j] not in freq_t and not window:
+                i += 1
+                pass
+            elif s[j] not in freq_t:
+                window[s[j]] = window.get(s[j], 0) + 1
+            if s[j] in freq_t:
+                window[s[j]] = window.get(s[j], 0) + 1
+                if window[s[j]] <= freq_t[s[j]]:
+                    t_counter += 1
+                # write condn when window doesn't have all freq_t 
+                # chars or not all duplicates in freq_t
+                # if t_counter != len(t):
+                if t_counter == len(t):
+                    local_answer = s[i:j+1]
+                if s[i] == s[j] and window[s[j]] > freq_t[s[j]]:
+                    window[s[i]] -= 1
+                    i += 1
+                    while (
+                        s[i] not in freq_t 
+                        or window[s[i]] > freq_t[s[i]]
+                        ):
+                        window[s[i]] -= 1
+                        i += 1
+                        local_answer = s[i:j+1]
+                        
+            j += 1
+            if t_counter == len(t):
+                global_answer = min((local_answer, global_answer), key=len)
 
-                if not maxstring:
-                    maxstring = currstring
-                maxstring = min(maxstring, currstring, key=len)
-            
-
-            right_idx += 1
-            
-        return maxstring
-            
+        if global_answer == s and t_counter != len(t): return ''
+        return global_answer
+                
