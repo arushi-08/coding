f84415699e0e949937025c8591161d77771dd64f
Arushi Sharma <arushi.net@outlook.com>
2023-01-27 04:46:32 +0000

    [LeetCode Sync] Runtime - 58 ms (8.59%), Memory - 13.9 MB (100.00%)

--- my-folder/0088-merge-sorted-array/solution.py
+++ my-folder/0088-merge-sorted-array/solution.py
@@ -1,17 +1,28 @@
 class Solution:
-    def merge(self, A: List[int], m: int, B: List[int], n: int) -> None:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+        nums1 < nums2 p1 += 1
+        else store nums2 value in nums1 and shift nums1 values by 1  p2 += 1
+
         """
-        a = m - 1
-        b = n - 1
-        write_idx = len(A) - 1
-        
-        while b >= 0:
-            if a >= 0 and A[a] > B[b]:
-                A[write_idx] = A[a]
-                a -= 1
+        p1, p2 = 0, 0
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] <= nums2[p2]:
+                p1 += 1
             else:
-                A[write_idx] = B[b]
-                b -= 1
-            write_idx -= 1
+                i = len(nums1)-1
+                while i > p1:
+                    nums1[i], nums1[i-1] = nums1[i-1], nums1[i]
+                    i -= 1
+                nums1[i] = nums2[p2]
+                p2 += 1
+        p1 = len(nums1)-(len(nums2)-p2)
+        if p1 < 0: p1 = 0
+        print(p1, p2)
+        while p2 < len(nums2):
+            nums1[p1] = nums2[p2]
+            p2 += 1
+            p1 += 1
+        return nums1
+
