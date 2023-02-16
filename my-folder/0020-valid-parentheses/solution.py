1163327deecd657fe4889cd85bda415d94bcda8e
Arushi Sharma <arushi.net@outlook.com>
2023-02-16 20:30:21 +0000

    [LeetCode Sync] Runtime - 30 ms (9.51%), Memory - 13.9 MB (100.00%)

--- my-folder/0020-valid-parentheses/solution.py
+++ my-folder/0020-valid-parentheses/solution.py
@@ -1,26 +1,18 @@
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        
-#         use stack -> list
-        dict1 = {')':'(', '}':'{', ']': '['}
         stack = []
-        for i in s:
-            # s = "()[]{}"
-            if i in dict1:
+        for i in range(len(s)):
+            if s[i] in ['(','{','[']:
+                stack.append(s[i])
+            else:
                 if not stack:
                     return False
-                print(1,i)
-                if dict1[i] in stack[-1]:
-                    stack.pop(-1)
-                else:
-                    return False
-            else:
-                stack.append(i)
-            
-        if stack:
-            return False
-        return True
+                if stack[-1]+s[i] in ['{}','()','[]']:
+                    stack.pop()
+                    continue
+                return False
         
-        
+        if not stack:
+            return True
+        return False
+                
