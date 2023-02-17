9168ea56ffe242c87b30472d49c01929d98c537e
Arushi Sharma <arushi.net@outlook.com>
2023-02-17 21:31:03 +0000

    [LeetCode Sync] Runtime - 133 ms (6.82%), Memory - 14.1 MB (100.00%)

--- my-folder/0733-flood-fill/solution.py
+++ my-folder/0733-flood-fill/solution.py
@@ -1,33 +1,42 @@
 from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
-        return self.bfs(image, sr, sc, image[sr][sc], color)
-    
-    def bfs(self, image, sr, sc, orig_color, color):
-        
-        q = deque()
-        q.append((sr, sc))
-        rows = [1, 0, -1, 0]
-        cols = [0, 1, 0, -1]
-        visited = [[False] * len(image[0]) for i in range(len(image))]
-        while q:
-            curr_row, curr_col = q.popleft()
-            image[curr_row][curr_col] = color
-            for i in range(4):
-                if self.isSafe(image, 
-                               curr_row + rows[i], 
-                               curr_col + cols[i], 
-                               orig_color,
-                               visited
-                              ):
-                    q.append((curr_row + rows[i], curr_col + cols[i]))
-                    visited[curr_row + rows[i]][curr_col + cols[i]] = True
-        
+#       bfs
+        visited = [[False]*len(image[0]) for i in range(len(image))]
+        for i in range(len(image)):
+            for j in range(len(image[0])):
+                if not visited[i][j]:
+                    self.bfs(image, sr, sc, color, visited)
         return image
     
-    def isSafe(self, image, row, col, orig_color, visited):
-        return 0 <= row and row < len(image) and 0 <= col and col < len(image[0]) and image[row][col] == orig_color and not visited[row][col]
-                    
-            
-        
+    def bfs(self, image, sr, sc, color, visited):
+
+        queue = deque()
+        queue.append((sr, sc))
+        rows = [-1, 0, 1, 0]
+        cols = [ 0, 1, 0, -1]
+        og_color = image[sr][sc]
+        while queue:
+            currr, currc = queue.popleft()
+            image[currr][currc] = color
+            visited[currr][currc] = True
+            for i in range(len(rows)):
+                if self.isSafe(
+                    image, 
+                    currr+rows[i], 
+                    currc+cols[i],
+                    og_color,
+                    visited
+                    ):
+                    queue.append((currr+rows[i], currc+cols[i]))
+
+    def isSafe(self, image, x, y, og_color, visited):
+        # 2, 0
+        return (x >= 0 
+        and x < len(image) 
+        and y >= 0 
+        and y < len(image[0])
+        and image[x][y] == og_color
+        and not visited[x][y]
+        )
+
