e0dc66858c5f58b01ce31c2fc8e4f2b01bfbf891
Arushi Sharma <arushi.net@outlook.com>
2023-02-17 21:04:53 +0000

    [LeetCode Sync] Runtime - 44 ms (5.72%), Memory - 14.5 MB (100.00%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,22 +1,5 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        
-        dict1 = {}
-        for i in s:
-            dict1[i] = dict1.get(i, 0) + 1
-        
-        
-        for j in t:
-            if j in dict1.keys() and dict1[j] > 0:
-                dict1[j] -= 1
-                if dict1[j] == 0:
-                    dict1.pop(j)
-                
-            else:
-                return False
-        
-        if dict1:
-            return False
-        
-        return True
+        return Counter(s) == Counter(t)
+
