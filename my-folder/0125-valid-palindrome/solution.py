20216bda039a48a7d89fb20850c6b9c0965232fa
Arushi Sharma <arushi.net@outlook.com>
2023-02-17 20:19:09 +0000

    [LeetCode Sync] Runtime - 41 ms (5.36%), Memory - 15.4 MB (100.00%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -1,23 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        s = "".join(s.lower().split(" "))
-        s = "".join([i for i in s if i.isalpha() or i.isdigit()])
         
-        if not s or not s.strip():
-            return True
-        i = 0
-        j = len(s) - 1
+        s = [i for i in s.lower() if i.isalpha() or i.isdigit()]
         
-        while i < j:
-            if s[i] != s[j]:
+        end_i = len(s)-1
+        for i in range(len(s)):
+            if s[i] != s[end_i]:
                 return False
-            
-            i += 1
-            j -= 1
-        # print(i, j)
-        if i >= j:
-            return True
-
-        return False
-
+            end_i-=1
+        return True
