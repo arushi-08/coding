727f27cd25d0ec2713526a0e591c0e5e3710d6e0
Arushi Sharma <arushi.net@outlook.com>
2023-02-20 23:34:27 +0000

    [LeetCode Sync] Runtime - 169 ms (5.00%), Memory - 15.6 MB (100.00%)

--- my-folder/0169-majority-element/solution.py
+++ my-folder/0169-majority-element/solution.py
@@ -1,18 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        #               3 ,2 , 3   2, 2, 1, 1, 1, 2, 2
-        # counter       1  1.  1.  1, 2, 1, 1, 2, 1, 1
-        # i             3  2   3.  2, 2, 2, 1, 1, 1, 2
-
+        
+        maj = nums[0]
         counter = 1
-        memory = nums[0]
         for i in range(1, len(nums)):
-            if memory == nums[i]:
+            if nums[i] == maj:
                 counter += 1
             else:
-                if counter == 1:
-                    memory = nums[i]
-                else:
-                    counter -= 1
+                counter -= 1
+                if counter == 0:
+                    maj = nums[i]
+                    counter += 1
         
-        return memory
+        return maj
