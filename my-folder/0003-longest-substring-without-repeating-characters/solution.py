f2ae29e95fd5020b8345ca3a5e92122a4d613b8f
Arushi Sharma <arushi.net@outlook.com>
2023-03-06 00:47:05 +0000

    [LeetCode Sync] Runtime - 70 ms (15.36%), Memory - 14.1 MB (100.00%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,34 +1,34 @@
-from collections import Counter
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        start idx, end idx
+        if s[end] not in set -> end += 1
+        else: store len(s[start:end]) and start += 1 end += 1
+        """
+        dist = 0
+        chars = set()
+        start = 0
+        for end in range(len(s)):
+            if s[end] not in chars:
+                chars.add(s[end])
+                dist = max(dist, len(s[start:end+1]))
+                # print(s[start:end], s[end], chars)
+            elif s[start] == s[end]:
+                start += 1
+            else:
+                # print(s[start:end], s[end], chars)
+                # dist = max(dist, len(s[start:end]))
+                # # 2 cases either s[end] != s[start] -> start += 1
+
+                # if s[end] not in s[start+1:end]:
+                while s[start] != s[end]:
+                    chars.remove(s[start])
+                    start += 1
+                if s[start] == s[end]:
+                    start += 1
+            end += 1
+                
+        if not dist:
+            dist = len(s[start:])
+        return dist
         
-        # find length of longest substring without repeat
-        # abcabcbb - abc - 3
-        # iiii 
-        # store length of abc, while left ptr reaches repeating char 1st occurence, dec local max.
-        # decrease local max by 1, add 1
-        # store length of bca if > abc, while left ptr reaches repeating char 1st occurence, dec local max. dec local max by 1, add 1
-        # 
-        window = {}
-        i = 0
-        j = 0
-        local_max = 0
-        global_max = 0
-        while j < len(s):
-            if s[j] in window:
-                global_max = max(global_max, local_max)
-                while s[j] in window:
-                    del window[s[i]]
-                    i += 1
-                    local_max -= 1
-
-            window[s[j]] = 1
-            j += 1
-            local_max += 1
-        
-        return max(local_max, global_max)
-
-
-
-
-
