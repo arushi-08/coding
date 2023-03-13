191f41c7ef418aefcb3c389ee8094b5ff6afe8a4
Arushi Sharma <arushi.net@outlook.com>
2023-03-13 19:54:49 +0000

    [LeetCode Sync] Runtime - 84 ms (6.00%), Memory - 15.5 MB (100.00%)

--- my-folder/0207-course-schedule/solution.py
+++ my-folder/0207-course-schedule/solution.py
@@ -1,33 +1,36 @@
 from collections import deque
 class Solution:
-    def count_parents(self, prereq):
-        map_dict = {}
-        for i in range(len(prereq)):
-            map_dict[prereq[i][0]] = map_dict.get(prereq[i][0], 0) + 1
-        
-        return map_dict
-            
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # topological sort
+        # first note all the courses that don't need a prereq course
+        # then start with one of these and complete courses those preerqs are done
+        prereq_courses = {}
+        for i in range(numCourses):
+            prereq_courses[i] = 0
+        for course_list in prerequisites:
+            prereq_courses[course_list[0]] += 1
         
-        map_dict = self.count_parents(prerequisites)
+        prereq_courses_inverse = {}
+        for i in range(numCourses):
+            prereq_courses_inverse[i] = []
+        for course_list in prerequisites:
+            prereq_courses_inverse[course_list[1]].append(course_list[0])
         
-        queue = deque()
-        for course_num in range(numCourses):
-            if course_num not in map_dict: queue.append(course_num)
-        top_sort = []
-        
-        while len(queue):
-            curr = queue.popleft()
-            top_sort.append(curr)
-            for i in range(len(prerequisites)):
-                if prerequisites[i][1] == curr:
-                    map_dict[prerequisites[i][0]] -= 1
-                    if map_dict[prerequisites[i][0]] == 0: 
-                        queue.append(prerequisites[i][0])
-    
-        if len(top_sort) == numCourses:
+        no_prereq_required = deque()
+        for key, val in prereq_courses.items():
+            if not val:
+                no_prereq_required.append(key)
+        done = set()
+
+        while len(no_prereq_required):
+            curr = no_prereq_required.popleft()
+            done.add(curr)
+            for course in prereq_courses_inverse[curr]:
+                prereq_courses[course] -= 1
+                if course not in done and prereq_courses[course] <= 0:
+                    done.add(course)
+                    no_prereq_required.append(course)
+                    
+        if len(done) == numCourses:
             return True
         return False
-                
-        
-        
