791ece2cfddc011ee1481aa3960268bbaa27f76d
Arushi Sharma <arushi.net@outlook.com>
2023-03-17 23:59:01 +0000

    [LeetCode Sync] Runtime - 39 ms (5.90%), Memory - 16.5 MB (100.00%)

--- my-folder/0098-validate-binary-search-tree/solution.py
+++ my-folder/0098-validate-binary-search-tree/solution.py
@@ -4,27 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import math
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def validate(node, low=-math.inf, high=math.inf):
+            if not node:
+                return True
+            if node.val <= low or node.val >= high:
+                return False
+            
+            return validate(node.right, node.val, high) and validate(node.left, low, node.val)
         
-        if not root:
-            return True
-        
-        return (
-            self.helper(root.left, -float('inf'), root.val) 
-         & self.helper(root.right, root.val, float('inf'))
-        )
-    
-    def helper(self, root, left_limit, right_limit):
-        if not root:
-            return True
-        
-        ans = (self.helper(root.left, left_limit, root.val) 
-         & self.helper(root.right, root.val, right_limit))
-        
-        
-        if left_limit < root.val < right_limit:
-            return ans & True
-        return ans & False
+        return validate(root)
             
             
