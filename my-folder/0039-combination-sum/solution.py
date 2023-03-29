0e00f120d0cd4ed8cbbf445f975aa9a34f7e6864
Arushi Sharma <arushi.net@outlook.com>
2023-03-29 15:31:09 +0000

    [LeetCode Sync] Runtime - 67 ms (9.28%), Memory - 14 MB (100.00%)

--- my-folder/0039-combination-sum/solution.py
+++ my-folder/0039-combination-sum/solution.py
@@ -1,23 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        results = []
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                results.append(list(comb))
+                return
+            if remain < 0:
+                return
+            for i in range(start, len(candidates)):
+                comb.append(candidates[i])
+                backtrack(remain - candidates[i], comb, i)
+                comb.pop()
         
-        res = []
-        curr = []
-        if target:
-            self.helper(candidates, target, res, curr)
-            # res = list(set(tuple(sorted(sub)) for sub in res))
-        return res
-    
-    def helper(self, candidates, target, res, curr):
-        
-        if target == 0:
-            res.append(curr.copy())
-            return 
-        
-        if target < 0: return
-        
-        for i in range(len(candidates)):
-            curr.append(candidates[i])
-            self.helper(candidates[i:], target - candidates[i], res, curr)
-            curr.pop()
-        
+        backtrack(target, [], 0)
+        return results
+
