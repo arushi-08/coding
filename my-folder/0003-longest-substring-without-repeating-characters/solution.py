17fa6bf0932ecbc35dc237db2a27b1e3d2bcd65d
Arushi Sharma <arushi.net@outlook.com>
2023-05-16 20:47:45 +0000

    [LeetCode Sync] Runtime - 104 ms (14.22%), Memory - 16.4 MB (99.94%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,34 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        """
-        start idx, end idx
-        if s[end] not in set -> end += 1
-        else: store len(s[start:end]) and start += 1 end += 1
-        """
-        dist = 0
-        chars = set()
-        start = 0
-        for end in range(len(s)):
-            if s[end] not in chars:
-                chars.add(s[end])
-                dist = max(dist, len(s[start:end+1]))
-                # print(s[start:end], s[end], chars)
-            elif s[start] == s[end]:
-                start += 1
-            else:
-                # print(s[start:end], s[end], chars)
-                # dist = max(dist, len(s[start:end]))
-                # # 2 cases either s[end] != s[start] -> start += 1
-
-                # if s[end] not in s[start+1:end]:
-                while s[start] != s[end]:
-                    chars.remove(s[start])
+        hmap = {}
+        start, end, ans = 0, 0, 0
+        while end < len(s):
+            if s[end] in hmap:
+                ans = max(ans, end - start)
+                while start < len(s) and hmap[s[end]] > 0:
+                    hmap[s[start]] -= 1
                     start += 1
-                if s[start] == s[end]:
-                    start += 1
+            hmap[s[end]] = 1
             end += 1
-                
-        if not dist:
-            dist = len(s[start:])
-        return dist
         
+        return max(ans, end-start)
