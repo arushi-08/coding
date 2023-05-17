8c68f290a6a960c68715447709c149acb090ab16
Arushi Sharma <arushi.net@outlook.com>
2023-05-17 20:23:54 +0000

    [LeetCode Sync] Runtime - 142 ms (37.09%), Memory - 16.4 MB (99.00%)

--- my-folder/0647-palindromic-substrings/solution.py
+++ my-folder/0647-palindromic-substrings/solution.py
@@ -1,35 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         
-        return self.helper(s)
-    
-    def helper(self ,s):
-        if not s: return 0
-        if len(s) == 1: return 1
-        
-        dp = [[0]*len(s) for _ in range(len(s))]
-        
-
+        count = 0
         for i in range(len(s)):
-            for j in range(len(s)):
-                
-                if i > j:
-                    continue
-                    
-                if i == j:
-                    dp[i][j] = 1
-                    
-                elif (i == j + 1 or j == i + 1) and s[i] == s[j]:
-                    dp[i][j] = 1
-        # print(dp)
-        for i in range(len(s)-1,-1,-1):
-            for j in range(len(s)):
-                if i > j:
-                    continue
-                # if i == 0 and j == 4:
-                #     print(i + 1 < len(s) ,  j - 1 >= 0 ,  dp[i+1][j-1] != 0 , s[i] == s[j])
-                if i + 1 < len(s) and j - 1 >= 0 and dp[i+1][j-1] != 0 and s[i] == s[j]:
-                    dp[i][j] = 1
-        # print(dp)
-        return sum(map(sum, dp))
-        
+            count += self.countPalindrome(s, i, i)
+            count += self.countPalindrome(s, i, i+1)
+        return count
+
+    def countPalindrome(self, s, l, r):
+        count = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+            count += 1
+            
+        return count
+
+
