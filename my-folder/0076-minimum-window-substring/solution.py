f29a3ad8eef8d1dac4deb449dce7c76c67d206f5
Arushi Sharma <arushi.net@outlook.com>
2023-05-17 18:33:47 +0000

    [LeetCode Sync] Runtime - 155 ms (26.41%), Memory - 17.1 MB (70.38%)

--- my-folder/0076-minimum-window-substring/solution.py
+++ my-folder/0076-minimum-window-substring/solution.py
@@ -1,51 +1,43 @@
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # min window substring of s so that every char in t
-        # including dups is included in window
-        # s = "ADOBECODEBANC", t = "ABC"
-        #       first get all chars between chars of t
-        #      iiiiii, got all chars of t, store local_ans = adobec
-        #            iiii, found b another char of t, store local_ans=adobecodeb, 
-        #                i, found a another char of t, left_ptr == a, move left_ptr till you get b,(but b is already in window, move left_ptr till next char of t), local_answer = codeba,
-        #                 ii, found c another char of t, left_ptr == c, move left_ptr till you get b, local_answer = banc
-        i = 0
-        j = 0
-        freq_t = Counter(t)
-        window = {}
-        t_counter = 0
-        local_answer = s
-        global_answer = s
-        while j < len(s):
-            if s[j] not in freq_t and not window:
-                i += 1
-                pass
-            elif s[j] not in freq_t:
-                window[s[j]] = window.get(s[j], 0) + 1
-            if s[j] in freq_t:
-                window[s[j]] = window.get(s[j], 0) + 1
-                if window[s[j]] <= freq_t[s[j]]:
-                    t_counter += 1
-                # write condn when window doesn't have all freq_t 
-                # chars or not all duplicates in freq_t
-                # if t_counter != len(t):
-                if t_counter == len(t):
-                    local_answer = s[i:j+1]
-                if s[i] == s[j] and window[s[j]] > freq_t[s[j]]:
-                    window[s[i]] -= 1
-                    i += 1
-                    while (
-                        s[i] not in freq_t 
-                        or window[s[i]] > freq_t[s[i]]
-                        ):
-                        window[s[i]] -= 1
-                        i += 1
-                        local_answer = s[i:j+1]
-                        
-            j += 1
-            if t_counter == len(t):
-                global_answer = min((local_answer, global_answer), key=len)
-
-        if global_answer == s and t_counter != len(t): return ''
-        return global_answer
-                
+        # 2 pointers + hmap
+        # hmap store t freq + t_len store len(t)
+        # move end to len(s)
+        # if s[end] in hmap and s[end] not in smap or smap[s[end]] < hmap[s[end]]: t_len -= 1, store s[end] in smap
+        # while t_len == 0: move start, store minlength 
+        # remove s[start], if s[start] in smap, remove it
+        if len(t) > len(s) : return ''
+        start, end = 0, 0
+        tmap = Counter(t)
+        smap = {}
+        minwindow, t_len = len(s)+1, len(t)
+        start_ans, end_ans = -1,-1
+        while end < len(s):
+            if s[end] in tmap:
+                if s[end] not in smap:
+                    smap[s[end]] = 1
+                    t_len -= 1
+                elif smap[s[end]] < tmap[s[end]]:
+                    smap[s[end]] += 1
+                    t_len -= 1
+                else:
+                    smap[s[end]] += 1
+            else:
+                smap[s[end]] = smap.get(s[end], 0) + 1
+            # print('outside',t_len, s[start:end+1], start)
+            while t_len == 0:
+                if end-start+1 <= minwindow:
+                    start_ans = start
+                    end_ans = end
+                    minwindow = end-start+1 
+                smap[s[start]] -= 1
+                # print("check", s[start], smap[s[start]], tmap[s[start]])
+                if s[start] in tmap and smap[s[start]] < tmap[s[start]]:
+                    # print(t_len, s[start:end+1], start)
+                    t_len += 1
+                start += 1
+            end += 1
+        if start_ans == -1:
+            return ''
+        return s[start_ans: end_ans + 1] 
