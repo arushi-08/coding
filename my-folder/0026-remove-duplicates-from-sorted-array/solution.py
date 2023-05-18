17da621a8d38feecf0ca984c68a44d9e037ea101
Arushi Sharma <arushi.net@outlook.com>
2023-05-18 22:21:58 +0000

    [LeetCode Sync] Runtime - 94 ms (5.08%), Memory - 18.1 MB (17.73%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.py
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,22 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-
-        ptr1 = 0
+        # 2 pointers
+        # ptr1 and ptr2
+        # if ptr1 == next pos, while next pos is same, next pos += 1
+        # make ptr1 = next pos element, ptr1 += 1
+        ptr1 = 1
         ptr2 = 1
         while ptr2 < len(nums):
-            if nums[ptr1] == nums[ptr2]:
-                ptr2 += 1
-                continue
-            if nums[ptr1] != nums[ptr2]:
-                if ptr1 + 1 == ptr2:
-                    ptr1 += 1
-                    ptr2 += 1
-                    continue
-
-                if ptr1 + 1 < len(nums):
-                    nums[ptr1 + 1] = nums[ptr2]
-                    ptr1 += 1
-                ptr2 += 1
-        
-        return ptr1 + 1
-            
+            if nums[ptr2] != nums[ptr2-1]:
+                nums[ptr1] = nums[ptr2]
+                ptr1 += 1
+            ptr2 += 1
+        return ptr1 
