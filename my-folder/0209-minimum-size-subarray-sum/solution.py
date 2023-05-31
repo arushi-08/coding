3b5777ab3b7cf62a7977a2fbe1463261cebc0b2c
Arushi Sharma <arushi.net@outlook.com>
2023-05-31 03:19:25 +0000

    [LeetCode Sync] Runtime - 284 ms (5.02%), Memory - 29.1 MB (65.33%)

--- my-folder/0209-minimum-size-subarray-sum/solution.py
+++ my-folder/0209-minimum-size-subarray-sum/solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        # 2 pointers + hmap
-        # end move to len(nums)
-        # if subarrsum >= target: save sublen sum, increase start counter
-
-        subarr_len = len(nums) + 1
+        ans = len(nums)+1
+        currsum = 0
         start, end = 0, 0
-        subarr_sum = 0
         while end < len(nums):
-            subarr_sum += nums[end]
-            while subarr_sum >= target:
-                subarr_len = min(subarr_len, end-start+1)
-                subarr_sum -= nums[start]
+            currsum += nums[end]
+            while start <= end and currsum >= target:
+                ans = min(ans, end - start + 1)
+                currsum -= nums[start]
                 start += 1
             end += 1
-        
-        if subarr_len == len(nums) + 1:
-            return 0
-        return subarr_len
+
+        if ans == len(nums)+1: return 0
+        return ans
+
