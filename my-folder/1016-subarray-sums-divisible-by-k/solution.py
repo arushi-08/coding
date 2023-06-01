d184c6cd3b8aa7329a8274d37754e70f4d05d842
Arushi Sharma <arushi.net@outlook.com>
2023-06-01 21:13:49 +0000

    [LeetCode Sync] Runtime - 297 ms (5.11%), Memory - 20.7 MB (63.36%)

--- my-folder/1016-subarray-sums-divisible-by-k/solution.py
+++ my-folder/1016-subarray-sums-divisible-by-k/solution.py
@@ -1,23 +1,15 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-
-        # calculate psum array
-        # calc diff between immediate psums-store in dict=
-        # {remainder/diff:count}
-        # apply nC2 formula
-        psum = [0]*len(nums)
-        psum[0] = nums[0]
-        for i in range(1, len(nums)):
-            psum[i] = psum[i-1]+nums[i]
-        dict1 = {}
-        for i in range(len(psum)):
-            dict1[psum[i]%k] = dict1.get(psum[i]%k, 0) + 1
+        # (p[j] - p[i])%k==0
+        psum = 0
+        hmap = {0:1}
         count = 0
-        for key, val in dict1.items():
-            if key == 0:
-                count += val + (val*(val-1)//2)
-            else:
-                count += (val*(val-1)//2)
+        for i in range(len(nums)):
+            psum += nums[i]
+            psum %= k
+            if psum in hmap:
+                count += hmap[psum]
+            hmap[psum] = hmap.get(psum, 0) + 1
+    
         return count
-
-
+        
