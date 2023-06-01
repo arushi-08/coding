9d07f9f2dd66d468b4109b7ac5556cde92077656
Arushi Sharma <arushi.net@outlook.com>
2023-06-01 04:04:45 +0000

    [LeetCode Sync] Runtime - 312 ms (5.23%), Memory - 19 MB (99.84%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,24 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-
-        # total no of subarrays whose sum = k
-        # 1, 1, 1 
-        # 1, 2 == k counter += 1
-        # 1, 2, 3 > k remove left_ptr 3-1 == k counter += 1
-        # -1, -1, 1
-        # -1, -2, -1
-        # Determine # sum−k has occurred already, since it will determine the number of times a subarray with sum k has occurred up to the current index. 
-        # Every time we encounter a new sum, we make a new entry or increment sum's value in the hashmap corresponding to that sum.
-        # We increment the count by the same amount.
-        answer = 0
-        j = 0
-        hmap = {0:1}
+        # psum
+        # p[j] - k = p[i] - store in map
+        hmap ={0:1}
+        ans = 0
         psum = 0
-        while j < len(nums):
+        for j in range(len(nums)):
             psum += nums[j]
-            if psum - k in hmap:
-                answer += hmap[psum-k]
+            if psum-k in hmap:
+                ans += hmap[psum-k]
             hmap[psum] = hmap.get(psum, 0) + 1
-            j += 1
+        return ans
 
-        return answer
