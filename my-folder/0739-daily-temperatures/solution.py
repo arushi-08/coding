02d3872bebc19f7afd606d65c84f10684ed508e9
Arushi Sharma <arushi.net@outlook.com>
2023-06-02 18:12:02 +0000

    [LeetCode Sync] Runtime - 1337 ms (5.01%), Memory - 31.6 MB (37.69%)

--- my-folder/0739-daily-temperatures/solution.py
+++ my-folder/0739-daily-temperatures/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        ans = [0] * n
-        hottest = 0
-        for i in range(n-1,-1,-1):
-            cur_temp = temperatures[i]
-            if cur_temp >= hottest:
-                hottest = cur_temp
-                continue
-            days = 1
-            while temperatures[i + days] <= cur_temp:
-                days += ans[i + days]
-            ans[i] = days
+        
+        # use stack to get days of next warmer temperature
+        # while current > stack[top] - pop top - update dist on its idx
+        # push current into stack
+        if not temperatures: return [0]
+        stack = [(0, temperatures[0])]
+        ans = [0]*len(temperatures)
+        for i in range(1, len(temperatures)):
+            current = temperatures[i]
+            while stack and current > stack[-1][1]:
+                j, _ = stack.pop() 
+                ans[j] = i-j
+            stack.append((i, current))
+        
         return ans
