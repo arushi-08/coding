5f902ae7c53c892632cb23758b49928d4acfad6b
Arushi Sharma <arushi.net@outlook.com>
2023-06-02 17:56:05 +0000

    [LeetCode Sync] Runtime - 383 ms (5.46%), Memory - 37.9 MB (5.10%)

--- my-folder/0041-first-missing-positive/solution.py
+++ my-folder/0041-first-missing-positive/solution.py
@@ -1,29 +1,13 @@
-import random
+from collections import Counter
 class Solution:
-    def partition(self, nums, left, right):
-        pivot = -1
-        for j in range(len(nums)):
-            if nums[j] > 0:
-                pivot += 1
-                nums[j], nums[pivot] = nums[pivot], nums[j]
-        return pivot
-        
-        
     def firstMissingPositive(self, nums: List[int]) -> int:
-#         use quick sort algo
-#         pivot = 0
-#         +ve nos will be right of pivot sort nos only in right of pivot
-        lpi = self.partition(nums, 0, len(nums)-1) + 1
-        for i in range(lpi):
-            k = abs(nums[i])
-            if k <= lpi:
-                if nums[k-1] >= 0:
-                    nums[k-1] = -nums[k-1]
-        # print(nums)
-        for i in range(lpi):
-            if nums[i] > 0:
-                return i + 1
-        return lpi + 1
+        dict1 = Counter(nums)
+
+        for i in range(len(nums)):
+            if i+1 not in dict1:
+                return i+1
         
+        return len(nums)+1
+            
         
-        
+
