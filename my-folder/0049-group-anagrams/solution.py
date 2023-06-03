66182898c91d48cb59248a534d43d49b1ceb96d0
Arushi Sharma <arushi.net@outlook.com>
2023-06-03 20:44:10 +0000

    [LeetCode Sync] Runtime - 166 ms (5.03%), Memory - 24.1 MB (9.23%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -1,11 +1,18 @@
-from itertools import chain
+from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        hmap = {}
+        for i in range(len(strs)):
+            # print()
+            # print(sorted(Counter(strs[i])))
+            key = tuple(sorted([(key, val) for key, val in Counter(strs[i]).items()]))
+            if key in hmap:
+                hmap[key].append(strs[i])
+            else:
+                hmap[key] = [strs[i]]
+        ans = []
         
-        dict1 = {}
-        
-        for i in strs:
-            key = tuple(sorted(i))
-            dict1[key] = dict1.get(key, []) + [i]
-        
-        return list(dict1.values())
+        for key, val in hmap.items():
+            ans.append(val)
+        return ans
