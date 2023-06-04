33b70cf413dea583f345af418085f8c1c80b5007
Arushi Sharma <arushi.net@outlook.com>
2023-06-04 05:08:12 +0000

    [LeetCode Sync] Runtime - 550 ms (40.24%), Memory - 16.5 MB (97.69%)

--- my-folder/0005-longest-palindromic-substring/solution.py
+++ my-folder/0005-longest-palindromic-substring/solution.py
@@ -1,20 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1: return s
-        n = len(s)
-        dp = [[0]*(n) for _ in range(n)]
-        max_len_pal = ''
-        for i in range(n):
-                dp[i][i] = True
-                max_len_pal = s[i]
-        for i in range(n-1, -1, -1):
-            for j in range(i+1, n):
-                if s[i] == s[j]:
-                    if j - i == 1 or dp[i+1][j-1] == True:
-                        dp[i][j] = True
-                        
-                        if len(s[i:j+1]) > len(max_len_pal):
-                            max_len_pal = s[i:j+1]
+        # start from each char as center, check if it can be center of a palindrome
+        maxlength = 0
+        def checklength(st, ed):
+            ans = ''
+            passonce = 0
+            while st >= 0 and ed < len(s) and s[st] == s[ed]:
+                st -= 1
+                ed += 1
+            return st+1, ed
+        st, ed = 0, 0
+        for i in range(len(s)):
+            l, r = checklength(i, i)
+            if r-l > (ed-st):
+                st, ed = l, r
+            l, r = checklength(i, i+1)
+            if r-l > (ed-st):
+                st, ed = l, r
+
+        return s[st:ed]
+            
         
-        return max_len_pal
-    
