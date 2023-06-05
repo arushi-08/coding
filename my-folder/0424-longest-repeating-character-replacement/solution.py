0d12d038bdb38d5d55ab592c9ed58f84a2bfee07
Arushi Sharma <arushi.net@outlook.com>
2023-06-05 04:29:47 +0000

    [LeetCode Sync] Runtime - 124 ms (13.88%), Memory - 16.3 MB (99.88%)

--- my-folder/0424-longest-repeating-character-replacement/solution.py
+++ my-folder/0424-longest-repeating-character-replacement/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        
+        st, ed = 0, 0
         hmap = {}
-        start, end = 0, 0
         max_freq = 0
         longest = 0
-        while end < len(s):
-            if s[end] not in hmap:
-                hmap[s[end]] = 1
-            else:
-                hmap[s[end]] += 1
-            max_freq = max(max_freq, hmap[s[end]])
-            while (end - start + 1) - max_freq > k:
-                longest = max(longest, end - start)
-                hmap[s[start]] -= 1
-                start += 1
-            end += 1
-        return max(longest, end-start)
+        while ed < len(s):
+            hmap[s[ed]] = hmap.get(s[ed], 0) + 1
+            max_freq = max(max_freq, hmap[s[ed]])
+            while ed - st + 1 - max_freq > k:
+                longest = max(longest, ed-st)
+                hmap[s[st]] -= 1
+                st += 1
+            ed += 1
+        return max(longest, ed-st)
+            
