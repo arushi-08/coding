02c31fc05bae446f871a986bfd3d5a0b525f02f8
Arushi Sharma <arushi.net@outlook.com>
2023-06-07 21:50:34 +0000

    [LeetCode Sync] Runtime - 48 ms (10.96%), Memory - 17.5 MB (51.74%)

--- my-folder/0112-path-sum/solution.py
+++ my-folder/0112-path-sum/solution.py
@@ -6,14 +6,11 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        if not root:
-            return False
-        
-        if not root.left and not root.right:
-            if targetSum == root.val:
-                return True
-            else:
-                return False
-        
-        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
+        if not root: return False
+        if root and not root.left and not root.right and root.val == targetSum: return True
+
+        left = self.hasPathSum(root.left, targetSum - root.val)
+        right = self.hasPathSum(root.right, targetSum - root.val)
+
+        return left | right
+
