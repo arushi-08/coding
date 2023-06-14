6c0799bf7ac000010825a9bdfb658fb8d973d367
Arushi Sharma <arushi.net@outlook.com>
2023-06-14 16:29:12 +0000

    [LeetCode Sync] Runtime - 434 ms (5.13%), Memory - 18.7 MB (98.46%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -2,45 +2,36 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        visited = [
-            [False]*len(grid[0]) for i in range(len(grid))
-            ]
-        output = 0
+        visited = [[False]*len(grid[0]) for _ in range(len(grid))]
+        ans = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if not visited[i][j] and grid[i][j]=='1':
-                    self.bfs(grid, i, j, visited, output)
-                    output += 1
-        return output
-
-    def bfs(self, grid, i, j, visited, output):
-
+                    visited[i][j] = True
+                    self.bfs(grid, i, j, visited)
+                    # print(i,j)
+                    ans += 1
+        return ans
+    
+    def bfs(self, grid, x, y, visited):
         queue = deque()
-        queue.append((i, j))
-        visited[i][j] = True
+        queue.append((x, y))
         rows = [1, 0, -1, 0]
         cols = [0, 1, 0, -1]
         while len(queue):
-            curri, currj = queue.popleft()
-            for i in range(4):
+            currx, curry = queue.popleft()
+            for k in range(len(rows)):
                 if self.isSafe(
-                    grid, curri+rows[i],
-                    currj+cols[i],visited
+                    grid, currx+rows[k], curry+cols[k], visited
                     ):
-                    queue.append(
-                        (curri+rows[i], currj+cols[i])
-                        )
-                    visited[curri+rows[i]][currj+cols[i]] = True
-
+                    visited[currx+rows[k]][curry+cols[k]] = True
+                    queue.append((currx+rows[k], curry+cols[k]))
+            # print('queue',queue)
     def isSafe(self, grid, x, y, visited):
-        return (
-            x >= 0 and 
-            x < len(grid) and 
-            y >= 0 and 
-            y < len(grid[0]) and 
-            not visited[x][y] and 
-            grid[x][y]=='1'
-            )
-
-
+        return (x>=0 and 
+        y>=0 and 
+        x<len(grid) and 
+        y<len(grid[0]) and 
+        not visited[x][y] and 
+        grid[x][y]=='1')
 
