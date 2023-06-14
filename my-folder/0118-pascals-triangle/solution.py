11cad8453f3d1af2ef43d6a7f17a198d441feb32
Arushi Sharma <arushi.net@outlook.com>
2023-06-14 19:03:02 +0000

    [LeetCode Sync] Runtime - 66 ms (11.41%), Memory - 16.5 MB (93.18%)

--- my-folder/0118-pascals-triangle/solution.py
+++ my-folder/0118-pascals-triangle/solution.py
@@ -1,25 +1,25 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
+#         1, 11 1C0 1C1, 121, 2C0 2C1 2C2 121, 3C0 3C1 3C2 3C3
+        def fact(x):
+            if x == 0 or x == 1: return 1
+            # print('here',x, fact(x-1)*x)
+            return fact(x-1)*x
+        
+        def comb(i, j):
+            # print('c',i, j, fact(i))
+            return int(fact(i)/(fact(j)*fact(i-j)))
         
         ans = []
         for i in range(numRows):
+            temp = []
             if i == 0:
                 ans.append([1])
-            else:
-                temp = [1]
-                for k in range(1, i):
-                    temp.append(int(self.factorial(i)/(self.factorial(i-k)* self.factorial(k))))
-                temp.append(1)
-                ans.append(temp)
-        
+                continue
+            for j in range(i+1):
+                temp.append(comb(i,j))
+            ans.append(temp)
+            # break
+            # break
         return ans
-    
-    def factorial(self,n):
-        
-        if n <= 1:
-            return 1
-        
-        return self.factorial(n-1) * n
-        
-        
