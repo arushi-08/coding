8d4320a58e5411cc59fe5e6301ec72d21d76ed45
Arushi Sharma <arushi.net@outlook.com>
2023-06-15 22:30:07 +0000

    [LeetCode Sync] Runtime - 109 ms (5.76%), Memory - 17.7 MB (96.02%)

--- my-folder/0207-course-schedule/solution.py
+++ my-folder/0207-course-schedule/solution.py
@@ -1,36 +1,30 @@
-from collections import deque
+from collections import defaultdict, deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # topological sort
-        # first note all the courses that don't need a prereq course
-        # then start with one of these and complete courses those preerqs are done
-        prereq_courses = {}
-        for i in range(numCourses):
-            prereq_courses[i] = 0
-        for course_list in prerequisites:
-            prereq_courses[course_list[0]] += 1
+        graph = defaultdict(list)
+        indegree = [0]*numCourses
+        queue = deque()
+        visited = set()
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            indegree[course] += 1
         
-        prereq_courses_inverse = {}
         for i in range(numCourses):
-            prereq_courses_inverse[i] = []
-        for course_list in prerequisites:
-            prereq_courses_inverse[course_list[1]].append(course_list[0])
+            if indegree[i] == 0:
+                queue.append(i)
+        nodevisited = 0
+        while len(queue):
+            prereq = queue.popleft()
+            nodevisited += 1
+            for course in graph[prereq]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
         
-        no_prereq_required = deque()
-        for key, val in prereq_courses.items():
-            if not val:
-                no_prereq_required.append(key)
-        done = set()
+        return nodevisited == numCourses
 
-        while len(no_prereq_required):
-            curr = no_prereq_required.popleft()
-            done.add(curr)
-            for course in prereq_courses_inverse[curr]:
-                prereq_courses[course] -= 1
-                if course not in done and prereq_courses[course] <= 0:
-                    done.add(course)
-                    no_prereq_required.append(course)
-                    
-        if len(done) == numCourses:
-            return True
-        return False
+        
+
+
+
+
