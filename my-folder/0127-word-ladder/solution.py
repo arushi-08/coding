f0d4c618719441650a561d6a789fe2d8862fdc51
Arushi Sharma <arushi.net@outlook.com>
2023-06-17 03:39:09 +0000

    [LeetCode Sync] Runtime - 9995 ms (5.04%), Memory - 17.4 MB (98.77%)

--- my-folder/0127-word-ladder/solution.py
+++ my-folder/0127-word-ladder/solution.py
@@ -1,44 +1,24 @@
 from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
-        if endWord not in wordList: return 0
-        
-        # try drawing a graph
-        # hit: hot
-        # hot: dot, lot
-        # dot: hot, dog, lot
-        # dog: cog, dot, log
-        # lot: dot, hot
-        
-        graph = {}
-        for word in wordList:
-            for i in range(len(beginWord)):
-                generic = word[:i] + "*" + word[i+1:]
-                graph.setdefault(generic, []).append(word)
-        
+        if not beginWord: return 0
         queue = deque()
-        
         queue.append((beginWord, 1))
+        visited = set()
+        while len(queue):
+            curr, count = queue.popleft()
+            for word in wordList:
+                if word not in visited:
+                    changes = 0
+                    for c1, c2, in zip(curr, word):
+                        if c1 != c2:
+                            changes += 1
+                    if changes > 1:
+                        continue
+                    visited.add(word)
+                    if word == endWord:
+                        return count + 1
+                    queue.append((word, count + 1))
         
-        visited = set([beginWord])
-        
-        while queue:
-            currword, moves = queue.popleft()
-            
-            if currword == endWord:
-                return moves
-            
-            for i in range(len(currword)):
-                generic = currword[:i] + "*" + currword[i+1:]
-                if generic in graph:
-                    for word in graph[generic]:
-                        if word not in visited:
-                            queue.append((word, moves+1))
-                            visited.add(word)
-
+        if curr == endWord: return count
         return 0
-            
-        
-                
-        
