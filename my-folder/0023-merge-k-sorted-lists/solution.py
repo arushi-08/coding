f8ff3d29f9b00739ca245534390e90aa807f9b52
Arushi Sharma <arushi.net@outlook.com>
2023-06-19 23:07:16 +0000

    [LeetCode Sync] Runtime - 119 ms (15.84%), Memory - 20 MB (7.85%)

--- my-folder/0023-merge-k-sorted-lists/solution.py
+++ my-folder/0023-merge-k-sorted-lists/solution.py
@@ -3,71 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class MinHeap:
-    def __init__(self):
-        self.heap_list = [0]
-        self.current_size = 0
-    
-    def insert(self, val):
-        self.heap_list.append(val)
-        self.current_size += 1
-        self.bubble_up(self.current_size)
-    
-    def bubble_up(self, idx):
-        while idx // 2 > 0: 
-            if self.heap_list[idx // 2][0] > self.heap_list[idx][0]:
-                self.heap_list[idx // 2], self.heap_list[idx] = self.heap_list[idx], self.heap_list[idx // 2]
-            else:
-                break
-            idx //= 2
-    
-    def delete(self):
-        root = self.heap_list[1]
-        self.heap_list[1] = self.heap_list[-1]
-        self.heap_list.pop()
-        self.current_size -= 1
-        self.bubble_down()
-        return root
-    
-    def bubble_down(self):
-        idx = 1
-        while (idx * 2) <= self.current_size:
-            min_child_idx = self.get_min_child_idx(idx)
-            
-            if self.heap_list[idx][0] > self.heap_list[min_child_idx][0]:
-                self.heap_list[idx], self.heap_list[min_child_idx] = self.heap_list[min_child_idx], self.heap_list[idx]
-            else:
-                break
-            idx = min_child_idx
-    
-    def get_min_child_idx(self, idx):
-        if (2*idx)+1 > self.current_size:
-            return 2*idx
-        
-        if self.heap_list[(2*idx)+1][0] > self.heap_list[(2*idx)][0]:
-            return 2*idx
-    
-        return (2*idx) + 1
-    
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        heap = MinHeap()
-        for idx, node in enumerate(lists):
-            if node:
-                heap.insert((node.val, idx))
-                lists[idx] = lists[idx].next
-        
-        result = ListNode()
-        curr = result
-        while heap.current_size > 0:
-            val, idx = heap.delete()
-            curr.next = ListNode(val)
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, 2*interval):
+                lists[i] = self.merge2Lists(lists[i], lists[i+interval])
+            interval*=2
+        return lists[0] if amount else None
+    
+    def merge2Lists(self, l1, l2):
+        head = curr = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
             curr = curr.next
-            if lists[idx]:
-                heap.insert((lists[idx].val, idx))
-                lists[idx] = lists[idx].next
+        if l1:
+            curr.next = l1
+        if l2:
+            curr.next = l2
         
-        return result.next
-        
-        
+        return head.next
+
