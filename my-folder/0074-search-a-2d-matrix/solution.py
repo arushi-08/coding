ee129408a9d2a325d9e6450ce44829336696080b
Arushi Sharma <arushi.net@outlook.com>
2023-06-24 17:09:08 +0000

    [LeetCode Sync] Runtime - 64 ms (6.16%), Memory - 17 MB (90.22%)

--- my-folder/0074-search-a-2d-matrix/solution.py
+++ my-folder/0074-search-a-2d-matrix/solution.py
@@ -1,21 +1,27 @@
 class Solution:
-    def binary_search(self, arr, target):
-        left = 0
-        right = len(arr) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if arr[mid] == target:
-                return True
-            if arr[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return False
-    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        low = 0
+        high = len(matrix) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if matrix[mid][0] == target: return True
+            if matrix[mid][0] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
         
-        for i in range(len(matrix)):
-            if target <= matrix[i][-1]:
-                return self.binary_search(matrix[i], target)
+        row = low - 1
+        if row < 0: return False
+        low = 0
+        high = len(matrix[0])-1
+        while low <= high:
+            mid = (low + high) // 2
+            if matrix[row][mid] == target: return True
+            if matrix[row][mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
         
         return False
+
