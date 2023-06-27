a9239afb404608f2112cfed9ef46195288d9b6e9
Arushi Sharma <arushi.net@outlook.com>
2023-06-27 22:11:57 +0000

    [LeetCode Sync] Runtime - 47 ms (5.27%), Memory - 16.7 MB (99.86%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,26 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        if not nums: return -1
-        left = 0
-        right = len(nums)-1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid]==target:
-                return mid
-            # if rotation is in left half
-            if nums[left] <= nums[mid]:
-                # non rotated part
-                if nums[left] <= target and target < nums[mid]:
-                    right = mid - 1
+        low = 0
+        high = len(nums)-1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target: return mid
+            if nums[mid] >= nums[low]: # left sorted
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid - 1
                 else:
-                    left = mid + 1
-            # if rotation is in second half
-            else:
-                # non rotated part
-                if nums[right] >= target and target > nums[mid]:
-                    left = mid + 1
+                    low = mid + 1
+            else: # right sorted
+                if target <= nums[high] and target > nums[mid]:
+                    low = mid + 1
                 else:
-                    right = mid - 1
-        
-        return left if left >=0 and left < len(nums) and nums[left] == target else -1
+                    high = mid - 1
+        return -1
