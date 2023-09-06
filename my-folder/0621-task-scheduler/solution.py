d231029f310025c7f92538efcd2aaf85788e6607
Arushi Sharma <arushi.net@outlook.com>
2023-09-06 03:57:08 +0000

    [LeetCode Sync] Runtime - 373 ms (6.39%), Memory - 17 MB (81.15%)

--- my-folder/0621-task-scheduler/solution.py
+++ my-folder/0621-task-scheduler/solution.py
@@ -1,23 +1,14 @@
 from collections import Counter
-from heapq import heappush, heappop
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        """create freq dict of char array => {A:3, B:3}
-           loop over freq dict keys => do A then B, then A but before A idle time
-        """
-        
-        freq_dict = Counter(tasks)
-        max_heap = []
-        for task, freq in freq_dict.items():
-            heappush(max_heap, (0, -freq, task))
-        time = 0
-        while max_heap:
-            task_execution_time = max_heap[0][0]
-            if task_execution_time <= time:
-                task_time, freq, task = heappop(max_heap)
-                if freq + 1 < 0 :
-                    heappush(max_heap, (task_time + n + 1, freq + 1,task))
-            time += 1
-        
-        return time
-            
+        busy_time = len(tasks)
+        hmap = Counter(tasks)
+        freq = list(hmap.values())
+        freq.sort()
+        max_freq = freq.pop()
+        idle_time = (max_freq - 1) * n
+
+        while freq and idle_time > 0:
+            idle_time -= min(max_freq-1, freq.pop())
+        idle_time = max(idle_time, 0)
+        return busy_time + idle_time
