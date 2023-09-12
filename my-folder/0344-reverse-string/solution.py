8d9686808e31f05f37609dcccf0f2a376a435f41
Arushi Sharma <arushi.net@outlook.com>
2023-09-12 12:38:20 +0000

    [LeetCode Sync] Runtime - 200 ms (10.46%), Memory - 20.7 MB (96.38%)

--- my-folder/0344-reverse-string/solution.py
+++ my-folder/0344-reverse-string/solution.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
         i = 0
-        j = len(s) - 1
-        while(i < j):
-            s[i], s[j] = s[j], s[i]
+        j = len(s)-1
+        while i < j:
+            s[i] , s[j] = s[j], s[i]
             i += 1
             j -= 1
-        return s
         
