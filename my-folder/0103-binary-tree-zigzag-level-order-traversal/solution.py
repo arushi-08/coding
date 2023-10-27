61cb1005cf76684478ef188f5cc659e6f096785c
Arushi Sharma <arushi.net@outlook.com>
2023-10-27 16:53:34 +0000

    [LeetCode Sync] Runtime - 40 ms (7.75%), Memory - 16.7 MB (93.05%)

--- my-folder/0103-binary-tree-zigzag-level-order-traversal/solution.py
+++ my-folder/0103-binary-tree-zigzag-level-order-traversal/solution.py
@@ -1,35 +1,31 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
         if not root: return []
         queue = deque()
-        queue.append(root)
-        ans = []
-        count = 0
-        while len(queue):
+        queue.append((root, 0))
+        res = []
+        while queue:
             l = len(queue)
             temp = []
             for _ in range(l):
-                curr = queue.popleft()
-                temp.append(curr.val)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-            
-            if count % 2 == 1:
-                tempreverse = []
-                for i in range(len(temp)-1,-1,-1):
-                    tempreverse.append(temp[i])
-                ans.append(tempreverse)
-            else:
-                ans.append(temp)
-            count += 1
-        return ans
+                node, level = queue.popleft()
+                if node:
+                    temp.append(node.val)
+                    queue.append((node.left, level+1))
+                    queue.append((node.right, level+1))
+            if temp:
+                res.append(temp)
+        for i in range(len(res)):
+            if i%2==1:
+                res[i].reverse()
+
+        return res
 
