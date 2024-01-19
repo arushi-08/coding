e5b6cd9646f2de3d284e1f663b4162f375efa050
Arushi Sharma <arushi.net@outlook.com>
2024-01-19 17:06:17 +0000

    [LeetCode Sync] Runtime - 94 ms (5.18%), Memory - 21.3 MB (38.49%)

--- my-folder/0347-top-k-frequent-elements/solution.py
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -1,26 +1,18 @@
+import heapq
 from collections import Counter
-from heapq import heapify
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        heap = []
         freq_dict = Counter(nums)
-        freq_array = [0] * (len(nums) + 1)
         for key, val in freq_dict.items():
-            if not freq_array[val]:
-                freq_array[val] = []
-            freq_array[val].append(key)
-        # print(freq_array)
-        ans = []
-        for i in range(len(freq_array)-1,-1,-1):
-            if k == 0:
-                break
-            if not freq_array[i]:
-                continue
-            for j in range(len(freq_array[i])):
-                # print("here", freq_array[i], j)
-                ans.append(freq_array[i][j])
-                k -= 1
+            heap.append((-val, key))
         
-        return ans
-                
-            
+        heapq.heapify(heap)
+        answer = []
+        while k > 0:
+            answer.append(heapq.heappop(heap)[-1])
+            k -= 1
+        return answer
+
+
