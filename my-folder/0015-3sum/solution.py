5099cae035fe47d9ab779ae0d6425558b115510c
Arushi Sharma <arushi.net@outlook.com>
2024-01-27 15:15:49 +0000

    [LeetCode Sync] Runtime - 1141 ms (16.00%), Memory - 20.4 MB (63.20%)

--- my-folder/0015-3sum/solution.py
+++ my-folder/0015-3sum/solution.py
@@ -1,16 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        set1 = set()
-        hmap = {}
+        dup = set()
         ans = set()
         for i in range(len(nums)):
-            if nums[i] not in set1:
-                set1.add(nums[i])
+            if nums[i] not in dup:
+                dup.add(nums[i])
+                hmap = {}
                 for j in range(i+1, len(nums)):
-                    seen_j = -(nums[i] + nums[j])
-                    if seen_j in hmap and hmap[seen_j]==i:
-                        ans.add(tuple(sorted([nums[i], nums[j], seen_j])))
-                    hmap[nums[j]] = i
+                    if nums[j] in hmap:
+                        if nums[j] + hmap[nums[j]] + nums[i] == 0:
+                            ans.add(tuple(sorted([nums[j], hmap[nums[j]], nums[i]])))               
+                            
+                    else:
+                        hmap[-nums[i] - nums[j]] = nums[j]
         
         return ans
+
+                        
