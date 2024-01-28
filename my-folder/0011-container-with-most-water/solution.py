b739c611b6330d298f6387f54d886ba329764c0a
Arushi Sharma <arushi.net@outlook.com>
2024-01-28 05:29:40 +0000

    [LeetCode Sync] Runtime - 528 ms (5.02%), Memory - 30.2 MB (8.06%)

--- my-folder/0011-container-with-most-water/solution.py
+++ my-folder/0011-container-with-most-water/solution.py
@@ -1,20 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # strt ptr, end ptr
-        # calculate area = min(start, end) * (end - start)
-        # increment the smaller of the start and end ptr elements
-
-        start = 0
-        end = len(height)-1
+        
         maxarea = 0
-        while start < end:
-            maxarea = max(
-                maxarea, 
-                min(height[start], height[end]) * (end - start)
-                )
-            if height[start] < height[end]:
-                start += 1
+        currarea = 0
+        i = 0
+        j = len(height)-1
+        while i < j:
+            currarea=min(height[j], height[i])*abs(j - i)
+            
+            if height[j] > height[i]:
+                i += 1
             else:
-                end -= 1
-        return maxarea
+                j -= 1
+            maxarea = max(maxarea, currarea)
 
+        return maxarea
