1216a7f38b2d7d145817c62a1d772bbbdc5e0529
Arushi Sharma <arushi.net@outlook.com>
2024-01-29 04:06:28 +0000

    [LeetCode Sync] Runtime - 62 ms (5.07%), Memory - 17.1 MB (56.98%)

--- my-folder/0150-evaluate-reverse-polish-notation/solution.py
+++ my-folder/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,31 +1,22 @@
 class Solution:
-    def add(self, x, y): return x + y
-    def subtract(self, x, y): return x - y
-    def multiply(self, x, y): return x * y
-    def divide(self, x, y): return int(x / y)
-
     def evalRPN(self, tokens: List[str]) -> int:
-
-        # put numbers in stack till operator, 
-        # 2 , 1 + then you pop numbers from stack and do operation put back in stack
-        # next operation
-
+        
         stack = []
-        operations = ['+', '-', '*', '/']
         for i in range(len(tokens)):
-            if tokens[i] not in operations:
+            if tokens[i].lstrip("-").isdigit():
                 stack.append(int(tokens[i]))
             else:
-                y, x = stack.pop(), stack.pop()
+                # print(stack)
+                b = stack.pop()
+                a = stack.pop()
+                # print(a,b, tokens[i])
                 if tokens[i] == '+':
-                    ans = self.add(x, y)
-                elif tokens[i] == '-':
-                    ans = self.subtract(x, y)
+                    stack.append(a+b)
                 elif tokens[i] == '*':
-                    ans = self.multiply(x, y)
+                    stack.append(a*b)
+                elif tokens[i] == '-':
+                    stack.append(a-b)
                 else:
-                    ans = self.divide(x, y)
-                stack.append(ans)
-        
+                    stack.append(int(float(a)/b))
+                # print(stack)
         return stack.pop()
-
