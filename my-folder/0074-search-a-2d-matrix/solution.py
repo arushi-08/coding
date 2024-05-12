ad9df3ce61694dd72688b9336beedabbe4a1f69b
Arushi Sharma <arushi.net@outlook.com>
2024-05-12 00:55:47 +0000

    [LeetCode Sync] Runtime - 50 ms (6.16%), Memory - 17.1 MB (30.54%)

--- my-folder/0074-search-a-2d-matrix/solution.py
+++ my-folder/0074-search-a-2d-matrix/solution.py
@@ -1,27 +1,16 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-
-        low = 0
-        high = len(matrix) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if matrix[mid][0] == target: return True
-            if matrix[mid][0] < target:
-                low = mid + 1
-            else:
-                high = mid - 1
         
-        row = low - 1
-        if row < 0: return False
-        low = 0
-        high = len(matrix[0])-1
-        while low <= high:
-            mid = (low + high) // 2
-            if matrix[row][mid] == target: return True
-            if matrix[row][mid] < target:
-                low = mid + 1
+        i = 0
+        # end = len(matrix[0])-1
+        j = len(matrix[0])-1
+
+        while i >= 0 and i < len(matrix) and j < len(matrix[0]) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            if matrix[i][j] > target:
+                j -= 1
             else:
-                high = mid - 1
+                i += 1
         
         return False
-
