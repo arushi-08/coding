d831fe982a7e6cac3b2f4c24e2fe84004ad55f1e
Arushi Sharma <arushi.net@outlook.com>
2024-05-13 19:22:40 +0000

    [LeetCode Sync] Runtime - 41 ms (5.39%), Memory - 16.6 MB (32.45%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,21 +1,24 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits: return []
+        
+        ans = []
         comb = {'2':['a','b','c'], 
         '3':['d','e','f'], '4':['g','h','i'],
         '5':['j','k','l'], '6':['m','n','o'],
         '7':['p','q','r','s'], '8':['t','u','v'],
         '9':['w','x','y','z']}
-        res = []
-        def dfs(index, path):
-            if len(path) == len(digits):
-                res.append("".join(path))
+
+        def dfs(curr, res, ans):
+            if not curr:
+                if res:
+                    ans.append(res)
                 return
-            letters = comb[digits[index]]
-            for letter in letters:
-                path.append(letter)
-                dfs(index+1, path)
-                path.pop()
+            
+            for digit in comb[curr[0]]:
+                res += digit
+                dfs(curr[1:], res, ans)
+                res = res[:-1]
         
-        dfs(0, [])
-        return res
+        dfs(digits, '', ans)
+
+        return ans
