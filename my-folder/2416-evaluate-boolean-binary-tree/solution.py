7e5034a1c7bd61f856a321da7d364f4686a7bd03
Arushi Sharma <arushi.net@outlook.com>
2024-05-16 17:29:10 +0000

    [LeetCode Sync] Runtime - 39 ms (7.97%), Memory - 16.8 MB (60.77%)

--- my-folder/2416-evaluate-boolean-binary-tree/solution.py
+++ my-folder/2416-evaluate-boolean-binary-tree/solution.py
@@ -7,15 +7,15 @@
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
         
-        if not root or not root.val: return False
-        
-        if not root.left and not root.right: return True
-        
+        if not root: return False
+
         left = self.evaluateTree(root.left)
         right = self.evaluateTree(root.right)
-        
-        if root.val == 2: return left or right
-        
-        if root.val == 3: return left and right
-        
-        return False
+
+        if root.val == 0:
+            return False
+        if root.val == 1:
+            return True
+        if root.val == 2:
+            return left or right
+        return left and right
