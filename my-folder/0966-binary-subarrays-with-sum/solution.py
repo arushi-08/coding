cad95706534ebf4e5e44fc1e98c1fdd1de7eb111
Arushi Sharma <arushi.net@outlook.com>
2024-05-17 23:32:44 +0000

    [LeetCode Sync] Runtime - 214 ms (6.20%), Memory - 20.2 MB (49.84%)

---
+++ my-folder/0966-binary-subarrays-with-sum/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        psum = 0
+        cursum = 0
+        hmap = {0:1}
+        for i in range(len(nums)):
+            psum += nums[i]
+            elem = psum - goal
+            if elem in hmap:
+                cursum += hmap[elem]
+            hmap[psum] = hmap.get(psum, 0) + 1
+        
+        return cursum
