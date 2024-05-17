8290e61673cd4ba26b4a58c2efab1285ca8b3a16
Arushi Sharma <arushi.net@outlook.com>
2024-05-17 22:24:48 +0000

    [LeetCode Sync] Runtime - 67 ms (5.27%), Memory - 19.9 MB (55.99%)

--- my-folder/0253-meeting-rooms-ii/solution.py
+++ my-folder/0253-meeting-rooms-ii/solution.py
@@ -1,26 +1,18 @@
+from heapq import heapify, heappush, heappop
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        # sort the intervals by start time
-        # the intervals that overlap -> # of rooms
-        
-        intervals.sort(key = lambda x: x[0])
-        print(intervals)
-        rooms = len(intervals)
-        meeting_time = []
-        for i in range(len(intervals)):
-            if not meeting_time:
-                meeting_time.append(intervals[i][1])
-            else:
-                j = 0
-                while j < (len(meeting_time)):
-                    if meeting_time[j] <= intervals[i][0]:
-                        break
-                    j += 1
-                if j < len(meeting_time):
-                    meeting_time[j] = intervals[i][1]
-                else:
-                    meeting_time.append(intervals[i][1])
-        
-        return len(meeting_time)
+        if not intervals: return 0
 
+        intervals.sort(key = lambda x: x[0])
+
+        end_time_heap = []
+        heappush(end_time_heap, intervals[0][1])
+
+        for i in range(1, len(intervals)):
+            if intervals[i][0] >= end_time_heap[0]:
+                heappop(end_time_heap)
+            
+            heappush(end_time_heap, intervals[i][1])
+
+        return len(end_time_heap)
