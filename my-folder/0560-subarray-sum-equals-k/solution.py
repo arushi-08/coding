d0b731e71847d65d6a47f8abbdc6456cfd595028
Arushi Sharma <arushi.net@outlook.com>
2024-05-17 00:34:50 +0000

    [LeetCode Sync] Runtime - 231 ms (5.23%), Memory - 19.6 MB (30.25%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,14 +1,14 @@
+from collections import Counter
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # psum
-        # p[j] - k = p[i] - store in map
-        hmap ={0:1}
-        ans = 0
+        
+        psumhmap = {0: 1}
+        count = 0
         psum = 0
-        for j in range(len(nums)):
-            psum += nums[j]
-            if psum-k in hmap:
-                ans += hmap[psum-k]
-            hmap[psum] = hmap.get(psum, 0) + 1
-        return ans
+        for i in range(len(nums)):
+            psum += nums[i]
+            if psum - k in psumhmap:
+                count += psumhmap[psum - k]
+            psumhmap[psum] = psumhmap.get(psum, 0) + 1
+        return count
 
