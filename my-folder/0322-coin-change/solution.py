4dae85d83cb186b125bb86137aef955914bcf212
Arushi Sharma <arushi.net@outlook.com>
2024-05-21 21:33:43 +0000

    [LeetCode Sync] Runtime - 910 ms (30.88%), Memory - 16.9 MB (69.74%)

--- my-folder/0322-coin-change/solution.py
+++ my-folder/0322-coin-change/solution.py
@@ -1,15 +1,40 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}
+
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        dp = [float('inf')] * (amount+1)
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
-        for i in range(len(coins)):
-            for j in range(coins[i], amount+1):
-                dp[j] = min(dp[j], dp[j-coins[i]] + 1)
-        
-        return dp[amount] if dp[amount] != float('inf') else -1
+        for i in range(1, len(dp)):
+            for j in range(len(coins)):
+                if i >= coins[j]:
+                    res = dp[i - coins[j]]
+                    if res != -1:
+                        dp[i] = min(res + 1, dp[i])
 
+        if dp[amount] == float('inf'):
+            return -1
+        return dp[amount]
+
+        # if not amount:
+        #     return 0
+
+        # if amount in self.memo:
+        #     return self.memo[amount]
+
+        # result = float('inf')
+        # for i in range(len(coins)):
+        #     if amount - coins[i] >= 0:
+        #         res = self.coinChange(coins, amount-coins[i])
+        #         if res != -1:
+        #             result = min(res + 1, result)
         
-        
+        # if str(result)!='inf':
+        #     self.memo[amount] = result
+        # else:
+        #     self.memo[amount] = -1
+
+        # return self.memo[amount]
 
 
