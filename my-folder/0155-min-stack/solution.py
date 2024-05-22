7915096cb143b32187dfa57da76aea97603b7c26
Arushi Sharma <arushi.net@outlook.com>
2024-05-22 04:04:05 +0000

    [LeetCode Sync] Runtime - 65 ms (11.18%), Memory - 21.5 MB (7.69%)

--- my-folder/0155-min-stack/solution.py
+++ my-folder/0155-min-stack/solution.py
@@ -1,25 +1,32 @@
+from heapq import heappush, heapify, heappop
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.min_val = float('inf')
+        self.stack = {}
+        self.heap = []
+        self.id = 0
 
     def push(self, val: int) -> None:
-        self.stack.append((val, min(val, self.min_val)))
-        self.min_val = min(val, self.min_val)
+        self.stack[self.id] = val
+        heappush(self.heap, (val, self.id))
+        self.id += 1
 
     def pop(self) -> None:
-        element = self.stack.pop()
-        if not self.stack:
-            self.min_val = float('inf')
-        else:
-            self.min_val = self.stack[-1][1]
+        element = self.stack[self.id-1]
+        del self.stack[self.id-1]
+        # print(element, self.id,self.heap[0])
+        if self.heap[0][1] == self.id-1:
+            heappop(self.heap)
+        self.id -= 1
 
     def top(self) -> int:
-        return self.stack[-1][0]
+        return self.stack[self.id-1]
 
     def getMin(self) -> int:
-        return self.min_val
+        # {(2147483646, 1)}
+        while self.heap[0][1] not in self.stack:
+            heappop(self.heap)
+        return self.heap[0][0]
 
 
 # Your MinStack object will be instantiated and called as such:
