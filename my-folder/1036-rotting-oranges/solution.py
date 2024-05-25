b585528a6b66e5c6e7d50847cdab2f0464d07aff
Arushi Sharma <arushi.net@outlook.com>
2024-05-25 04:41:43 +0000

    [LeetCode Sync] Runtime - 55 ms (5.15%), Memory - 16.4 MB (95.99%)

--- my-folder/1036-rotting-oranges/solution.py
+++ my-folder/1036-rotting-oranges/solution.py
@@ -2,34 +2,37 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        fresh_oranges = 0
+        # maintain levels
         queue = deque()
+        visited = set()
+        fresh = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 2:
+                if grid[i][j] == 2 and (i,j) not in visited:
                     queue.append((i,j))
-                elif grid[i][j] == 1:
-                    fresh_oranges += 1
-        
-        queue.append((-1,-1))
-        time = -1
-        rows = [-1, 1, 0, 0]
-        cols = [0, 0, -1, 1]
-        while len(queue):
-            curri, currj = queue.popleft()
-            if curri == -1:
-                time += 1
-                if len(queue):
+                    visited.add((i,j))
+                if grid[i][j] == 1:
+                    fresh += 1
+
+        queue.append((-1,0))
+        rows = [1, 0, -1, 0]
+        cols = [0, -1, 0, 1]
+        level = -1
+        while queue:
+
+            i, j = queue.popleft()
+            
+            if i == -1:
+                level += 1
+                if queue:
                     queue.append((-1,-1))
             else:
-                for i in range(len(rows)):
-                    if self.isSafe(rows[i] + curri, cols[i] + currj, grid):
-                        queue.append((rows[i] + curri, cols[i] + currj))
-                        grid[rows[i] + curri][cols[i] + currj] = 2
-                        fresh_oranges -= 1
-        return time if fresh_oranges == 0 else -1
-    def isSafe(self, x, y, grid):
-        return (x >= 0 and x < len(grid) and y >= 0 and y <len(grid[0]) and grid[x][y]==1)
+                for x in range(len(rows)):
+                    if self.isSafe(grid, i + rows[x], j + cols[x], visited):
+                        queue.append((i + rows[x], j + cols[x]))
+                        visited.add((i + rows[x], j + cols[x]))
+                        fresh -= 1
+        return level if not fresh else -1
 
-
-
+    def isSafe(self, grid, x, y, visited):
+        return 0 <= x and x < len(grid) and 0 <= y and y < len(grid[0]) and grid[x][y] == 1 and (x,y) not in visited
