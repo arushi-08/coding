5443f8e9830d6310e9605a82bb1ab083792dfc0f
Arushi Sharma <arushi.net@outlook.com>
2024-05-26 18:19:11 +0000

    [LeetCode Sync] Runtime - 51 ms (5.27%), Memory - 17 MB (53.24%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,26 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        left = 0
-        right = len(nums)-1
-        while left < right:
-            mid = (left + right)//2
+        st = 0
+        ed = len(nums)-1
+        while st < ed:
+            mid = (st + ed)//2
+            print("mid", mid, "st", st, "ed", ed)
             if nums[mid] == target:
                 return mid
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target and target < nums[mid]:
-                    right = mid - 1
-                elif nums[left] > target or target > nums[mid]:
-                    left = mid + 1
-                # else:
-                    # print(nums[left], nums[mid], nums[right])
-            elif nums[mid] < nums[right]:
-                if nums[mid] < target and target <= nums[right]:
-                    left = mid + 1
-                elif nums[mid] > target or target > nums[right]:
-                    right = mid - 1
-
-        if nums[left] == target: 
-            return left
-        return -1
-
+            
+            if nums[mid] >= nums[st]:
+                # left half is sorted
+                if nums[st] <= target and target < nums[mid]:
+                    ed = mid - 1
+                else:
+                    st = mid + 1
+            
+            if nums[mid] <= nums[ed]:
+                if nums[mid] < target and target <= nums[ed]:
+                    st = mid + 1
+                else:
+                    ed = mid - 1
+            
+        return st if nums[st] == target else -1
