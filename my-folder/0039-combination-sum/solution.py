b07bc0d5e9c52f41e94cae2952d7c3a5ea43ba38
Arushi Sharma <arushi.net@outlook.com>
2024-05-26 18:32:11 +0000

    [LeetCode Sync] Runtime - 43 ms (10.47%), Memory - 16.5 MB (99.23%)

--- my-folder/0039-combination-sum/solution.py
+++ my-folder/0039-combination-sum/solution.py
@@ -1,20 +1,17 @@
+from collections import Counter
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        res = []
-
-        def backtrack(index, path, pathsum):
-            if pathsum > target: return
-            if pathsum == target:
-                res.append(path.copy())
+        def backtrack(res, currans, index):
+            if res == 0:
+                ans.append(currans.copy())
                 return
-            
+            if res < 0:
+                return
             for i in range(index, len(candidates)):
-                if pathsum+candidates[i] <= target:
-                    path.append(candidates[i])
-                    backtrack(i, path, pathsum+candidates[i])
-                    path.pop()
-                
-        
-        backtrack(0, [], 0)
-        return res
+                currans.append(candidates[i])
+                backtrack(res-candidates[i], currans, i)
+                currans.pop()
+        ans = []
+        backtrack(target, [], 0)
+        return ans
