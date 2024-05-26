1ecca1db06962b84945c7e51cd2207063afd8447
Arushi Sharma <arushi.net@outlook.com>
2024-05-26 23:01:26 +0000

    [LeetCode Sync] Runtime - 556 ms (5.09%), Memory - 73.4 MB (79.99%)

--- my-folder/1023-time-based-key-value-store/solution.py
+++ my-folder/1023-time-based-key-value-store/solution.py
@@ -2,40 +2,28 @@ from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
-        self.timemap = defaultdict(list)
+        self.tmap = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.timemap[key].append((timestamp, value))
+        self.tmap[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        item = self.timemap[key]
-        # if timestamp in item:
-        #     return item[timestamp]
-        
-        # sorted_items = dict(sorted(item.items(), key=lambda x:x[0]))
-        
-        if not item:
-            return ""
-        if item[0][0] > timestamp:
-            return ""
-        
-        left = 0
-        right = len(item)-1
-        res = ""
-        while left <= right:
-            mid = (left + right)//2
-            if item[mid][0] <= timestamp:
-                res = item[mid][1]
-                left = mid + 1
+        if key not in self.tmap: return ""
+        kmap = self.tmap[key]
+
+        st = 0
+        ed = len(kmap)-1
+        maxt = ""
+        while st <= ed:
+            mid = (st + ed)//2
+            if kmap[mid][0] <= timestamp:
+                maxt = kmap[mid][1]
+                st = mid + 1
             else:
-                right = mid - 1
+                ed = mid - 1
+        
+        return maxt
 
-        return res
-
-        # love: 10: high, 20: low
-        # love : 5 -> ""
-        # love : 10 -> high
-        # love : 15 -> HIGH
 
 
 # Your TimeMap object will be instantiated and called as such:
