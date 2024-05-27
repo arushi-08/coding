bf4eb7965e98b8cfc70c27a7ae003c53cef62b8e
Arushi Sharma <arushi.net@outlook.com>
2024-05-27 18:43:27 +0000

    [LeetCode Sync] Runtime - 31 ms (6.73%), Memory - 16.4 MB (86.37%)

--- my-folder/0075-sort-colors/solution.py
+++ my-folder/0075-sort-colors/solution.py
@@ -3,14 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        curr, p1, p2 = 0, 0, len(nums)-1
-        while curr <= p2:
-            if nums[curr] == 0:
-                nums[curr], nums[p1] = nums[p1], nums[curr]
-                curr += 1
-                p1 += 1
-            elif nums[curr] == 2:
-                nums[curr], nums[p2] = nums[p2], nums[curr]
-                p2 -= 1
+        
+
+        left = 0
+        right = len(nums) - 1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
             else:
-                curr += 1
+                i += 1
+                
+
