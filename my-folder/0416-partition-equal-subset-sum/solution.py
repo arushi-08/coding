d10dbad3abcbbff4b244baf4c112a902eaa07477
Arushi Sharma <arushi.net@outlook.com>
2024-05-27 23:37:38 +0000

    [LeetCode Sync] Runtime - 775 ms (44.80%), Memory - 196.7 MB (9.40%)

--- my-folder/0416-partition-equal-subset-sum/solution.py
+++ my-folder/0416-partition-equal-subset-sum/solution.py
@@ -1,45 +1,28 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        """
-        subsetsum = totalsum / 2
-        
-        [1,5,11,5]
-        [1] [5,11,5]    [] [5,11,5]
-        [1,5] [11,5] [1] [11,5]    [5] [11,5] [] [11,5]
-        
-        recursive: idx, sum 
-        
-        """
-        
-        if sum(nums) % 2 == 1: return False
-        
-        subset_sum = sum(nums) // 2
-        
-        if subset_sum in nums: return True
-        
-        dp = [[False]*(subset_sum+1) for _ in range(len(nums))]
-        dp[0][0] = True
-        for i in range(len(dp)):
-            for j in range(len(dp[0])):
-                if j == nums[i]:
-                    dp[i][j] = True
-                if j == 0 :
-                    dp[i][j] = True
-        
-        for i in range(1, len(dp)):
-            for j in range(len(dp[0])):
-                condn1 = False
-                if nums[i] <= j:
-                    condn1 = dp[i-1][j-nums[i]]
-                condn2 = dp[i-1][j]
-                
-                dp[i][j] = condn1 or condn2
-        
-        return dp[len(nums)-1][subset_sum]
-        
-        
-        
-        
-        
-        
-        
+    
+        tsum = sum(nums)
+        if tsum % 2 == 1: return False
+        memo = {}
+
+        def dfs(subsetsum, csum, index):
+            if csum == subsetsum:
+                return True
+
+            if csum > subsetsum or index == len(nums):
+                return False
+            
+            if (csum + nums[index], index) in memo:
+                return memo[(csum + nums[index], index)]
+
+            result =  dfs(
+                subsetsum, csum + nums[index], index+1
+                ) or dfs(
+                    subsetsum, csum, index+1
+                    )
+            memo[(csum + nums[index], index)] = result
+
+            return result
+
+        subsetsum = tsum // 2
+        return dfs(subsetsum, 0, 0)
