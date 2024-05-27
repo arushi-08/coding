09ee7a9e9500d6089a8060a2562ff84e79c915f4
Arushi Sharma <arushi.net@outlook.com>
2024-05-27 23:54:22 +0000

    [LeetCode Sync] Runtime - 41 ms (5.67%), Memory - 16.4 MB (99.75%)

--- my-folder/0008-string-to-integer-atoi/solution.py
+++ my-folder/0008-string-to-integer-atoi/solution.py
@@ -1,47 +1,27 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         
-        
+        s = s.strip()
         int_list = []
         
         for i in range(len(s)):
-            if s[i].isdigit():
-                if i > 0 and s[i-1] == "-":
-                    int_list.append(s[i-1])
+            if (ord(s[i]) >= ord('0') and ord(s[i]) <= ord('9')) or (i==0 and s[i] in ['-', '+']):
                 int_list.append(s[i])
-            
-            elif not s[i].isdigit() and s[i] not in [" ", "-", "+"] and not int_list:
-                    return 0
-                
-            if s[i] in ["-", "+"] and i + 1 < len(s) and not s[i + 1].isdigit() and not int_list:
-                return 0
-            
-            elif int_list and not s[i].isdigit():
+            else:
                 break
         
-        if not int_list:
-            print(s)
+        uplimit = 2**31
+        downlimit = -2**31
+        
+
+        if int_list in (['-'], ['+']):
             return 0
-        
-        pos = False
-        
-        if int_list[0].isdigit():
-            pos = True
-            start = 0
-            count = len(int_list) - 1
-        else:
-            start = 1
-            count = len(int_list) - 2
-        ans = 0
-        for i in range(start,len(int_list)):
-            ans += int(int_list[i]) * pow(10, count)
-            count -= 1
-        
-        if pos:
-            if ans < pow(2,31) - 1:
-                return ans
-            return pow(2,31) - 1
-        if -ans > pow(-2, 31):
-            return -ans
-        return pow(-2, 31)
-            
+        if int_list:
+            ans = int(''.join(int_list))
+            if ans > uplimit - 1:
+                ans = uplimit - 1
+            elif ans < downlimit:
+                ans = downlimit
+                
+            return ans
+        return 0
