bbdc2e6f52032787a8c45468a6ba3bd433196dc5
Arushi Sharma <arushi.net@outlook.com>
2024-05-28 17:56:19 +0000

    [LeetCode Sync] Runtime - 29 ms (5.17%), Memory - 16.6 MB (83.22%)

--- my-folder/0078-subsets/solution.py
+++ my-folder/0078-subsets/solution.py
@@ -1,15 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        if nums:
-            chosen = [False]*(len(nums)+1)
-            curr = []
-            self.dfs(nums, res, curr, 0)
-        return res
-    
-    def dfs(self, nums, res, curr, idx):
-        res.append(curr)
         
-        for i in range(idx, len(nums)):
-            self.dfs(nums, res, curr + [nums[i]], i+1)
-        
+        ans = []
+
+        def recursion(nums, index, res):
+
+            if index == len(nums):
+                ans.append(res)
+                return
+            
+            recursion(nums, index + 1, res)
+            recursion(nums, index + 1, res + [nums[index]])
+
+        recursion(nums, 0, [])
+
+        return ans
+            
+
