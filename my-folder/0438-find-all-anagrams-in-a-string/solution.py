7b9a87c152edc54fa1049a4a629a61b5eef14428
Arushi Sharma <arushi.net@outlook.com>
2024-05-29 18:31:14 +0000

    [LeetCode Sync] Runtime - 70 ms (47.18%), Memory - 17.6 MB (64.68%)

--- my-folder/0438-find-all-anagrams-in-a-string/solution.py
+++ my-folder/0438-find-all-anagrams-in-a-string/solution.py
@@ -1,18 +1,35 @@
 from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-            # s = cbaebabacd , p = abc o/p = [0, 6]
-        if len(s) < len(p): return []
-        i = 0
-        j = len(p) - 1
-        freq_p = Counter(p)
-        answer = []
-        while j < len(s):
-            window = Counter(s[i:j + 1])
-            if window == freq_p:
-                answer.append(i)
-            i += 1
-            j += 1
-        return answer    
+        
+
+        # return list of start idx of anagrams of p
+        if len(p) > len(s): return []
+        st = 0
+        plen = len(p)
+        pmap = [0]*26
+        for i in range(plen):
+            pmap[ord(p[i]) - ord('a')] += 1
+        ed = len(p) - 1
+        ans = []
+        smap = [0]*26
+        for i in range(plen):
+            smap[ord(s[i]) - ord('a')] += 1
+
+        while st < len(s):
+            if smap == pmap:
+                ans.append(st)
             
+            smap[ord(s[st]) - ord('a')] -= 1
+            # if smap[s[st]] == 0:
+            #     del smap[s[st]] 
+            st += 1
+            ed += 1
+            if ed < len(s):
+                smap[ord(s[ed]) - ord('a')] += 1
+            
+
+        return ans
+
+
 
