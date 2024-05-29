700b827471a6a2b6a37d0409312b4795e2eff69b
Arushi Sharma <arushi.net@outlook.com>
2024-05-29 14:20:12 +0000

    [LeetCode Sync] Runtime - 6373 ms (12.27%), Memory - 16.5 MB (97.00%)

--- my-folder/0079-word-search/solution.py
+++ my-folder/0079-word-search/solution.py
@@ -1,25 +1,39 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                if self.backtrack(board, word, row, col):
-                    return True
-
-    def backtrack(self, board, word, i, j):
         
-        if not word: return True
+        # dfs - similar to #islands
 
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j]!=word[0]:
-            return False
-        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                visited = set()
+                if board[i][j] == word[0]:
+                    visited.add((i, j))
+                    ans = self.dfs(i, j, board, word, 1, visited)
+                    if ans:
+                        return True
 
-        board[i][j] = '#'
-        rows = [1, 0, -1, 0]
-        cols = [0, 1, 0, -1]
-        for k in range(len(rows)):
-            if self.backtrack(board, word[1:], i+rows[k], j+cols[k]):
-                return True
+        return False
+    
+    def dfs(self, x, y, board, word, idx, visited):
         
-        board[i][j] = word[0]
+        if idx == len(word):
+            return True
+
+        rows = [0, 1, 0, -1]
+        cols = [1, 0, -1, 0]
+
+        for i in range(len(rows)):
+            if self.isSafe(x+rows[i], y+cols[i], board, word, idx, visited):
+                visited.add((x+rows[i], y+cols[i]))
+                ans = self.dfs(x+rows[i], y+cols[i], board, word, idx + 1, visited)
+                if ans:
+                    return ans
+                else:
+                    visited.remove((x+rows[i], y+cols[i]))
         
         return False
+    
+    def isSafe(self, x, y, board, word, idx, visited):
+        return x >= 0 and y >= 0 and x < len(board) and y < len(board[0]) and board[x][y] == word[idx] and (x,y) not in visited
+        
+
