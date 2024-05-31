81e7d410bf152b629f4528766e6b2442a30d0bb6
Arushi Sharma <arushi.net@outlook.com>
2024-05-31 20:49:51 +0000

    [LeetCode Sync] Runtime - 564 ms (7.75%), Memory - 77.5 MB (74.65%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,47 +1,40 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
-
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.hmap = {}
-        self.left = Node(0,0)
-        self.right = Node(0,0)
-        self.left.next = self.right
-        self.right.prev = self.left
+        self.cache = {}
+        self.heap = []
+        self.use = defaultdict(int)
+        self.counter = 0
 
     def get(self, key: int) -> int:
-        if key in self.hmap:
-            self.remove(self.hmap[key])
-            self.insert(self.hmap[key])
-            return self.hmap[key].value
+        if key in self.cache:
+            self.use[key] = self.counter
+            self.counter += 1
+            return self.cache[key]
+            
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hmap:
-            self.remove(self.hmap[key])
+        
+        if key in self.cache:
+            self.cache[key] = value
+            self.use[key] = self.counter
+            self.counter += 1
+            return
 
-        self.hmap[key] = Node(key, value)
-        self.insert(self.hmap[key])
-        if len(self.hmap) > self.capacity:
-            lru = self.left.next
-            self.remove(lru)
-            del self.hmap[lru.key]
-    
-    def remove(self, node):
-        left, right = node.prev, node.next
-        left.next = right
-        right.prev = left
-    
-    def insert(self, node):
-        self.right.prev.next = node
-        node.prev = self.right.prev
-        node.next = self.right
-        self.right.prev = node
+        if len(self.cache) >= self.capacity:
+            while self.heap[0][0] != self.use[self.heap[0][1]]:
+                _, k = heappop(self.heap)
+                heappush(self.heap, (self.use[k], k))
+            _, k = heappop(self.heap)
+            del self.cache[k]
+            del self.use[k]
+
+        self.cache[key] = value
+        self.use[key] = self.counter
+        heappush(self.heap, (self.use[key], key))
+        self.counter += 1
 
 
 # Your LRUCache object will be instantiated and called as such:
