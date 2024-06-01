8f52f1e1c4c36f7c57a0e8918e5af993ce780847
Arushi Sharma <arushi.net@outlook.com>
2024-06-01 20:18:47 +0000

    [LeetCode Sync] Runtime - 111 ms (19.48%), Memory - 21.4 MB (15.25%)

--- my-folder/0297-serialize-and-deserialize-binary-tree/solution.py
+++ my-folder/0297-serialize-and-deserialize-binary-tree/solution.py
@@ -13,52 +13,19 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        if not root : return ""
-        
-        tree_list = self.levelOrdertraversal(root)
-        for i in range(len(tree_list)-1,-1,-1):
-            if isinstance(tree_list[i],int):
-                break
-            del tree_list[i]
-        
-        ans = "["
-        for i in range(len(tree_list)):
-            if isinstance(tree_list[i], int):
-                ans += str(tree_list[i]) + ","
-            else:
-                ans += "null,"
-            if i == len(tree_list)-1:
-                ans = ans[:-1]
-        ans += "]"
-        # print(ans) 
-        return ans
-    
-    def levelOrdertraversal(self, root):
-        
-        queue = [root]
-        ans = []
-        while len(queue):
-            curr = queue.pop(0)
-            if not curr:
-                ans.append(curr)
-                continue
+        ans = ''
+        def preorder(root):
+            nonlocal ans
+            if not root:
+                ans += 'null,'
+                return 
             
-            ans.append(curr.val)
-            
-            if curr.left:
-                queue.append(curr.left)
-            else:
-                queue.append(None)
-            if curr.right:
-                queue.append(curr.right)
-            else:
-                queue.append(None)
-        
-        return ans
-        
-        
-        
-        
+            ans+=str(root.val) + ','
+            preorder(root.left)
+            preorder(root.right)
+        preorder(root)
+        return ans[:-1]
+
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -66,39 +33,24 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        # print("data", data)
-        tree_list = data.strip("[").strip("]").split(",")
-        if tree_list[0] in ["", "null"]:
-            return None
-        tree_list = [TreeNode(i) if i != "null" else None for i in tree_list ]
-        # print("decode",tree_list)
-        
-        root = tree_list[0]
-        i = 0
+        if not data:
+            return 
+        data = data.split(',')
         idx = 0
-        while i < len(tree_list):
-            curr = tree_list[i]
+        def preorder():
+            nonlocal idx, data
 
-            if not curr:
-                i += 1
-                continue
-                
-            # print("curr.val", curr.val, 2*idx+1, len(tree_list))
-            if 2*idx+1 < len(tree_list):
-                if tree_list[2*idx+1]:
-                    curr.left = tree_list[2*idx+1]
-                    # print(curr.val, "left", curr.left.val)
-                
-            if 2*idx+2 < len(tree_list):
-                if tree_list[2*idx+2]:
-                    curr.right = tree_list[2*idx+2]
-                    # print(curr.val, "right", curr.right.val)
-            
-            i += 1
+            if data[idx] == 'null':
+                idx += 1
+                return
+            root = TreeNode(int(data[idx]))
             idx += 1
-            
-        return root
+            root.left = preorder()
+            root.right = preorder()
+            return root
 
+        return preorder()
+        
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
