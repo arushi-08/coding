bc0fc057b8955019ac64b90701c68c5f99ceb6f5
Arushi Sharma <arushi.net@outlook.com>
2024-06-05 18:17:22 +0000

    [LeetCode Sync] Runtime - 39 ms (7.71%), Memory - 16.3 MB (98.18%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,14 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        # self.memo = {}
+        # return self.helper(nums, 0)
+
         if len(nums) <= 2:
             return max(nums)
-        
-        dp = [0] * len(nums)
+        dp = [0]*len(nums)
         dp[0] = nums[0]
-        dp[1] = max(nums[:2])
-        
-        for i in range(2, len(dp)):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-        
-        return dp[-1]
+        dp[1] = nums[1]
+        for i in range(2, len(nums)):
+            for j in range(2,i+1):
+                dp[i] = max(dp[i-j]+nums[i], dp[i])
+
+        return max(dp)
+
+    def helper(self, nums, index):
+
+        if index >= len(nums):
+            return 0
+
+        if index in self.memo:
+            return self.memo[index]
+
+        self.memo[index] = max(self.helper(nums, index+2)+nums[index], self.helper(nums, index+1))
+
+        return self.memo[index]
