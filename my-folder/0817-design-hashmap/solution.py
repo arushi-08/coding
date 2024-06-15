4cb47244bb9bd35233d476434d78a93b5b892815
Arushi Sharma <arushi.net@outlook.com>
2024-06-15 23:17:10 +0000

    [LeetCode Sync] Runtime - 176 ms (22.09%), Memory - 20.3 MB (33.55%)

---
+++ my-folder/0817-design-hashmap/solution.py
@@ -0,0 +1,33 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.const = 1000
+        self.d = [0] * (self.const + 1)
+
+    def key(self, i):
+        return divmod(i, self.const)
+
+    def put(self, key: int, value: int) -> None:
+        k1, k2 = self.key(key)
+        if self.d[k1] == 0:
+            self.d[k1] = [-1] * self.const
+        self.d[k1][k2] = value
+
+    def get(self, key: int) -> int:
+        k1, k2 = self.key(key)
+        if self.d[k1] == 0:
+            return -1
+        return self.d[k1][k2]
+
+    def remove(self, key: int) -> None:
+        k1, k2 = self.key(key)
+        if self.d[k1] == 0:
+            return 
+        self.d[k1][k2] = -1
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
