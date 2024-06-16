06ba914115f4b33bf95776f56336bfa69793036a
Arushi Sharma <arushi.net@outlook.com>
2024-06-16 00:22:21 +0000

    [LeetCode Sync] Runtime - 455 ms (5.13%), Memory - 29.8 MB (69.70%)

---
+++ my-folder/0219-contains-duplicate-ii/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        
+        hmap = {}
+        for i in range(len(nums)):
+            if nums[i] in hmap:
+                if i - hmap[nums[i]] <= k:
+                    return True
+            hmap[nums[i]] = i
+        
+        return False
