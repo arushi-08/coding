3f6ca21c9433645848a65410e13457ff32e59b32
Arushi Sharma <arushi.net@outlook.com>
2024-06-22 16:25:58 +0000

    [LeetCode Sync] Runtime - 130 ms (6.51%), Memory - 16.7 MB (73.48%)

---
+++ my-folder/2816-lexicographically-smallest-palindrome/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        
+        s, l = list(s), len(s)
+        for i in range(l//2):
+            if s[i] != s[l-i-1]:
+                c = min(s[i], s[l-i-1])
+                s[i] = c
+                s[l-i-1] = c
+        return ''.join(s)
+
