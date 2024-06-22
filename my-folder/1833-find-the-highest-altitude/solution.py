7c32c23e1ba323d7d7c24429bc8d66f2d7439242
Arushi Sharma <arushi.net@outlook.com>
2024-06-22 14:23:59 +0000

    [LeetCode Sync] Runtime - 36 ms (7.47%), Memory - 16.5 MB (82.07%)

---
+++ my-folder/1833-find-the-highest-altitude/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        
+        altitudes = [0]
+        for g in gain:
+            altitudes.append(altitudes[-1] + g)
+        
+        return max(altitudes)
