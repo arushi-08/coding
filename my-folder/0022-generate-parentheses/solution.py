348517fae20224f2139a06dd970f05d51840f2bf
Arushi Sharma <arushi.net@outlook.com>
2024-06-26 22:58:57 +0000

    [LeetCode Sync] Runtime - 39 ms (5.37%), Memory - 16.8 MB (86.25%)

--- my-folder/0022-generate-parentheses/solution.py
+++ my-folder/0022-generate-parentheses/solution.py
@@ -1,23 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        # given n pair of parentheses
-        # write function to generate all combinations of well-formed parentheses
         ans = []
-        def dfs(left_count, right_count, res):
-            if len(res) == n*2: 
+        def dfs(res, left_count, right_count):
+            if len(res)==n*2:
                 ans.append(''.join(res))
                 return
             
             if left_count < n:
                 res.append('(')
-                dfs(left_count+1, right_count, res)
+                dfs(res, left_count+1, right_count)
                 res.pop()
-
+            
             if right_count < left_count:
                 res.append(')')
-                dfs(left_count, right_count+1, res)
+                dfs(res, left_count, right_count+1)
                 res.pop()
-        
-        dfs(0, 0, [])
+
+        dfs([], 0, 0)
+
         return ans
