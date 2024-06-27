b45df17b6275157d17e93ca2da4a7ad1e90119e3
Arushi Sharma <arushi.net@outlook.com>
2024-06-27 01:42:37 +0000

    [LeetCode Sync] Runtime - 348 ms (9.01%), Memory - 21.5 MB (74.15%)

---
+++ my-folder/0305-number-of-islands-ii/solution.py
@@ -0,0 +1,64 @@
+class UnionFind:
+    def __init__(self, size):
+        self.rank = [0] * size
+        self.parent = [-1] * size
+        self.count = 0
+    
+    def isLand(self, x):
+        if self.parent[x] >= 0:
+            return True
+        return False
+    
+    def addLand(self, x):
+        if self.parent[x] >= 0:
+            return
+        self.parent[x] = x
+        self.count += 1
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        xset = self.find(x)
+        yset = self.find(y)
+        if xset == yset:
+            return
+        
+        if self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        elif self.rank[yset] > self.rank[xset]:
+            self.parent[xset] = yset
+        else:
+            self.parent[xset] = yset
+            self.rank[yset] += 1
+        
+        self.count -= 1
+
+    def numberofislands(self):
+        return self.count
+
+class Solution:
+    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        
+        answer = []
+        uf = UnionFind(m*n)
+
+        x = [-1, 0, 1, 0]
+        y = [0, 1, 0, -1]
+        for p in positions:
+            landpos = p[0] * n + p[1]
+            uf.addLand(landpos)
+
+            for i in range(4):
+                neighborx = p[0] + x[i]
+                neighbory = p[1] + y[i]
+                neighborpos = neighborx * n + neighbory
+
+                if neighborx >= 0 and neighborx < m and neighbory >= 0 and neighbory < n and uf.isLand(neighborpos):
+                    uf.union(landpos, neighborpos)
+
+            answer.append(uf.numberofislands())
+        
+        return answer
