d7c49868ba39295975c101e6b3e1bf1ac0874c5d
Arushi Sharma <arushi.net@outlook.com>
2024-06-27 03:11:19 +0000

    [LeetCode Sync] Runtime - 64 ms (76.47%), Memory - 20.8 MB (27.98%)

--- my-folder/0885-exam-room/solution.py
+++ my-folder/0885-exam-room/solution.py
@@ -1,70 +1,61 @@
 class ExamRoom:
-    # [(diff, startgap, endgap), (diff2, startgap, endgap)]
-    # 1000010000001
-    # 2 things
-    # find max dist to closest user -> midpoint of this dist return
-        # for each point, calculate left distance, right distance
-            # left psum -  111111222222
-            # right psum - 100000
-    # if user leaves, update max dist for other users
+
     def __init__(self, n: int):
         self.n = n
         self.heap = []
-        self.heap.append((-self.dist(0,n-1), 0, n-1))
-
-    def dist(self, l, r):
-        if l == 0 or r == self.n-1:
-            return r-l
+        heapify(self.heap)
+        heappush(self.heap, (-self.nextposition(0,n-1),0,n-1))
+    
+    def nextposition(self, l, r):
         
+        if l == 0 or r == self.n-1:
+            return r - l
         return (r-l)//2
-
 
     def seat(self) -> int:
 
+        pos, l, r = heappop(self.heap)
+        pos = -pos
+
         res = 0
-
-        curr_dist, l, r = heappop(self.heap)
-        curr_dist = -curr_dist
-        
         if l == 0:
-            res = 0
+            res = l
         elif r == self.n-1:
-            res = self.n-1
+            res = r
         else:
-            res = l + curr_dist
+            res = l + pos
 
-        track1, track2 = 0, 0
         if res > l:
-            track1 = -self.dist(l, res-1)
-            heappush(self.heap, (-self.dist(l, res-1) , l,res-1))
+            heappush(self.heap, (-self.nextposition(l, res-1), l, res-1))
         if res < r:
-            track2 = -self.dist(res+1, r)
-            heappush(self.heap, (-self.dist(res+1, r) , res+1, r))
+            heappush(self.heap, (-self.nextposition(res+1, r), res+1, r))
 
-        print(l, r, "dist", track1, track2)
-        print("res", res)
         return res
+        
 
     def leave(self, p: int) -> None:
-        
         previnterval, nextinterval = None, None
-
+        item_to_remove = None
         for item in self.heap:
-            if item[1]-1 == p:
+            pos, l, r = item
+            if l == p+1:
                 nextinterval = item
-
-            elif item[2]+1 == p:
+            if r == p-1:
                 previnterval = item
         
-        start, end = p, p
-        if previnterval:
-            start = previnterval[1]
-            self.heap.remove(previnterval)
+        start = p
+        end = p
+
         if nextinterval:
             end = nextinterval[2]
             self.heap.remove(nextinterval)
+        if previnterval:
+            start = previnterval[1]
+            self.heap.remove(previnterval)
 
-        heappush(self.heap, (-self.dist(start, end), start, end))
+        heappush(self.heap, (-self.nextposition(start, end), start, end))
+                
+
 
 # Your ExamRoom object will be instantiated and called as such:
 # obj = ExamRoom(n)
