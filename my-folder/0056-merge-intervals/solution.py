f72a66f54ded891847bc783baa8dcf8d35acbaae
Arushi Sharma <arushi.net@outlook.com>
2024-06-28 23:31:08 +0000

    [LeetCode Sync] Runtime - 128 ms (5.88%), Memory - 21.1 MB (20.31%)

--- my-folder/0056-merge-intervals/solution.py
+++ my-folder/0056-merge-intervals/solution.py
@@ -2,32 +2,23 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
         intervals.sort(key = lambda x: x[0])
-        if not intervals: return []
-        curr = 0
-        remove_idx = set()
-        print("intervals")
-        print(intervals)
-        gap = 1
-        for i in range(1,len(intervals)):
-            # print("curr", curr, i, intervals[curr], intervals[i])
-            if intervals[curr][1] >= intervals[i][0]:
-                # merge them
-                intervals[curr] = [min(
-                    intervals[curr][0],
-                    intervals[i][0]
-                ), max(
-                    intervals[curr][1],
-                    intervals[i][1]
-                )]
-                remove_idx.add(i)
-                gap += 1
-            else:
-                curr += gap
-                gap = 1
-            # print("curr", curr, i, intervals[curr])
-        ans = []
-        for i in range(len(intervals)):
-            if i in remove_idx:
-                continue
-            ans.append(intervals[i])
-        return ans
+
+        newintervals = []
+        i = 0
+        counter = 0
+        while i < len(intervals):
+            sti, edi = intervals[i]
+            preved = edi
+            j = i + 1
+            while j < len(intervals):
+                stj, edj = intervals[j]
+                if stj <= preved:
+                    preved = max(preved, edj)
+                else:
+                    break
+                j += 1
+            
+            newintervals.append([sti, preved])
+            i = j
+
+        return newintervals
