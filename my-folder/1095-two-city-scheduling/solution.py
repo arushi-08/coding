ffc3f1bbe126cc39b7e6fa0bd5fc02d9f6bd3218
Arushi Sharma <arushi.net@outlook.com>
2024-06-28 12:45:33 +0000

    [LeetCode Sync] Runtime - 37 ms (5.60%), Memory - 16.6 MB (78.56%)

---
+++ my-folder/1095-two-city-scheduling/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        costs.sort(key = lambda x: x[0] - x[1])
+
+        # send first half to city a, second half to city b
+        res = 0
+        n = len(costs)//2
+        for i in range(n):
+            res += costs[i][0] + costs[i+n][1]
+        
+        return res
+        
