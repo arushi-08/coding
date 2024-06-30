43f369d10fce07df0dc7bf57b47bccdc10d5948f
Arushi Sharma <arushi.net@outlook.com>
2024-06-30 01:02:30 +0000

    [LeetCode Sync] Runtime - 71 ms (5.57%), Memory - 17.9 MB (92.71%)

--- my-folder/0813-all-paths-from-source-to-target/solution.py
+++ my-folder/0813-all-paths-from-source-to-target/solution.py
@@ -1,29 +1,23 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        # return all paths from 0 to n-1
         
-#         graph is adjacency list
-        res = []
-        curr = []
-        chosen = [False]*len(graph)
-        self.dfs(graph, 0, len(graph)-1, res, curr, chosen)
-        return res
-    
-    def dfs(self, graph, source, target, res, curr, chosen):
-        
-        if source == target:
-            curr.append(source)
-            res.append(curr.copy())
-            return
-        
-        # if not graph[source]: return
-        
-        curr.append(source)
-        for i in range(len(graph[source])):
-            if not chosen[graph[source][i]]:
-                chosen[graph[source][i]] = True
-                self.dfs(graph, graph[source][i], target, res, curr, chosen)
-                chosen[graph[source][i]] = False
-                curr.remove(graph[source][i])
-                
-        
-        
+        hmap = defaultdict(list)
+        for st, ed in enumerate(graph):
+            hmap[st].extend(ed)
+
+        n = len(graph)
+        ans = []
+        def dfs(res, ans, idx):
+            if idx == n-1:
+                ans.append(res.copy())
+                return
+            
+            for i in hmap[idx]:
+                res.append(i)
+                dfs(res, ans, i)
+                res.pop()
+
+        res = [0]
+        dfs(res, ans, 0)
+        return ans
