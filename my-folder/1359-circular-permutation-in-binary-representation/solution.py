62a43d622ce260828580fd0a0aa60dd0322cb09b
Arushi Sharma <arushi.net@outlook.com>
2024-07-29 16:59:38 +0000

    [LeetCode Sync] Runtime - 129 ms (31.82%), Memory - 24.8 MB (82.63%)

---
+++ my-folder/1359-circular-permutation-in-binary-representation/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def circularPermutation(self, n: int, start: int) -> List[int]:
+
+
+        graycodes = [0] * (1<<n)
+
+        for i in range(1 << n):
+            graycodes[i] = i ^ i >> 1
+        
+        result = []
+        for i in range(1 << n):
+            result.append(start^graycodes[i])
+        
+        return result
