382d59395d92a60b12f3f5250b2785ac4bf42cd9
Arushi Sharma <arushi.net@outlook.com>
2024-08-05 23:35:55 +0000

    [LeetCode Sync] Runtime - 531 ms (43.61%), Memory - 16.8 MB (63.69%)

--- my-folder/0673-number-of-longest-increasing-subsequence/solution.py
+++ my-folder/0673-number-of-longest-increasing-subsequence/solution.py
@@ -1,22 +1,28 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
         
-        dp = [1]*len(nums)
-        cnt = [1]*len(nums)
+
+        dp = [1] * len(nums)
+        ways = [1] * len(nums)
+
         for i in range(1, len(nums)):
+            
             for j in range(i):
                 if nums[i] > nums[j]:
                     if dp[i] < dp[j] + 1:
                         dp[i] = dp[j] + 1
-                        cnt[i] = cnt[j]
+                        ways[i] = ways[j]
                     elif dp[i] == dp[j] + 1:
-                        cnt[i] += cnt[j]
-                
-        max_lis_length = max(dp)
-        num_lis = 0
-        for i in range(len(dp)):
-            if dp[i] == max_lis_length:
-                num_lis += cnt[i]
+                        ways[i] += ways[j]
         
-        return num_lis
-        
+        maxi = max(dp)
+        count = 0
+        for i in range(len(nums)):
+            if dp[i] == maxi:
+                count += ways[i]
+        print(dp)
+        print(ways)
+        return count
+
+
+
