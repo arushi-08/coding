b6f132c391d986a44d0644b74e2f0354b3c80f44
Arushi Sharma <arushi.net@outlook.com>
2024-08-21 17:55:26 +0000

    [LeetCode Sync] Runtime - 40 ms (11.41%), Memory - 16.5 MB (93.18%)

--- my-folder/0118-pascals-triangle/solution.py
+++ my-folder/0118-pascals-triangle/solution.py
@@ -1,25 +1,36 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
-#         1, 11 1C0 1C1, 121, 2C0 2C1 2C2 121, 3C0 3C1 3C2 3C3
-        def fact(x):
-            if x == 0 or x == 1: return 1
-            # print('here',x, fact(x-1)*x)
-            return fact(x-1)*x
+        # recursively
+
+        # n = 1 -> return [1]
+        # n = 2 -> return [[1,1]]
+
+        # n = 3 -> add previous adj numbers
+
+        # logic:
+        # results = f(n-1)
+        # create new list
+        # iterate on the last results list
+        # add adj elements of l_r_l
+        # append 1 to sides
+
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
+            return self.generate(numRows-1) + [[1,1]]
+
+        results = self.generate(numRows-1)
+
+        last_result_val = results[-1]
+        new_row_list = [1]
+
+        for i in range(len(last_result_val)-1):
+            new_row_list.append(last_result_val[i]+last_result_val[i+1])
         
-        def comb(i, j):
-            # print('c',i, j, fact(i))
-            return int(fact(i)/(fact(j)*fact(i-j)))
-        
-        ans = []
-        for i in range(numRows):
-            temp = []
-            if i == 0:
-                ans.append([1])
-                continue
-            for j in range(i+1):
-                temp.append(comb(i,j))
-            ans.append(temp)
-            # break
-            # break
-        return ans
+        new_row_list.append(1)
+        results.append(new_row_list)
+        return results
+
+
+
