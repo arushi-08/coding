a06cfd6284c9752bb01225ce1d3a7cdf7085071a
Arushi Sharma <arushi.net@outlook.com>
2024-08-23 03:25:11 +0000

    [LeetCode Sync] Runtime - 231 ms (5.23%), Memory - 19.4 MB (64.24%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,14 +1,20 @@
-from collections import Counter
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
-        psumhmap = {0: 1}
-        count = 0
+        '''
+        [1,2,3-3,1,1,1,4,2,-3]
+        |----|
+        psum - k is the subarray sum from ((psum-k) to psum)
+        as psum - psum - k = k
+        hence, look for psum - k in hashmap
+        '''
         psum = 0
+        hmap = {0:1}
+        ans = 0
         for i in range(len(nums)):
             psum += nums[i]
-            if psum - k in psumhmap:
-                count += psumhmap[psum - k]
-            psumhmap[psum] = psumhmap.get(psum, 0) + 1
-        return count
+            if psum - k in hmap: # psum - k are the elements you exclude and you get subarray sum
+                ans += hmap[psum - k]
+            
+            hmap[psum] = hmap.get(psum, 0) + 1 # {0:1,1:1,2:1}
 
+        return ans
