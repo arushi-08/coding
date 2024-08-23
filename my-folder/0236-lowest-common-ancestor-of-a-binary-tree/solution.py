7571e19771f0eaa10b01bdeea9ce8d29b06ea76c
Arushi Sharma <arushi.net@outlook.com>
2024-08-23 15:23:28 +0000

    [LeetCode Sync] Runtime - 51 ms (50.49%), Memory - 21.1 MB (44.63%)

--- my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -7,14 +7,17 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        if not root or root == p or root == q:
+        # 7, 4
+        if not root:
             return root
         
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
 
-        if left and right:
+        if root == p or root == q:
             return root
         
+        if left and right:
+            return root
+
         return left or right
