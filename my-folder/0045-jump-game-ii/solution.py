5c8866c46b088ca2ae6f84112cd917c421854516
Arushi Sharma <arushi.net@outlook.com>
2024-08-24 21:17:42 +0000

    [LeetCode Sync] Runtime - 119 ms (30.81%), Memory - 17.6 MB (38.01%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -1,25 +1,30 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if not nums: return 0
-        dp = [0] * (len(nums)+1)
-        n = len(nums)
-        i = 0
-        for j in range(1, n):
-            while nums[i] + i < j:
-                i += 1
-            dp[j] = dp[i] + 1
-        return dp[n-1]
+
+        if len(nums) == 1:
+            return 0
+        
+        # maintain range
+        # left and right
+        # update right to max jump possible from the range
+        # update left to right + 1
+        # [1,5,1,1,1,5,1,1,1,]
+        # O(n)?- yes
+
+        l = 0
+        r = 0
+        n_jumps = 0
+        # [2,3,1,1,4]
+        while r < len(nums)-1:
             
-    
-    def jump_rec(self, nums):
-        ans = float("inf")
-        return self.helper(nums, ans)
-        
-    def helper(self, nums, ans):
-        if not nums: return 0
-        
-        for i in range(1,nums[-1]):
-            ans = min(self.helper(nums[:-i], ans) + 1, ans)
-        
-        return ans
-        
+            old_r = r
+            maxjump = 0
+            for i in range(l, r+1):
+                maxjump = max(maxjump, i + nums[i])
+            r = maxjump
+            l = old_r + 1
+            n_jumps += 1
+        # print('l', 'r', l, r)
+        return n_jumps
+
+
