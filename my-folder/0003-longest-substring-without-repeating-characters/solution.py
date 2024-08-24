79b5e59a17cc8912eceb98b88caf701eca03e1fc
Arushi Sharma <arushi.net@outlook.com>
2024-08-24 17:51:12 +0000

    [LeetCode Sync] Runtime - 50 ms (16.59%), Memory - 16.7 MB (66.23%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,17 +1,37 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
-        st, ed = 0, 0
-        hmap = {}
-        count = 0
-        maxcount = 0
-        while ed < len(s):
-            while s[ed] in hmap:
-                del hmap[s[st]]
-                st += 1
-                count -= 1
-            hmap[s[ed]] = 1
-            count += 1
-            maxcount = max(count, maxcount)
-            ed += 1
-        return maxcount
+        # sliding window approach
+        # 2 ptr: start=0, end=0
+        # set of visited chars
+        # when we encounter a duplicate
+            # increment start till duplicate char index = start
+        
+
+        # IMPORTANT :
+        # contracting window condn: start should not go till end
+        # start should go till 1st s[end] index + 1
+
+
+        start = 0
+        visited_chars = set()
+        max_substring_length = 0
+
+        # "abcabcbb"
+        # start=0, end=0 | max_substring_length = 1
+        # start=0, end=1 | max_substring_length = 2
+        # start=0, end=2 | max_substring_length = 3
+        # end=3, start=3 | max_substring_length = 3
+
+        # "aab"
+        for end in range(len(s)):
+
+            # contracting window
+            while s[end] in visited_chars and start < end:
+                visited_chars.remove(s[start])
+                start += 1
+            
+            visited_chars.add(s[end])
+            max_substring_length = max(max_substring_length, end - start + 1)
+
+        return max_substring_length
