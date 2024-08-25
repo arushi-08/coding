a98a97d4df0bc0065c162514112aa400a63908aa
Arushi Sharma <arushi.net@outlook.com>
2024-08-25 23:08:50 +0000

    [LeetCode Sync] Runtime - 27 ms (96.27%), Memory - 16.6 MB (21.47%)

--- my-folder/0007-reverse-integer/solution.py
+++ my-folder/0007-reverse-integer/solution.py
@@ -1,24 +1,28 @@
+from math import log10, floor
 class Solution:
     def reverse(self, x: int) -> int:
         
-        if x > 0:
-            copy_x = x
-        else:
-            copy_x = -x
-        # print(copy_x, str(copy_x))
-        int_list = 0
-        count = len(str(copy_x)) - 1
-        while(copy_x>0):
-            # print(copy_x%10)
-            # print(pow(10,count))
-            int_list += int(int(copy_x%10) * pow(10,count))
-            copy_x /= 10
+        if x == 0: return 0
+        sign = x > 0
+        x = abs(x)
+        x_reverse = 0
+        count = floor(log10(x))
+        # x = 123
+        # count = 2 | x_reverse += 300
+        # count = 1 | x = 12 | x_reverse += 20
+        # count = 0 | x = 1 | x_reverse += 1
+        while x:
+            quotient, remainder = divmod(x, 10)
+            x_reverse += remainder * 10**count
+            x = quotient
             count -= 1
         
-        if int_list > pow(2, 31) - 1 or int_list < pow(-2, 31):
+        if sign:
+            if x_reverse > (2**31)-1: 
+                return 0
+            return x_reverse
+        
+        if - x_reverse < -(2**31):
             return 0
-        if x > 0:
-            return int_list
-        else:
-            return -int_list
-            
+        
+        return -x_reverse
