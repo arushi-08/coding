99b2a86a2057b066a73cb2d79d56457bbfa71e29
Arushi Sharma <arushi.net@outlook.com>
2024-08-25 19:13:05 +0000

    [LeetCode Sync] Runtime - 295 ms (16.40%), Memory - 24.3 MB (34.18%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -2,28 +2,38 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        ans = 0
+        # return numislands
+        numislands = 0
         visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    self.helper(grid, visited, i, j)
-                    ans += 1
+                if grid[i][j]=='1' and (i,j) not in visited:
+                    visited.add((i,j))
+                    self.bfs(grid, i, j, visited)
+                    numislands += 1
         
-        return ans
+        return numislands
+    
+    def bfs(self, grid, i, j, visited):
 
-    def helper(self, grid, visited, i, j):
         queue = deque()
         queue.append((i,j))
+
         rows = [1, 0, -1, 0]
         cols = [0, -1, 0, 1]
+
         while queue:
-            curri, currj = queue.popleft()
-            for x in range(len(rows)):
-                if self.isSafe(grid, visited, curri+rows[x], currj+cols[x]):
-                    visited.add((curri+rows[x], currj+cols[x]))
-                    queue.append((curri+rows[x], currj+cols[x]))
+            currx, curry = queue.popleft()
 
-    def isSafe(self, grid, visited, x, y):
+            for i in range(len(rows)):
+                if self.isSafe(grid, currx+rows[i], curry+cols[i], visited):
+                    visited.add((currx+rows[i], curry+cols[i]))
+                    queue.append((currx+rows[i], curry+cols[i]))
+    
+    def isSafe(self, grid, x, y, visited):
+        return (
+            x >= 0 and x < len(grid) and
+            y >= 0 and y < len(grid[0]) and
+            grid[x][y] == '1' and (x,y) not in visited
+        )
 
-        return 0 <= x and x < len(grid) and 0 <= y and y < len(grid[0]) and grid[x][y] == "1" and (x,y) not in visited
