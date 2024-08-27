9a4203befeea748322b37c7568fa309bea08199e
Arushi Sharma <arushi.net@outlook.com>
2024-08-27 17:56:56 +0000

    [LeetCode Sync] Runtime - 47 ms (6.30%), Memory - 16.5 MB (66.63%)

--- my-folder/0031-next-permutation/solution.py
+++ my-folder/0031-next-permutation/solution.py
@@ -2,50 +2,47 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        find the first smaller element from right side
+        swap it with the smallest greater element seen till now on right side
+        rest write in sorted order
         """
+        # return []
 
-        i = len(nums) - 1
-
-        while i > 0:
-            if nums[i] > nums[i-1]:
+        i = len(nums)-2
+        pivot = -1
+        while i >= 0:
+            if nums[i] < nums[i+1]:
+                pivot = i
                 break
-            else:
-                i -= 1
-        if i == 0:
-            # all decreasing
+            i -= 1
+        
+        if pivot == -1:
             nums.reverse()
             return
-        
-        # if i == len(nums) - 1:
-        #     nums[i], nums[0] = nums[0], nums[i]
-        #     j,k = 1,len(nums)-1
-        #     while j<k:
-        #         nums[j],nums[k] = nums[k], nums[j]
-        #         j += 1
-        #         k -= 1
-
-        #     return
-
-        j = i
-        # swap i-1 with j
-        first_greater_elem = 100
-        first_greater_elem_idx = -1
-        while j < len(nums):
-            if nums[j] > nums[i-1]:
-                first_greater_elem = min(first_greater_elem, nums[j])
-                if first_greater_elem == nums[j]:
-                    first_greater_elem_idx = j
-            j += 1
-        # print(first_greater_elem_idx, i-1)
-        if first_greater_elem_idx == -1:
-            first_greater_elem_idx = i
-        nums[first_greater_elem_idx], nums[i-1] = nums[i-1], nums[first_greater_elem_idx]
-
-        k = len(nums)-1
-        while i<k:
-            nums[i],nums[k] = nums[k], nums[i]
+        # [1,2,3,2]
+        # pivot = 1
+        # find the smallest greater element 
+        i = pivot + 1
+        min_greater_element_idx = pivot + 1
+        while i < len(nums):
+            if nums[i] > nums[pivot] and nums[min_greater_element_idx] >= nums[i]:
+                min_greater_element_idx = i
             i += 1
-            k -= 1
-        return 
+        
+        nums[pivot], nums[min_greater_element_idx] = nums[min_greater_element_idx], nums[pivot]
 
+        left = pivot + 1
+        right = len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        
+
+
+
+
+        
+        
 
