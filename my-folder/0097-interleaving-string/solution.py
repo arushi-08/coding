8bf284ad70abd505dfb99c74088f95034f520bd7
Arushi Sharma <arushi.net@outlook.com>
2024-09-07 18:46:17 +0000

    [LeetCode Sync] Runtime - 43 ms (49.93%), Memory - 18.1 MB (11.67%)

--- my-folder/0097-interleaving-string/solution.py
+++ my-folder/0097-interleaving-string/solution.py
@@ -1,29 +1,41 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        memo = {}
-        def dfs(s1ptr, s2ptr, s3ptr):
-            nonlocal memo
+        
+        # s1 = 'aabcc'
+        # s2 = 'dbbca'
+        # s3 = 'aadbbcbcac'
 
-            if (s1ptr, s2ptr, s3ptr) in memo:
-                return memo[(s1ptr, s2ptr, s3ptr)]
+        # if both s1ptr, s2ptr point to same ptr on s3
+        # increment both and check what is the next char on s3
+        # decrement the ptr on s1/s2, i.e., not matching next char of s3
 
-            if s1ptr == len(s1) and s2ptr == len(s2) and s3ptr == len(s3):
-                memo[(s1ptr, s2ptr, s3ptr)] = True
+        # dp method
+        self.memo = {}
+
+        def dfs(p1,p2,p3):
+            if p1 == len(s1) and p2 == len(s2) and p3 == len(s3):
                 return True
-            
-            if s3ptr == len(s3):
-                memo[(s1ptr, s2ptr, s3ptr)] = False
+            if p3 == len(s3) or (p1 == len(s1) and p2 == len(s2)):
                 return False
 
-            if s1ptr < len(s1) and s1[s1ptr] == s3[s3ptr]:
-                if dfs(s1ptr+1, s2ptr, s3ptr+1):
-                    return True
-            
-            if s2ptr < len(s2) and s2[s2ptr] == s3[s3ptr]:
-                if dfs(s1ptr, s2ptr+1, s3ptr+1):
-                    return True
-            
-            memo[(s1ptr, s2ptr, s3ptr)] = False
-            return False
-        
+            if (p1,p2,p3) in self.memo:
+                return self.memo[(p1,p2,p3)]
+
+            ans = False
+            not_eql = False
+
+            if p1 < len(s1):
+                if s1[p1] == s3[p3]:
+                    ans |= dfs(p1+1, p2, p3+1)
+                else:
+                    ans |= False
+            if p2 < len(s2):
+                if s2[p2] == s3[p3]:
+                    ans |= dfs(p1, p2+1, p3+1)
+                else:
+                    ans |= False
+
+            self.memo[(p1,p2,p3)] = ans
+            return ans
+
         return dfs(0,0,0)
