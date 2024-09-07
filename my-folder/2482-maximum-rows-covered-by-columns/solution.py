1c8f330f33d0ee096425e6dc3c9dd55da97b5376
Arushi Sharma <arushi.net@outlook.com>
2024-09-07 19:47:33 +0000

    [LeetCode Sync] Runtime - 5629 ms (5.00%), Memory - 16.6 MB (61.44%)

--- my-folder/2482-maximum-rows-covered-by-columns/solution.py
+++ my-folder/2482-maximum-rows-covered-by-columns/solution.py
@@ -1,48 +1,36 @@
 class Solution:
     def maximumRows(self, matrix: List[List[int]], numSelect: int) -> int:
         
-        # backtracking
-
-        bitmask_mat = [0] * len(matrix)
-        for i in range(len(matrix)):
-            r = 0
-            for j in range(len(matrix[i])):
-                r = 2*r + matrix[i][j]
-            bitmask_mat[i] = r
+        # do backtrack on columns selected
+        # store max_rows_covered
 
         max_rows_covered = 0
+        selected_cols = set()
 
-        def backtrack(selected_cols, idx, count):
+        def dfs(idx, selected_cols):
             nonlocal max_rows_covered
 
-            if count == numSelect:
-
+            if len(selected_cols) == numSelect:
                 n_rows_covered = 0
-                # for loop
-                for r in bitmask_mat:
-                    if r & selected_cols == r:
+
+                for i in range(len(matrix)):
+                    row_covered = True
+                    for j in range(len(matrix[0])):
+                        if matrix[i][j] and j not in selected_cols:
+                            row_covered = False
+                    if row_covered:
                         n_rows_covered += 1
-                    
-                max_rows_covered = max(max_rows_covered, n_rows_covered)
+                
+                max_rows_covered = max(
+                    max_rows_covered, n_rows_covered
+                    )
                 return
             
-            for col in range(idx, len(matrix[0])):
-                if selected_cols & 1 << col == 0:
-                    backtrack(selected_cols | 1 << col, col+1, count+1)
- 
-        
-        selected_cols = [0] * len(matrix[0])
-        count = 0
-        backtrack(0, 0, count)
+            for i in range(idx, len(matrix[0])):
+                if i not in selected_cols:
+                    selected_cols.add(i)
+                    dfs(idx+1, selected_cols)
+                    selected_cols.remove(i)
 
+        dfs(0, selected_cols)
         return max_rows_covered
-# [
-    # [0,0,1],
-    # [1,0,0],
-    # [0,0,0]
-# ]
-
-
-
-
-
