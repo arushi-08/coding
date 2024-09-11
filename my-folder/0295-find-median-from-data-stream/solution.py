286fce2e2e618cb19c6f23f31071f5f2630264e2
Arushi Sharma <arushi.net@outlook.com>
2024-09-11 20:18:31 +0000

    [LeetCode Sync] Runtime - 377 ms (15.23%), Memory - 38 MB (92.78%)

--- my-folder/0295-find-median-from-data-stream/solution.py
+++ my-folder/0295-find-median-from-data-stream/solution.py
@@ -3,33 +3,28 @@ class MedianFinder:
     def __init__(self):
         self.minheap = []
         self.maxheap = []
-        self.size = 0
 
     def addNum(self, num: int) -> None:
-        if self.maxheap:
-            if self.maxheap[0] <= num:
-                heappush(self.minheap, -heappop(self.maxheap))
-                heappush(self.maxheap, num)
-            else:
-                heappush(self.minheap, -num)
-        else:
-            heappush(self.minheap, -num)
         
-        while len(self.minheap) < len(self.maxheap):
+        if not self.maxheap or num <= -self.maxheap[0]:
+            heappush(self.maxheap, -num)
+        else:
+            heappush(self.minheap, num)
+
+        while len(self.maxheap)  > len(self.minheap) + 1:
             heappush(self.minheap, -heappop(self.maxheap))
         
-        while len(self.minheap) - len(self.maxheap) > 1:
+        while len(self.minheap) > len(self.maxheap):
             heappush(self.maxheap, -heappop(self.minheap))
-        self.size += 1
+        
+
 
     def findMedian(self) -> float:
+        if len(self.maxheap) + len(self.minheap) & 1 == 1:
+            return -self.maxheap[0]
+        else:
+            return (-self.maxheap[0] + self.minheap[0])/2
 
-        if self.size & 1 == 1:
-            return -self.minheap[0]
-            
-        return (self.maxheap[0] - self.minheap[0])/2
-        
-        
         
 
 
