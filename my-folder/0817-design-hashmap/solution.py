0e03ae4bbf065fd8b06719dfedc6c5d071fc0395
Arushi Sharma <arushi.net@outlook.com>
2024-09-11 00:13:50 +0000

    [LeetCode Sync] Runtime - 185 ms (21.53%), Memory - 19.8 MB (72.36%)

--- my-folder/0817-design-hashmap/solution.py
+++ my-folder/0817-design-hashmap/solution.py
@@ -1,29 +1,51 @@
+class Node:
+    def __init__(self, key, val=0):
+        self.key = key
+        self.val = val
+        self.next = None
+
 class MyHashMap:
 
     def __init__(self):
-        self.const = 1000
-        self.d = [0] * (self.const + 1)
-
-    def key(self, k):
-        return divmod(k, self.const)
+        self.size_map = 1000
+        self.map = [Node(-1) for _ in range(self.size_map)]
 
     def put(self, key: int, value: int) -> None:
-        k1, k2 = self.key(key)
-        if self.d[k1] == 0:
-            self.d[k1] = [-1] * self.const
-        self.d[k1][k2] = value
+        node = self.map[key % self.size_map]
+        curr = node
+        while curr:
+            if curr.key == key:
+                curr.val = value
+                self.map[key % self.size_map] = node
+                return
+            
+            if not curr.next:
+                curr.next = Node(key, value)
+            else:
+                curr = curr.next
+
+        self.map[key % self.size_map] = node
 
     def get(self, key: int) -> int:
-        k1, k2 = self.key(key)
-        if self.d[k1] == 0:
-            return -1
-        return self.d[k1][k2]
+        node = self.map[key % self.size_map]
+        while node and node.key != key:
+            node = node.next
+        if node:
+            return node.val
+        return -1
 
     def remove(self, key: int) -> None:
-        k1, k2 = self.key(key)
-        if self.d[k1] == 0:
-            return
-        self.d[k1][k2] = -1
+        node = self.map[key % self.size_map]
+        prev = node
+        curr = node.next
+        while curr and curr.key != key:
+            curr = curr.next
+            prev = prev.next
+
+        if curr and curr.key == key:
+            prev.next = curr.next
+
+        self.map[key % self.size_map] = node
 
 
 # Your MyHashMap object will be instantiated and called as such:
