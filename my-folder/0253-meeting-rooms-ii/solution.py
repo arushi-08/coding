a84191955c282367293c1238e2d529340c17327a
Arushi Sharma <arushi.net@outlook.com>
2024-09-11 16:03:51 +0000

    [LeetCode Sync] Runtime - 74 ms (5.27%), Memory - 19.9 MB (55.99%)

--- my-folder/0253-meeting-rooms-ii/solution.py
+++ my-folder/0253-meeting-rooms-ii/solution.py
@@ -1,18 +1,16 @@
-from heapq import heapify, heappush, heappop
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        if not intervals: return 0
-
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort(key=lambda x: x[0])
 
         end_time_heap = []
         heappush(end_time_heap, intervals[0][1])
 
         for i in range(1, len(intervals)):
+
             if intervals[i][0] >= end_time_heap[0]:
                 heappop(end_time_heap)
             
             heappush(end_time_heap, intervals[i][1])
-
+        
         return len(end_time_heap)
