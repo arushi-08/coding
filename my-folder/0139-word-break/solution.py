dd23ea7fe155809569ec668f379ff726f5b6d35a
Arushi Sharma <arushi.net@outlook.com>
2024-09-11 21:42:50 +0000

    [LeetCode Sync] Runtime - 41 ms (5.09%), Memory - 16.6 MB (90.34%)

--- my-folder/0139-word-break/solution.py
+++ my-folder/0139-word-break/solution.py
@@ -1,33 +1,25 @@
 class Solution:
     def __init__(self):
         self.memo = {}
-        
+
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        if not s: return True
+        if s == '':
+            return True
         
-        if s in wordDict: return True
-        
-        dp = [False] * (len(s)+1)
-        dp[0] = True
-        
-        for i in range(len(s)+1):
-            for j in range(i):
-                if dp[j] and s[j:i] in wordDict:
-                    dp[i] = True
-                    break
-                
-        return dp[-1]
-                
-            
-        
-#         if s in self.memo: return self.memo[s]
-        
-#         ans = True
-#         for word in wordDict:
-#             if word == s[:len(word)]:
-#                 self.memo[s] = self.wordBreak(s[len(word):], wordDict)
-#                 if self.memo[s]:
-#                     return self.memo[s]
-                
-#         return False
+        if s in self.memo:
+            return self.memo[s]
+
+        for word in wordDict:
+            if s == word:
+                return True
+            if s.startswith(word):
+                if self.wordBreak(s[len(word):], wordDict):
+                    self.memo[s] = True
+                    return True
+                    
+        self.memo[s] = False
+        return False
+
+
+
