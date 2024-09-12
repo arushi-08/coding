aafe04fe2ecdcf306f99087d6e4c752dbaf4a482
Arushi Sharma <arushi.net@outlook.com>
2024-09-12 16:48:33 +0000

    [LeetCode Sync] Runtime - 34 ms (5.31%), Memory - 16.5 MB (56.78%)

--- my-folder/0394-decode-string/solution.py
+++ my-folder/0394-decode-string/solution.py
@@ -1,35 +1,34 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         
-        # iterate on s
-        # store elements in stack
-        # when we arrive on close bracket
-        # pop out elements acc to count given outside bracket
+        # use stack to store substrings
+        # store digits & substrings in stack
+        # when i=']':
+        #       pop stack -> substring
+        #       substring * digits -> stack
+        # in the end read from stack
+
 
         stack = []
-        number = ''
-        enter_stack = False
+        i = 0
 
-        for i in range(len(s)):
-            if s[i].isdigit():
-                number += s[i]
-            elif s[i] == '[':
-                stack.append(number)
-                stack.append(s[i])
-                number = ''
-            elif s[i] == ']':
-                string_element = ''
+        while i < len(s):
+            if s[i] == ']':
+                substring = ''
                 while stack[-1] != '[':
-                    string_element = stack.pop() + string_element
+                    substring = stack.pop() + substring
                 if stack[-1] == '[':
                     stack.pop()
-                    old_num = stack.pop()
-                stack.append(int(old_num) * string_element)
-
-            else: # s[i] is char
+                digit = ''
+                while stack and stack[-1].isdigit():
+                    digit = stack.pop() + digit
+                stack.append(substring * int(digit))
+            else:
                 stack.append(s[i])
+            i += 1
 
         ans = ''
         while stack:
             ans = stack.pop() + ans
+        
         return ans
