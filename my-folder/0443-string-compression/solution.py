645f0ba22aa7999381017a2dfe3503a63b5353e3
Arushi Sharma <arushi.net@outlook.com>
2024-09-12 16:57:30 +0000

    [LeetCode Sync] Runtime - 53 ms (14.84%), Memory - 16.5 MB (94.51%)

--- my-folder/0443-string-compression/solution.py
+++ my-folder/0443-string-compression/solution.py
@@ -1,24 +1,25 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        
-        if len(chars)==1: return 1
-        i = 0
-        j = 0
-        count = 0
-        compressed_str = ''
-        for j in range(len(chars)):
-            if not compressed_str or chars[j] != compressed_str[-1]:
+        # iterate 
+        # when chars[i] != curr_char
+        #   store curr_char and count in chars
+        # 
+
+        curr_char = chars[0]
+        count = 1
+        ptr = 0
+        chars += '  '
+        for i in range(1, len(chars)):
+            if chars[i] != curr_char:
+                chars[ptr] = curr_char
+                ptr += 1
                 if count > 1:
-                    compressed_str += str(count)
-                compressed_str += chars[j]
-                count = 1
+                    for c in str(count):
+                        chars[ptr] = c
+                        ptr += 1
+                    count = 1
+                curr_char = chars[i]
             else:
                 count += 1
-        if count > 1:
-            compressed_str += str(count)
         
-        for j in range(len(compressed_str)):
-            chars[j] = compressed_str[j]
-        
-        return len(compressed_str)
-
+        return ptr
