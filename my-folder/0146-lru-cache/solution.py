b6eba10b7a9737c29ec60607476cd8ea139c77f8
Arushi Sharma <arushi.net@outlook.com>
2024-09-14 18:27:41 +0000

    [LeetCode Sync] Runtime - 622 ms (7.48%), Memory - 78.5 MB (21.69%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,40 +1,89 @@
-class LRUCache:
 
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}
-        self.heap = []
-        self.use = defaultdict(int)
-        self.counter = 0
+from threading import Lock
 
-    def get(self, key: int) -> int:
-        if key in self.cache:
-            self.use[key] = self.counter
-            self.counter += 1
-            return self.cache[key]
-            
-        return -1
 
-    def put(self, key: int, value: int) -> None:
+class Node:
+    def __init__(self, key=0, val=0, prev=None, next=None) -> None:
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
         
-        if key in self.cache:
-            self.cache[key] = value
-            self.use[key] = self.counter
-            self.counter += 1
-            return
+class LRUCache: 
+    '''
+    self.hmap:
+        {key: Node(key, val, next, prev)}
+    '''
+    def __init__(self, capacity) -> None:
+        if capacity < 0:
+            raise ValueError('Capacity must be positive')
+        self.capacity = capacity
+        self.hmap = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.lock = Lock()
 
-        if len(self.cache) >= self.capacity:
-            while self.heap[0][0] != self.use[self.heap[0][1]]:
-                _, k = heappop(self.heap)
-                heappush(self.heap, (self.use[k], k))
-            _, k = heappop(self.heap)
-            del self.cache[k]
-            del self.use[k]
 
-        self.cache[key] = value
-        self.use[key] = self.counter
-        heappush(self.heap, (self.use[key], key))
-        self.counter += 1
+    def get(self, key):
+        with self.lock:
+            if key in self.hmap:
+                self._move_to_head(self.hmap[key])
+                return self.hmap[key].val
+            
+            return -1
+
+
+    def put(self, key, value):
+        with self.lock:
+            if key in self.hmap:
+                node = self.hmap[key]
+                self._move_to_head(node)
+                if value != node.val:
+                    self.hmap[key].val = value
+                    self.head.val = value
+            else:
+                if len(self.hmap) == self.capacity:
+                    tail = self._pop_tail()
+                    del self.hmap[tail.key]
+                node = Node(key=key, val=value, next=self.head)
+                self.hmap[key] = node
+                self._add_head(node)
+
+
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_head(node)
+        
+
+    def _remove_node(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+    
+
+    def _pop_tail(self):
+        '''
+        remove node before dummy tail
+        '''
+        node = self.tail.prev
+        if node == self.head:
+            return None
+        self._remove_node(node)
+        return node
+
+
+    def _add_head(self, node):
+        '''
+        add a node after dummy head
+        '''
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
