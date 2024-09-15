a748bc106c9a8ba17308b12c1366db377cdfef21
Arushi Sharma <arushi.net@outlook.com>
2024-09-15 17:26:34 +0000

    [LeetCode Sync] Runtime - 119 ms (13.05%), Memory - 18 MB (61.64%)

--- my-folder/0283-move-zeroes/solution.py
+++ my-folder/0283-move-zeroes/solution.py
@@ -3,11 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # algo: slow ptr and i if i != 0: swap slow ptr and i
-        # 0 1 0 3 12 swap 1 0
-        ptr = 0
+        
+        zero_ptr = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[ptr] = nums[ptr], nums[i]
-                ptr += 1
-             
+                nums[zero_ptr], nums[i] = nums[i], nums[zero_ptr]
+                zero_ptr += 1
+                
