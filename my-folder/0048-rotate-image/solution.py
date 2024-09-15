6a26358ed6a65c6633d844ab42d9187d3b4030ad
Arushi Sharma <arushi.net@outlook.com>
2024-09-15 19:05:48 +0000

    [LeetCode Sync] Runtime - 47 ms (5.85%), Memory - 16.6 MB (76.02%)

--- my-folder/0048-rotate-image/solution.py
+++ my-folder/0048-rotate-image/solution.py
@@ -3,18 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        up = 0
-        down = len(matrix) - 1
-        while(up < down):
-            matrix[up], matrix[down] = matrix[down], matrix[up]
-            up += 1
-            down -= 1
+        # modify image in place
+        # col is becoming row
+        # row is becoming col
         
-        for i in range(len(matrix)):
-            for j in range(i):
-                if i!=j:
-                    temp = matrix[i][j]
-                    matrix[i][j] = matrix[j][i]
-                    matrix[j][i] = temp
-                
-        return matrix
+        # transpose
+        # reverse row
+
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+
+
