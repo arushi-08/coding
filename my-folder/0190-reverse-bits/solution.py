430a2d0389309329b0f70d1faad6709ec9ef3d0e
Arushi Sharma <arushi.net@outlook.com>
2024-09-15 15:48:01 +0000

    [LeetCode Sync] Runtime - 37 ms (49.64%), Memory - 16.4 MB (91.85%)

--- my-folder/0190-reverse-bits/solution.py
+++ my-folder/0190-reverse-bits/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         
-        
-        ans = 0
-        for i in range(32):
-            
-            ans = (ans << 1) + (n & 1)
-            n >>= 1
-        
-        return ans
+        reverse = 0
+        j = 0
+        for i in range(31, -1, -1):
+            mask = 1 << i
+            if (n & mask) != 0:
+                set_mask = 1 << j
+                reverse |= set_mask
+            j += 1
+
+        return reverse
