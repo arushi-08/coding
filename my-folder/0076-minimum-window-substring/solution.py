5bda1e12c999f4dca806274a16b70934678653de
Arushi Sharma <arushi.net@outlook.com>
2024-09-17 00:30:21 +0000

    [LeetCode Sync] Runtime - 121 ms (40.55%), Memory - 17.3 MB (30.33%)

--- my-folder/0076-minimum-window-substring/solution.py
+++ my-folder/0076-minimum-window-substring/solution.py
@@ -1,42 +1,50 @@
-from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        if len(t) > len(s): return ""
-        # 2 pointer
+        
+        st = 0
         tmap = Counter(t)
+        min_length = len(s)
+        min_substring = s
+        tlen = len(t)
+        ed = 0
         smap = {}
-        st, ed = 0, 0
-        minwindow = len(s)+1
-        t_len = len(t)
         st_ans, ed_ans = -1,-1
+
         while ed < len(s):
             if s[ed] in tmap:
-                if s[ed] not in smap:
-                    smap[s[ed]] = 1
-                    t_len -= 1
-                elif smap[s[ed]] < tmap[s[ed]]:
-                    smap[s[ed]] += 1
-                    t_len -= 1
+                if s[ed] not in smap or smap[s[ed]] < tmap[s[ed]]:
+                    smap[s[ed]] = smap.get(s[ed], 0) + 1
+                    tlen -= 1 
                 else:
                     smap[s[ed]] += 1
             else:
                 smap[s[ed]] = smap.get(s[ed], 0) + 1
 
-            while t_len == 0:
-                if ed - st + 1 <= minwindow:
+            # s='ADOBECODEBANC'
+            # st = 0
+            # ed = 5
+            while tlen == 0:
+                if min_length >= ed - st + 1:
                     st_ans = st
                     ed_ans = ed
-                    minwindow = ed - st + 1
+                    min_length = ed-st+1
                 smap[s[st]] -= 1
                 
                 if s[st] in tmap and smap[s[st]] < tmap[s[st]]:
-                    t_len += 1
-
+                    tlen += 1
+                
                 st += 1
+            
             ed += 1
+
         if st_ans == -1:
-            return ""
+            return ''
         
         return s[st_ans:ed_ans+1]
-                    
+
+
+                
+
+
+
+
