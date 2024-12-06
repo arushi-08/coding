c63373221394657196fc6f3783f3aa33ba81d9ed
Arushi Sharma <arushi.net@outlook.com>
2024-12-06 21:44:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.5 MB (5.30%)

--- my-folder/0091-decode-ways/solution.py
+++ my-folder/0091-decode-ways/solution.py
@@ -1,59 +1,39 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        # if s[0] == '0': return 0
-        # # backtracking?
+        # return num of ways you can decode string
+        # 12
+        # 1 -> A
+        # 2 -> B
+        # 12 -> L
+        # backtracking
 
-        # ans = []
-        # def dfs(res, idx):
-        #     if idx == len(s):
-        #         ans.append(res.copy())
-        #         return
-            
-        #     if s[idx] == '0':
-        #         return len(ans)
 
-        #     if idx < len(s) - 1 and int(s[idx:idx+2]) <= 26:
-        #         if s[idx+1] == '0':
-        #             res.append(s[idx:idx+2])
-        #             dfs(res, idx+2)
-        #             res.pop()
-        #         else:
-        #             for i in range(2):
-        #                 res.append(s[idx:idx+i+1])
-        #                 dfs(res, idx+i+1)
-        #                 res.pop()
-            
-        #     else:
-        #         res.append(s[idx])
-        #         dfs(res, idx+1)
-        #         res.pop()
-            
-        # res = []
-        # dfs(res, 0)
-        # # print(ans)
-        # return len(ans)
-            
-            
-        # dp problem
-        self.memo = {}
-        return self.helper(s)
-    
-    def helper(self, s):
-        if s.startswith("0"):
-            return 0
-        
-        if not s or len(s) == 1:
-            return 1
-        
-        if s in self.memo:
-            return self.memo[s]
+        # how do we go about it?
+        # start at the beginning, select 1 or select 2
+        # 2 choices
+        # form the result, but 2^100
 
-        first = self.helper(s[1:])
-        second = 0
-        if int(s[0:2]) <= 26:
-            second = self.helper(s[2:])
+        # backtracking seems the only option
+        # let's start
 
-        self.memo[s] = first + second
-        return self.memo[s]
+        memo = {}
+        def dfs(i, s):
+            if i == len(s):
+                return 1
+            
+            if s[i] == '0':
+                return 0
+
+            if i in memo:
+                return memo[i]
+
+            res = dfs(i+1, s)
+            if s[i] != '0' and i+1 < len(s) and s[i:i+2] < '27':
+                res += dfs(i+2, s)
+            
+            memo[i] = res
+            return res
+
+        return dfs(0, s)
 
