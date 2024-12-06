e5f9fb556d0bcddee64220892b75afa84d54a98d
Arushi Sharma <arushi.net@outlook.com>
2024-12-06 16:34:06 +0000

    [LeetCode Sync] Runtime - 55 ms (5.32%), Memory - 18.9 MB (7.57%)

--- my-folder/0042-trapping-rain-water/solution.py
+++ my-folder/0042-trapping-rain-water/solution.py
@@ -1,19 +1,28 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # prefix max & suffix max
-        # curr water trapped = min(prefixmax, suffixmax) - height[i]
-        n = len(height)
-        prefix_max = [0] * n
-        suffix_max = [0] * n
-        prefix_max[0] = height[0]
-        suffix_max[-1] = height[-1]
+        # i already saw prefix sum
 
-        for i in range(1, n):
-            prefix_max[i] = max(height[i], prefix_max[i-1])
-            suffix_max[n-1-i] = max(height[n-1-i], suffix_max[n-i])
+        # compute how much water can be trapped
 
-        trapped_water = 0
-        for i in range(len(height)):
-            trapped_water += min(prefix_max[i], suffix_max[i]) - height[i]
+        # [0,1,0,2,1,0,1,3,2,1,2,1]
+        # [0,0,1,1,2,2,2,3,3,3,3,3]
+        # [3,3,3,3,3,3,3,3,2,2,1,0]
         
-        return trapped_water
+        prefix_max = [0] * len(height)
+        for i in range(1, len(height)):
+            if height[i] < max(height[i-1], prefix_max[i-1]):
+                prefix_max[i] = max(height[i-1], prefix_max[i-1])
+        
+        suffix_max = [0] * len(height)
+        for i in range(len(height)-2,-1,-1):
+            if height[i] < max(height[i+1], suffix_max[i+1]):
+                suffix_max[i] = max(height[i+1], suffix_max[i+1])
+        
+        water = 0
+        for i in range(len(height)):
+            water += max(0, min(prefix_max[i], suffix_max[i]) - height[i])
+        
+        return water
+
+
+
