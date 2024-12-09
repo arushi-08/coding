2c44155f9bd263b636484c90e2c8aa0f6b1cabe6
Arushi Sharma <arushi.net@outlook.com>
2024-12-09 17:59:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.3 MB (14.17%)

--- my-folder/0008-string-to-integer-atoi/solution.py
+++ my-folder/0008-string-to-integer-atoi/solution.py
@@ -2,26 +2,38 @@ class Solution:
     def myAtoi(self, s: str) -> int:
         
         s = s.strip()
-        int_list = []
+        neg = False
+        if s.startswith('-'):
+            neg = True
+            s = s[1:]
+        elif s.startswith('+'):
+            s = s[1:]
         
+        ans = ''
         for i in range(len(s)):
-            if (ord(s[i]) >= ord('0') and ord(s[i]) <= ord('9')) or (i==0 and s[i] in ['-', '+']):
-                int_list.append(s[i])
+            if s[i] == '0' and not ans:
+                continue
+            
+            if s[i].isdigit():
+                ans += s[i]
             else:
                 break
+
+        if not ans:
+            return 0
         
-        uplimit = 2**31
-        downlimit = -2**31
+        ans = int(ans)
+
+        if neg:
+            if -ans < -2**31:
+                return -2**31
+            return -ans
+
+        
+        if ans > (2**31)-1:
+            print((2**31) - 1)
+            return (2**31) - 1
+
+        return ans
         
 
-        if int_list in (['-'], ['+']):
-            return 0
-        if int_list:
-            ans = int(''.join(int_list))
-            if ans > uplimit - 1:
-                ans = uplimit - 1
-            elif ans < downlimit:
-                ans = downlimit
-                
-            return ans
-        return 0
