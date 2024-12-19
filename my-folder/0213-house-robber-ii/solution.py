03cfef80f0ffbd1fcfb94a84ae3b5bf85f8ade97
Arushi Sharma <arushi.net@outlook.com>
2024-12-19 20:04:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (8.57%)

--- my-folder/0213-house-robber-ii/solution.py
+++ my-folder/0213-house-robber-ii/solution.py
@@ -1,30 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        # [2,3,2]
-        # base case
-        #   [] -> return 0
-        # max(rob(nums, idx+2)+currentamount, rob(nums, idx+1))
-        if len(nums) == 1:
-            return nums[0]
-            
-        memo = {}
-        memo2 = {}
-        return max(self.helper(nums[:-1], 0, memo),
-        self.helper(nums, 1, memo2)
-        )
-    
-    def helper(self, nums, idx, memo):
-
-        if idx >= len(nums):
-            return 0
+        # (i + n - 1)%n
+        if len(nums)==1: return nums[0]
         
-        if idx in memo:
-            return memo[idx]
-        
-        condn1 = self.helper(nums, idx+2, memo) + nums[idx]
-        condn2 = self.helper(nums, idx+1, memo)
+        return max(self.helper(nums[1:]), self.helper(nums[:-1]))
 
-        memo[idx] = max(condn1, condn2)
+    def helper(self, nums):
 
-        return memo[idx] 
+        if not nums: return 0
+
+        if len(nums)==1: return nums[0]
+
+        prev2 = nums[0]
+        prev = max(nums[1], nums[0])
+
+        for i in range(2, len(nums)):
+            curr = max(prev, nums[i] + prev2)
+            prev2 = prev
+            prev = curr
+
+        return prev
