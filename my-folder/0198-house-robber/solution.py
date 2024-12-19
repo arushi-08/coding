01aa533e43262339f4ae77a5cfaea7491f5ca587
Arushi Sharma <arushi.net@outlook.com>
2024-12-19 19:44:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (8.94%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,28 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        # self.memo = {}
-        # return self.helper(nums, 0)
+        # non adj sum
+        if len(nums)==1: return nums[0]
+        prev2 = nums[0]
+        prev = max(nums[0], nums[1])
 
-        if len(nums) <= 2:
-            return max(nums)
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
-        dp[1] = nums[1]
-        for i in range(2, len(nums)):
-            for j in range(2,i+1):
-                dp[i] = max(dp[i-j]+nums[i], dp[i])
+        for i in range(2,len(nums)):
+            curr= max(prev, nums[i] + prev2)
+            prev2 = prev
+            prev = curr
 
-        return max(dp)
+        return prev
 
-    def helper(self, nums, index):
-
-        if index >= len(nums):
-            return 0
-
-        if index in self.memo:
-            return self.memo[index]
-
-        self.memo[index] = max(self.helper(nums, index+2)+nums[index], self.helper(nums, index+1))
-
-        return self.memo[index]
