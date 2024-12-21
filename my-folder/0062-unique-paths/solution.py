e167554551172ac7937050b4193541720538d8ee
Arushi Sharma <arushi.net@outlook.com>
2024-12-21 01:44:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (5.56%)

--- my-folder/0062-unique-paths/solution.py
+++ my-folder/0062-unique-paths/solution.py
@@ -1,20 +1,20 @@
 class Solution:
-
-    def __init__(self):
-        self.memo = {}
-
     def uniquePaths(self, m: int, n: int) -> int:
         
+        if m == 1 and n == 1: return 1
 
-        # move down or right
-        if m == 1 or n == 1:
-            return 1
-        
-        if m == 2 and n == 2:
-            return 2
-        
-        if (m,n) in self.memo:
-            return self.memo[(m,n)]
+        # dp = [[0] * n for _ in range(m)]
+        prev_m = [0]*n
+        for i in range(m):
+            prev_n = [0]*n
+            for j in range(n):
+                if i < 0 and j < 0:
+                    prev_n[j] = 0
+                elif i == 0 and j == 0:
+                    prev_n[j] = 1
+                else:
+                    prev_n[j] = prev_m[j] + prev_n[j-1]
+            prev_m = prev_n
 
-        self.memo[(m,n)] =  self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
-        return self.memo[(m,n)]
+        return prev_m[-1]
+
