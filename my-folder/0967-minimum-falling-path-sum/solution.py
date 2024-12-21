39097ba469cbee07d73ee3d2986aa4fd426e39e2
Arushi Sharma <arushi.net@outlook.com>
2024-12-21 22:43:50 +0000

    [LeetCode Sync] Runtime - 15 ms (91.82%), Memory - 18.6 MB (31.97%)

--- my-folder/0967-minimum-falling-path-sum/solution.py
+++ my-folder/0967-minimum-falling-path-sum/solution.py
@@ -1,46 +1,27 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         
-        rows = len(matrix)
-        cols = len(matrix[0])
+        # return min sum of any falling path through matrix
 
-        dp = [[0] * (cols+2) for _ in range(rows+1)]
-        
-        for j in range(1,rows+1):
-            dp[j][0] = float('inf')
-            dp[j][cols+1] = float('inf')
-        
-        # for j in range(col+1):
-        #     dp[0][j] = float('inf')
+        # below or diag left/right
 
-        for j in range(1,rows+1):
-            for k in range(1,cols+1):
-                dp[j][k] = min(
-                    dp[j-1][k-1],
-                    dp[j-1][k],
-                    dp[j-1][k+1]
-                    ) + matrix[j-1][k-1]
-        # print('dp', dp)
-        ans = float('inf')
-        for i in range(1, cols+1):
-            ans = min(ans, dp[rows][i])
-        return ans
-    
-    def helper(self, row, col, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
 
-        if row == len(matrix):
-            return 0
-        
-        if col == len(matrix[0]) or col < 0:
-            return float('inf')
-        
-        if (row,col) in self.memo:
-            return self.memo[(row, col)]
+        prev = [float('inf')] * n
 
-        self.memo[(row, col)] = min(
-            self.helper(row+1, col-1, matrix), 
-            self.helper(row+1, col, matrix),
-            self.helper(row+1, col+1, matrix)
-        ) + matrix[row][col]
+        for i in range(m):
+            curr = [float('inf')] * n
 
-        return self.memo[(row, col)]
+            for j in range(n):
+                if i == 0:
+                    curr[j] = matrix[i][j]
+                elif j == 0:
+                    curr[j] = min(prev[j], prev[j+1]) + matrix[i][j]
+                elif j == n-1:
+                    curr[j] = min(prev[j-1], prev[j]) + matrix[i][j]
+                else:
+                    curr[j] = min(prev[j-1], prev[j], prev[j+1]) + matrix[i][j]
+            
+            prev = curr
+        return min(prev)
