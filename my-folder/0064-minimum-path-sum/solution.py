69d626084549ff1afe66e46465bbb0884db3e9f6
Arushi Sharma <arushi.net@outlook.com>
2024-12-21 21:28:20 +0000

    [LeetCode Sync] Runtime - 10 ms (94.33%), Memory - 18.5 MB (90.14%)

--- my-folder/0064-minimum-path-sum/solution.py
+++ my-folder/0064-minimum-path-sum/solution.py
@@ -1,21 +1,23 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
-        row = len(grid)
-        col = len(grid[0])
-        dp = [[0]*(col+1) for _ in range(row+1)]
-        
-        for i in range(row+1):
-            dp[i][0] = float('inf')
-        for i in range(col+1):
-            dp[0][i] = float('inf')
+        # top left to bottom right
+        # move down or right
+        m = len(grid)
+        n = len(grid[0])
 
-        dp[1][1] = grid[0][0]
+        prev_n1 = [float('inf')] * n
 
-        for i in range(1, row+1):
-            for j in range(1, col+1):
-                if (i,j) != (1,1):
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i-1][j-1]
-                    # print(dp[i][j])
-        # print(dp)
-        return dp[row][col]
+        for i in range(m):
+            prev_n2 = [float('inf')] * n
+            for j in range(n):
+
+                if i == 0 and j == 0:
+                    prev_n2[j] = grid[i][j]
+                
+                else:
+                    prev_n2[j] = min(prev_n1[j], prev_n2[j-1]) + grid[i][j]
+            prev_n1 = prev_n2
+        
+        return prev_n1[n-1]
+
+        # 1 3
