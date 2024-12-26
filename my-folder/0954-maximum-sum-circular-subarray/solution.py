418f0a588f34a3cfc27f523c8acc47f7355d4fd3
Arushi Sharma <arushi.net@outlook.com>
2024-12-26 00:32:14 +0000

    [LeetCode Sync] Runtime - 927 ms (5.04%), Memory - 21.6 MB (7.84%)

--- my-folder/0954-maximum-sum-circular-subarray/solution.py
+++ my-folder/0954-maximum-sum-circular-subarray/solution.py
@@ -1,32 +1,31 @@
 class Solution:
-    def getmaxsum(self, nums):
-        currsum = 0
-        maxsum = float('-inf')
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        
+        maxsum = self.get_maxsum(nums)
+        # use specialsum method
+        right_max = self.get_right_max(nums)
+        psum = 0
+        special_sum = -float('inf')
+        for i in range(len(nums)-1):
+            psum += nums[i]
+            special_sum = max(special_sum, psum + right_max[i+1])
+        # print(right_max)
+        return max(maxsum, special_sum)
+    
+    def get_maxsum(self, nums):
+        currsum = -float('inf')
+        maxsum = -float('inf')
         for i in range(len(nums)):
-            if currsum + nums[i] > nums[i]:
-                currsum += nums[i]
-            else:
-                currsum = nums[i]
+            currsum = max(currsum+nums[i], nums[i])
             maxsum = max(currsum, maxsum)
         return maxsum
 
-    def getminsum(self, nums):
-        currsum = 0
-        minsum = float('inf')
+    def get_right_max(self, nums):
+        """array of max suffixsum from i to n-1"""
+        ssum = nums[-1]
+        right_max = [nums[-1]]
+        for i in range(len(nums)-2,-1,-1):
+            ssum += nums[i]
+            right_max.insert(0, max(right_max[0], ssum))
         
-        for i in range(len(nums)):
-            if currsum + nums[i] < nums[i]:
-                currsum += nums[i]
-            else:
-                currsum = nums[i]
-            minsum = min(currsum, minsum)
-        return minsum
-
-    def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        
-        max1 = self.getmaxsum(nums)
-        totalsum = sum(nums)
-        max2 = totalsum - self.getminsum(nums)
-        return max(max1, max2) if max1 >=0 else max1
-
-
+        return right_max
