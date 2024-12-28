518fdf5d776eb8a394dfb03018e3b3d3aad64de0
Arushi Sharma <arushi.net@outlook.com>
2024-12-28 11:22:58 +0000

    [LeetCode Sync] Runtime - 1155 ms (42.74%), Memory - 34.2 MB (55.95%)

--- my-folder/0516-longest-palindromic-subsequence/solution.py
+++ my-folder/0516-longest-palindromic-subsequence/solution.py
@@ -1,16 +1,24 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         
-        n = len(s)
-        reverse_s = list(s).copy()
-        reverse_s.reverse()
-        reverse_s = "".join(reverse_s)
-        dp = [[0]*(n+1) for _ in range(n+1)]
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                if s[i-1] == reverse_s[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
+        dp = [[0] * len(s) for _ in range(len(s))]
+
+        for i in range(len(s)-1,-1,-1):
+            for j in range(i,len(s)):
+                if i == j:
+                    dp[i][j] = 1
+                    continue
+                
+                if s[i] == s[j]:
+                    dp[i][j] =  dp[i+1][j-1] + 2
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[n][n]
-        
+                    t1 = 0
+                    if i < len(s) - 1:
+                        t1 = max(t1, dp[i+1][j])
+                    if j > 0:
+                        t1 = max(t1, dp[i][j-1])
+                    if i < len(s) - 1 and j > 0:
+                        t1 = max(t1, dp[i+1][j-1])
+                    dp[i][j] = t1
+
+        return dp[0][-1]
