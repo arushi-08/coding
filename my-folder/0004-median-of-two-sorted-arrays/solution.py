586912ebd1b91874d4df8188e0f00a63144b026d
Arushi Sharma <arushi.net@outlook.com>
2024-12-31 05:46:24 +0000

    [LeetCode Sync] Runtime - 27 ms (6.43%), Memory - 18.2 MB (6.40%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.py
+++ my-folder/0004-median-of-two-sorted-arrays/solution.py
@@ -1,50 +1,82 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        """
-        idea : binary search to find the mid point in nums1 that is last element going into 1st half of nums1+nums2 array
-        start with assuming that half of half of the nums1+nums2 array would be in nums1 and the other half is in nums2
-        l1 r1
-        l2 r2
-        main conditions: if r1_first < l2_second and r2_first < l1_second
-        """
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
+        
+        # find mid point of both n1 and n2
+        # n1 = [1,3] | n2 = [2]
+        # mid1 = 1  | mid2 = 2
+        # 
+        
+        # compare the 2, if mid1 < mid2
+        # search for next mid1 where mid2 can just fit
 
+        # mid1 - do binary search
+        # mid2 = len(total)//2 - mid1 = 4 - 1 => 3
+
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        half = len(nums1)//2
+        if not len(nums2):
+            if len(nums1) & 1 == 0:
+                return (nums1[half] + nums1[half-1])/2
+            return nums1[half]
+
+        total_len = len(nums1) + len(nums2)
+        half = (len(nums1) + len(nums2) + 1)//2
         st = 0
         ed = len(nums1)
-        half = (len(nums1) + len(nums2) + 1)//2
+        print('nums1', nums1)
+        print('nums2', nums2)
         while st <= ed:
-            mid1 = (st + ed) // 2
+            mid1 = (st + ed)//2
             mid2 = half - mid1
+            print('half', half, 'mid1', mid1)
+            print('mid2', mid2)
+            l1, r1 = self.helper(nums1, mid1)
+            l2, r2 = self.helper(nums2, mid2)
+            
+            # 1 3 3
+            # 4
+            # if l1 == -float('inf') or r1 == -float('inf') or l2 == -float('inf'):
+            # mid1 - 1, mid2 - 0
+            # l1 - -2
+            # r2 - inf
+            # l2 - 3
+            # r1 - -1
 
-            l1, l2 = self.get_l1_l2(mid1, mid2, nums1, nums2)
-            r1, r2 = self.get_r1_r2(mid1, mid2, nums1, nums2)
+            print(
+                st, ed, 'mid1',mid1, 'mid2',mid2, 'l1', l1, 'r2',r2, 'l2',l2, 'r1', r1
+                )
+            # l1 r1
+            # l2 r2
 
             if l1 <= r2 and l2 <= r1:
-                if (len(nums1) + len(nums2)) & 1 == 0:
+                if total_len & 1 == 0:
                     return (min(r1,r2) + max(l1,l2))/2
                 return max(l1, l2)
-            if l1 > r2:
-                ed = mid1 - 1 # move ed to left as nums1 ed is pointing to greater element and we need less of nums1
-            elif l2 > r1:
+            
+            if r1 < l2:
                 st = mid1 + 1
+            elif l1 > r2:
+                ed = mid1 - 1
         
         return 0
     
-    def get_l1_l2(self, mid1, mid2, nums1, nums2):
-        l1, l2 = -float('inf'), -float('inf')
-        if mid1 > 0:
-            l1 = nums1[mid1-1]
-        if mid2 > 0:
-            l2 = nums2[mid2-1]
-        return l1, l2
+    def helper(self, nums, mid):
+        
+        l1, r1 = -float('inf'), float('inf')
+        print('mid - 1', mid - 1)
+        if 0 <= mid - 1 :
+            l1 = nums[mid-1]
+        if mid < len(nums):
+            r1 = nums[mid]
+        return l1, r1
 
-    def get_r1_r2(self, mid1, mid2, nums1, nums2):
-        r1, r2 = float('inf'), float('inf')
-        if mid1 < len(nums1):
-            r1 = nums1[mid1]
-        if mid2 < len(nums2):
-            r2 = nums2[mid2]
-        return r1, r2
+
+
+
+
+
+
 
 
