aded88f9c8a3b033011b10cb388a01191bdd3fe2
Arushi Sharma <arushi.net@outlook.com>
2025-01-07 05:06:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (23.12%)

--- my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.py
+++ my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,39 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        # return maxprofit
-        # buy - sell only 1 share at a time
-        #     [7,1,5,3,6,4]
-        # store ascending order in stack ,1,5,
-        # when nums[i] > nums[i+1] 
-        # empty stack and save stack[-1] - stack[0]
-
-        # [7,1,5,3,6,4]
-        # i=0, stack[7]
-        # i=1, 7 > 1, pop from stack, profit = 0
-        # 
+        # this works, because we can buy and immediately sell it
         profit = 0
-        stack = []
-        for i in range(len(prices)):
-
-            if stack and stack[-1] > prices[i]:
-                maxsell = stack[-1]
-                minbuy = stack[-1]
-                while stack:
-                    minbuy = stack.pop()
-                
-                profit += maxsell - minbuy
-
-            if not stack or stack[-1] < prices[i]:
-                stack.append(prices[i])
+        buy = prices[0]
         
-        if stack:
-            maxsell = stack[-1]
-            minbuy = stack[-1]
-            while stack:
-                minbuy = stack.pop()
-            
-            profit += maxsell - minbuy
+        for i in range(1, len(prices)):
+            if prices[i] > buy:
+                profit += prices[i] - buy
+            buy = prices[i]
+                
             
         return profit
+            
+        
+        
         
