6c7a07719be80233625a9185556b87f050450a97
Arushi Sharma <arushi.net@outlook.com>
2025-01-09 19:14:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (31.29%)

--- my-folder/0091-decode-ways/solution.py
+++ my-folder/0091-decode-ways/solution.py
@@ -1,39 +1,13 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
-        # return num of ways you can decode string
-        # 12
-        # 1 -> A
-        # 2 -> B
-        # 12 -> L
-        # backtracking
 
-
-        # how do we go about it?
-        # start at the beginning, select 1 or select 2
-        # 2 choices
-        # form the result, but 2^100
-
-        # backtracking seems the only option
-        # let's start
-
-        memo = {}
-        def dfs(i, s):
-            if i == len(s):
-                return 1
-            
-            if s[i] == '0':
-                return 0
-
-            if i in memo:
-                return memo[i]
-
-            res = dfs(i+1, s)
-            if s[i] != '0' and i+1 < len(s) and s[i:i+2] < '27':
-                res += dfs(i+2, s)
-            
-            memo[i] = res
-            return res
-
-        return dfs(0, s)
-
+        dp = [0]*(len(s)+1)
+        dp[-1] = 1
+        for idx in range(len(s)-1,-1,-1):
+            for i in range(1,3):
+                if idx + i > len(s) or s[idx] == '0': continue
+                curr_digit = int(s[idx:idx+i])
+                if 1 <= curr_digit <= 26:
+                    dp[idx] += dp[idx+i]
+        print(dp)
+        return dp[0]
