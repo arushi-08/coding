beae6e79bee26c54c430d6ff20c6df3c5891b2b5
Arushi Sharma <arushi.net@outlook.com>
2025-01-09 12:23:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (35.30%)

--- my-folder/0153-find-minimum-in-rotated-sorted-array/solution.py
+++ my-folder/0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -1,21 +1,41 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         
-        left = 0
-        right = len(nums)-1
-        while left < right:
-            mid = (left + right)//2
-            if mid - 1 >= 0 and nums[mid-1] > nums[mid] and mid+1 < len(nums) and nums[mid] < nums[mid+1]:
-                return nums[mid]
-            if nums[left] > nums[right]:
-                if nums[left] > nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            elif nums[left] < nums[right]:
-                if nums[right] > nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
+        # [3,4,5,1,2]
 
-        return nums[left]
+        # half array is sorted
+        # other half array is not sorted
+
+        # 3,4,5                           |    5,1,2
+        # left (1) < right (2)            |    left > right
+        # left < mid -> sorted           |    
+        #   
+
+        # 4,0,1,2,3.  |  2,3,4,0,1
+        # if left < right -> not rotated
+
+        # if left > right -> rotated -> check half till mid
+        
+        # 3,4,5,1,2
+        # 2,1
+        st = 0
+        ed = len(nums)-1
+        if nums[st] <= nums[ed]:
+            return nums[st]
+
+        while st <= ed:
+            mid = (st + ed) // 2
+
+            if nums[mid] > nums[mid+1]:
+                return nums[mid+1]
+
+            if nums[mid] <= nums[ed]:
+                ed = mid
+            
+            else:
+                st = mid + 1
+        
+        return nums[st]
+
+
+        
