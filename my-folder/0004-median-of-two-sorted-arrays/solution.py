f10946673370d3dda4b5c8eba07ae835905ef3ab
Arushi Sharma <arushi.net@outlook.com>
2025-01-12 09:44:08 +0000

    [LeetCode Sync] Runtime - 3 ms (54.07%), Memory - 18 MB (26.46%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.py
+++ my-folder/0004-median-of-two-sorted-arrays/solution.py
@@ -1,82 +1,60 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
-        # find mid point of both n1 and n2
-        # n1 = [1,3] | n2 = [2]
-        # mid1 = 1  | mid2 = 2
-        # 
-        
-        # compare the 2, if mid1 < mid2
-        # search for next mid1 where mid2 can just fit
+        # 2 sorted arrays - m and n size
+        # return median of 2 sorted arr's
+        # compare l1 w r2, l2 w r1
 
-        # mid1 - do binary search
-        # mid2 = len(total)//2 - mid1 = 4 - 1 => 3
+        # find mid1 point
+        # mid2 = (m+n)//2 - mid1
+        # find l1, l2, r1,r2 using mid1, mid2
+        # if l1  < r1 and l2 < r1
+        # l1 r2
+        # l2 r2
+        # 1 3
+        # 2 4
+        # then return l2+r1//2
+        # 1 3
+        # 2 inf
+        # return max(l1,l2)
+        # 2 3
+        # 1
+        # 3 4
+        # 2
+
+        def get_l_r(mid, nums):
+            l, r = -float('inf'), float('inf')
+            if 0 < mid:
+                l = nums[mid-1] #
+            if mid < len(nums):
+                r = nums[mid]
+            return l, r
+
 
         if len(nums1) > len(nums2):
-            return self.findMedianSortedArrays(nums2, nums1)
-        
-        half = len(nums1)//2
-        if not len(nums2):
-            if len(nums1) & 1 == 0:
-                return (nums1[half] + nums1[half-1])/2
-            return nums1[half]
+            nums1, nums2 = nums2, nums1
 
-        total_len = len(nums1) + len(nums2)
-        half = (len(nums1) + len(nums2) + 1)//2
         st = 0
-        ed = len(nums1)
-        print('nums1', nums1)
-        print('nums2', nums2)
+        ed = len(nums1) #
+        totallen = len(nums1) + len(nums2)
+        halflen = (totallen+1)//2 #
         while st <= ed:
             mid1 = (st + ed)//2
-            mid2 = half - mid1
-            print('half', half, 'mid1', mid1)
-            print('mid2', mid2)
-            l1, r1 = self.helper(nums1, mid1)
-            l2, r2 = self.helper(nums2, mid2)
-            
-            # 1 3 3
-            # 4
-            # if l1 == -float('inf') or r1 == -float('inf') or l2 == -float('inf'):
-            # mid1 - 1, mid2 - 0
-            # l1 - -2
-            # r2 - inf
-            # l2 - 3
-            # r1 - -1
-
-            print(
-                st, ed, 'mid1',mid1, 'mid2',mid2, 'l1', l1, 'r2',r2, 'l2',l2, 'r1', r1
-                )
-            # l1 r1
-            # l2 r2
+            mid2 = halflen - mid1
+            l1, r1 = get_l_r(mid1, nums1)
+            l2, r2 = get_l_r(mid2, nums2)
 
             if l1 <= r2 and l2 <= r1:
-                if total_len & 1 == 0:
-                    return (min(r1,r2) + max(l1,l2))/2
+                if totallen & 1 == 0:
+                    return (max(l1,l2) + min(r1,r2))/2 #
                 return max(l1, l2)
-            
-            if r1 < l2:
+
+            if r1 < l2: # more of nums1
                 st = mid1 + 1
-            elif l1 > r2:
+            else:
                 ed = mid1 - 1
         
         return 0
-    
-    def helper(self, nums, mid):
         
-        l1, r1 = -float('inf'), float('inf')
-        print('mid - 1', mid - 1)
-        if 0 <= mid - 1 :
-            l1 = nums[mid-1]
-        if mid < len(nums):
-            r1 = nums[mid]
-        return l1, r1
-
-
-
-
-
-
-
 
 
