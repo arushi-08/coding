6650a0bb7a8a4d2e8396d05afa76f609f49dad4a
Arushi Sharma <arushi.net@outlook.com>
2025-01-15 09:09:43 +0000

    [LeetCode Sync] Runtime - 4730 ms (39.92%), Memory - 695.9 MB (10.74%)

--- my-folder/0421-maximum-xor-of-two-numbers-in-an-array/solution.py
+++ my-folder/0421-maximum-xor-of-two-numbers-in-an-array/solution.py
@@ -1,58 +1,56 @@
-class Node:
-    def __init__(self):
-        self.links = [None]*2
-    
-    def contains_key(self, bit):
-        return self.links[bit] is not None
-
-    def get(self, bit):
-        return self.links[bit]
-    
-    def put(self, bit, node):
-        self.links[bit] = node
-    
-    def get_max(self, x):
-        return x
-
-class Trie:
-    def __init__(self):
-        self.root = Node()
-
-    def insert(self, num):
-        node = self.root
-        for i in range(31, -1, -1):
-            bit = (num >> i) & 1
-            if not node.contains_key(bit):
-                node.put(bit, Node())
-            node = node.get(bit)
-    
-    def get_max(self, num):
-        node = self.root
-        maxnum = 0
-        for i in range(31, -1, -1):
-            bit = (num >> i) & 1
-            if node.contains_key(1-bit):
-                maxnum = maxnum | (1 << i) # set the bit if trie contains opposite bit at ith index
-                node = node.get(1-bit) 
-            else:
-                node = node.get(bit)
-        return maxnum
-
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
-        '''
-        use trie 
-        convert nums to a trie
-        iterate on nums and compare with trie
-        '''
-        trie = Trie()
-        for i in range(len(nums)):
-            trie.insert(nums[i])
         
-        maxnum = 0
-        for i in range(len(nums)):
-            maxnum = max(maxnum, trie.get_max(nums[i]))
-        
-        return maxnum
+        # hashset not optimal
+        #   because we can prune/eliminate some numbers from nums that don't result in required prefix
+        # e.g. we infer after 2 bits that 25 elem xor with 5 or 3 will give max xor
+        #   and we can remove checking 10, 25 from nums
+        # so we can prune prefixes set
+        # this is done using bitwise trie
 
+        # form bitwise trie for each element
+        # to get max xor, see if the opposite bit is present in the trie at each step
+        # if it is, add 1 to maxxor
+        # else don't add 1 to maxxor
+
+        maxelem = max(nums)
+        L = len(bin(maxelem)) - 2
+        trie = {}
+        bin_nums = []
+        for num in nums:
+            # L = 5
+            # 1 << L left shift 1 L times -> 100000 (6 zero's)
+            bits = []
+            for i in range(L-1,-1,-1):
+                bits.append((num >> i) & 1)
+            bin_nums.append(bits)
+
+            self.add_to_trie(bits, trie)
+            
+        max_xor = 0
+        for num in bin_nums:
+            curr_xor = 0
+            xnode = trie
+            for bit in num:
+                opp_bit = 1 - bit
+                if opp_bit in xnode:
+                    curr_xor = curr_xor << 1 | 1
+                    xnode = xnode[opp_bit]
+                else:
+                    curr_xor <<= 1
+                    xnode = xnode[bit]
+            
+            max_xor = max(max_xor, curr_xor)
+
+        return max_xor
+        # for i in range(L-1,-1,-1):
+        #     if 
+            
     
+    def add_to_trie(self, bits, trie):
+        for bit in bits:
+            if bit not in trie:
+                trie[bit] = {}
+            trie = trie[bit]
+        trie['#'] = {}
+
