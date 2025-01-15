c5a2478677d392831d5ef5ae35034f4136de7c15
Arushi Sharma <arushi.net@outlook.com>
2025-01-15 14:25:33 +0000

    [LeetCode Sync] Runtime - 7 ms (29.27%), Memory - 17.7 MB (52.27%)

--- my-folder/1036-rotting-oranges/solution.py
+++ my-folder/1036-rotting-oranges/solution.py
@@ -1,38 +1,50 @@
-from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        # maintain levels
-        queue = deque()
-        visited = set()
-        fresh = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2 and (i,j) not in visited:
-                    queue.append((i,j))
-                    visited.add((i,j))
+        # m x n grid
+        # rotten - 2
+        # fresh - 1
+        # empty - 0
+        # 4 directions of rotten
+        # return min num of minutes
+        m, n = len(grid), len(grid[0])
+        n_minutes = 0
+        rotten_queue = deque()
+        n_fresh = 0
+
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 1:
-                    fresh += 1
+                    n_fresh += 1
+                elif grid[i][j] == 2:
+                    rotten_queue.append((i,j))
+        
+        n_rotten = len(rotten_queue)
 
-        queue.append((-1,0))
-        rows = [1, 0, -1, 0]
-        cols = [0, -1, 0, 1]
-        level = -1
-        while queue:
+        rows = [1,0,-1,0]
+        cols = [0,1,0,-1]
 
-            i, j = queue.popleft()
-            
-            if i == -1:
-                level += 1
-                if queue:
-                    queue.append((-1,-1))
-            else:
-                for x in range(len(rows)):
-                    if self.isSafe(grid, i + rows[x], j + cols[x], visited):
-                        queue.append((i + rows[x], j + cols[x]))
-                        visited.add((i + rows[x], j + cols[x]))
-                        fresh -= 1
-        return level if not fresh else -1
+        if not n_fresh: return 0
+        visited = set(rotten_queue)
 
-    def isSafe(self, grid, x, y, visited):
-        return 0 <= x and x < len(grid) and 0 <= y and y < len(grid[0]) and grid[x][y] == 1 and (x,y) not in visited
+        while rotten_queue and n_fresh:
+            n_rotten = len(rotten_queue)
+            n_minutes += 1
+            for i in range(n_rotten):
+                x,y = rotten_queue.popleft()
+
+                for j in range(len(rows)):
+                    if self.is_fresh(x+rows[j], y+cols[j], grid, visited):
+                        visited.add((x+rows[j], y+cols[j]))
+                        rotten_queue.append((x+rows[j], y+cols[j]))
+                        n_fresh -= 1
+                        
+        if n_fresh:
+            return -1
+        return n_minutes
+    
+    def is_fresh(self, x, y, grid, visited):
+        return 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 1 and (x,y) not in visited
+
+
+
