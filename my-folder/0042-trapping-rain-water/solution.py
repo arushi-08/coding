64260fe986649ec54693d10962fb957124cf0ce7
Arushi Sharma <arushi.net@outlook.com>
2025-01-16 15:37:03 +0000

    [LeetCode Sync] Runtime - 11 ms (74.29%), Memory - 19.2 MB (41.08%)

--- my-folder/0042-trapping-rain-water/solution.py
+++ my-folder/0042-trapping-rain-water/solution.py
@@ -1,23 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
-
-        prefix_max = [height[0]]
-        for i in range(1, len(height)):
-            prefix_max.append(max(prefix_max[-1], height[i]))
+        left = 0
+        right = len(height)-1
+        left_max = 0
+        right_max = 0
+        ans = 0
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                ans += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                ans += right_max - height[right]
+                right -= 1
         
-        # [0,1,1,2,2,2,2,3,3,3,3,3]
-        # [3,3,3,3,3,3,3,3,2,2,2,1]
-        # [0,1,0,2,1,0,1,3,2,1,2,1] - height
-        # [0,0,1,0,1,2,1,] max(0,min(left, right) - height)
+        return ans
 
-        suffix_max = [height[-1]]
-
-        for i in range(len(height)-2,-1,-1):
-            suffix_max.insert(0, max(suffix_max[0], height[i]))
-        
-        count = 0
-        for i in range(len(height)):
-            count += max(0, min(prefix_max[i], suffix_max[i]) - height[i])
-        
-        return count
