49f35619b61de441466ca054a0977625286c3642
Arushi Sharma <arushi.net@outlook.com>
2025-01-16 14:15:13 +0000

    [LeetCode Sync] Runtime - 2 ms (35.19%), Memory - 17.9 MB (17.50%)

--- my-folder/0091-decode-ways/solution.py
+++ my-folder/0091-decode-ways/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-
+        
         dp = [0]*(len(s)+1)
         dp[-1] = 1
-        for idx in range(len(s)-1,-1,-1):
+
+        for idx in range(len(dp)-1,-1,-1):
             for i in range(1,3):
-                if idx + i > len(s) or s[idx] == '0': continue
-                curr_digit = int(s[idx:idx+i])
-                if 1 <= curr_digit <= 26:
+                if idx+i > len(s) or s[idx:idx+i] == '0':
+                    break
+                curr_word = s[idx:idx+i]
+                if 0 <= int(curr_word) <= 26:
                     dp[idx] += dp[idx+i]
-        print(dp)
+
         return dp[0]
