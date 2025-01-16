5dfac54e2c6012ee4455d2b8672df54e1e09a0e3
Arushi Sharma <arushi.net@outlook.com>
2025-01-16 14:37:04 +0000

    [LeetCode Sync] Runtime - 5 ms (64.18%), Memory - 21.3 MB (44.18%)

--- my-folder/0347-top-k-frequent-elements/solution.py
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -1,23 +1,23 @@
-import heapq
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        # nlogn - sorting
+        # nlogn - minheap
+        # nlogk - maxheap - store 1st k elems, pop from heap if new elem > heap top
+
+        hmap = Counter(nums)
         heap = []
-        freq_dict = Counter(nums)
-        print('freq_dict', freq_dict)
-        for key, val in freq_dict.items():
+        for key, val in hmap.items():
             if len(heap) == k:
                 if val > heap[0][0]:
                     heappop(heap)
                 else:
                     continue
             heappush(heap, (val, key))
-            
-        answer = []
-        while heap and k:
-            answer.append(heappop(heap)[-1])
-            k -= 1
-        return answer
-
-
+        
+        ans = []
+        while heap:
+            _, key = heappop(heap)
+            ans.append(key)
+        
+        return ans
