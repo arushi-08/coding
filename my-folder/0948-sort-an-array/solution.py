3952df3641530c2b0ae383a9a277d494b50afaa6
Arushi Sharma <arushi.net@outlook.com>
2025-01-16 13:00:03 +0000

    [LeetCode Sync] Runtime - 723 ms (34.59%), Memory - 24.3 MB (67.99%)

--- my-folder/0948-sort-an-array/solution.py
+++ my-folder/0948-sort-an-array/solution.py
@@ -1,51 +1,36 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        
+
         self.merge_sort(nums, 0, len(nums)-1)
         return nums
     
     def merge_sort(self, nums, st, ed):
-        
-        if st == ed:
-            return
+        if st >= ed : return
 
         mid = (st+ed)//2
+
         self.merge_sort(nums, st, mid)
         self.merge_sort(nums, mid+1, ed)
-
-        self.merge(nums, st, mid, ed)
+        self.merge(nums, st, ed, mid)
     
-    def merge(self, nums, st, mid, ed):
-
-        nums1 = nums[st:mid+1]
-        nums2 = nums[mid+1:ed+1]
-        
-        i = 0
-        j = 0
-        k = 0
-        temp = [0]*len(nums1+nums2)
-
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] <= nums2[j]:
-                temp[k] = nums1[i]
+    def merge(self, nums, st, ed, mid):
+        temp = []
+        i = st
+        j = mid+1
+        while i <= mid and j <= ed:
+            if nums[i] < nums[j]:
+                temp.append(nums[i])
                 i += 1
             else:
-                temp[k] = nums2[j]
+                temp.append(nums[j])
                 j += 1
-            k += 1
-        
-        while i < len(nums1):
-            temp[k] = nums1[i]
+        while i <= mid:
+            temp.append(nums[i])
             i += 1
-            k += 1
-        
-        while j < len(nums2):
-            temp[k] = nums2[j]
+        while j <= ed:
+            temp.append(nums[j])
             j += 1
-            k += 1
         
-        nums[st:ed+1] = temp
-
-
-        
+        for i in range(st, ed+1):
+            nums[i] = temp[i-st]
 
