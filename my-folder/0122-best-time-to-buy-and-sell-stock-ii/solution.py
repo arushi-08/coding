4b279338b7b9c4ca1803fbcc797866299f29f0c5
Arushi Sharma <arushi.net@outlook.com>
2025-01-16 14:31:37 +0000

    [LeetCode Sync] Runtime - 3 ms (38.94%), Memory - 18.8 MB (41.07%)

--- my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.py
+++ my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # this works, because we can buy and immediately sell it
-        profit = 0
-        buy = prices[0]
         
+        no_hold_profit = 0
+        hold_profit = -prices[0]
+        # [1,2,3,4,5]
+        # hold_profit =   -1, -1,-1
+        # no_hold_profit = 0, 0,  
         for i in range(1, len(prices)):
-            if prices[i] > buy:
-                profit += prices[i] - buy
-            buy = prices[i]
-                
-            
-        return profit
-            
-        
-        
-        
+            # no_hold_profit - prices[i] - new hold profit, if we decide to buy at prices[i]
+            # hold_profit + prices[i] - new no hold profit, if we decide to sell at prices[i]
+            new_hold_profit = max(hold_profit, no_hold_profit - prices[i])
+            new_no_hold_profit = max(no_hold_profit,  hold_profit + prices[i])
+            hold_profit = new_hold_profit
+            no_hold_profit = new_no_hold_profit
+
+        return no_hold_profit
+
+
