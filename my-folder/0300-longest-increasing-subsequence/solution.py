1fadc3f53e1278513a00f0696e704d9b01add582
Arushi Sharma <arushi.net@outlook.com>
2025-01-24 20:05:01 +0000

    [LeetCode Sync] Runtime - 1884 ms (17.97%), Memory - 18 MB (39.06%)

--- my-folder/0300-longest-increasing-subsequence/solution.py
+++ my-folder/0300-longest-increasing-subsequence/solution.py
@@ -1,11 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
-        dp = [1] * (len(nums)+1)
-        
+        # return length of longest inc subseq
+        # store min elem seen so far
+
+        dp = [1]*len(nums)
+        # store lis ending at index i
+
         for i in range(1, len(nums)):
             for j in range(i):
                 if nums[j] < nums[i]:
-                    dp[i] = max(dp[j] + 1, dp[i])
-        
+                    dp[i] = max(dp[i], dp[j]+1)
+
         return max(dp)
+
+            
+
