66cc0c1c1d9988420d6611b8c52c1a23fe8b6aec
Arushi Sharma <arushi.net@outlook.com>
2025-01-26 17:48:35 +0000

    [LeetCode Sync] Runtime - 3 ms (39.90%), Memory - 18.3 MB (8.29%)

--- my-folder/1074-high-five/solution.py
+++ my-folder/1074-high-five/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        
-        items.sort(key=lambda x: (x[0], x[1]), reverse=True)
 
         result_dict = defaultdict(list)
 
         for i in range(len(items)):
-            if items[i][0] not in result_dict or len(result_dict[items[i][0]]) < 5:
-                result_dict[items[i][0]].append(items[i][1])
+            heappush(result_dict[items[i][0]], items[i][1])
+            
+            if len(result_dict[items[i][0]]) > 5:
+                heappop(result_dict[items[i][0]])
         
         result = []
 
