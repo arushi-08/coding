81e298b0b26aa9805bdabcbae86ca32cdc870499
Arushi Sharma <arushi.net@outlook.com>
2025-01-28 15:46:30 +0000

    [LeetCode Sync] Runtime - 3 ms (26.48%), Memory - 18 MB (6.74%)

--- my-folder/1146-greatest-common-divisor-of-strings/solution.py
+++ my-folder/1146-greatest-common-divisor-of-strings/solution.py
@@ -1,17 +1,26 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         
-        len1, len2 = len(str1), len(str2)
 
-        def valid(i):
-            if len1 % i or len2 % i:
-                return False
-            n1, n2 = len1 // i, len2 // i
-            base = str1[:i]
-            return str1 == base * n1 and str2 == base * n2
-            
-        for i in range(min(len1, len2), 0, -1):
-            if valid(i):
-                return str1[:i]
+        # given 2 strings s and t
+        # s = abcabc, t = abc
+        # return largest string x such that x divides both s and t
 
-        return ""
+        # 
+
+        if len(str1) > len(str2):
+            str1, str2 = str2, str1
+
+        
+        def can_divide(t):
+            if not t: return False
+            return t * (len(str1)//len(t)) == str1 and t * (len(str2)//len(t)) == str2
+        
+        for i in range(len(str1)-1,-1,-1):
+            if can_divide(str1[:i+1]):
+                return str1[:i+1]
+        
+        return ''
+
+
+
