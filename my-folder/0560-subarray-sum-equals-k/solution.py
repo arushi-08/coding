ddc67bac9d219191351f068e8878a06417b02c99
Arushi Sharma <arushi.net@outlook.com>
2025-02-09 16:40:46 +0000

    [LeetCode Sync] Runtime - 29 ms (69.21%), Memory - 20.4 MB (34.81%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,22 +1,18 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        # prefix sum
-        if not nums:
-            return 0
-        
-        psum = [nums[0]]
-        for i in range(1, len(nums)):
-            psum.append(psum[-1] + nums[i])
-
-        hmap = {0:1}
+        # num of subarr with sum k
+        hmap = {}
+        hmap[0] = 1
+        psum = 0
         ans = 0
-        for i in range(len(psum)):
+        for num in nums:
+            psum += num
+            if psum - k in hmap:
+                ans += hmap[psum-k]
+            hmap[psum] = hmap.get(psum, 0) + 1
 
-            if psum[i] - k in hmap:
-                ans += hmap[psum[i] - k]
-            
-            hmap[psum[i]] = hmap.get(psum[i], 0) + 1
-        
         return ans
+
+
 
