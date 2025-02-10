2bb8de6c56d2666df88a422252f1286988648b5d
Arushi Sharma <arushi.net@outlook.com>
2025-02-10 19:49:56 +0000

    [LeetCode Sync] Runtime - 15 ms (54.47%), Memory - 19.3 MB (68.78%)

--- my-folder/0042-trapping-rain-water/solution.py
+++ my-folder/0042-trapping-rain-water/solution.py
@@ -1,22 +1,33 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # 2 pointers:
-        # shift the minimum towards the arr
-        # ans += max_ptr - height[ptr]
-        left = 0
-        right = len(height)-1
+        
+        # track the max left height
+        # track the max right height
+        # max_right = float('inf')
+        # [0,1,0,2,1,0,1,3,2,1,2,1]
+        #  _,0,1,1,2,2,2,2,3,3,3,3        <- max left
+        #  _,0,1,0,2,2,1,0,3,2,1,2      <- max gap
+        # get max_right also
+        # max left > curr -> use min(max left, maxright) - curr
+        # else 0
+
+        # intuition the smaller of left and right walls determine the trapped water
+        # use max_right as len(height)-1
         left_max = 0
         right_max = 0
-        ans = 0
+        left = 0
+        right = len(height)-1
+        trapped_water = 0
+
         while left < right:
             if height[left] < height[right]:
                 left_max = max(left_max, height[left])
-                ans += left_max - height[left]
+                trapped_water += left_max - height[left]
                 left += 1
             else:
                 right_max = max(right_max, height[right])
-                ans += right_max - height[right]
+                trapped_water += right_max - height[right]
                 right -= 1
-        
-        return ans
+
+        return trapped_water
 
