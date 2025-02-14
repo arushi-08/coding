d5ed631855501f370f148ce12b94fce4fb390388
Arushi Sharma <arushi.net@outlook.com>
2025-02-14 20:47:54 +0000

    [LeetCode Sync] Runtime - 118 ms (81.35%), Memory - 78.3 MB (63.01%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, key=0, val=0):
+    def __init__(self, key, val=0):
         self.key = key
         self.val = val
         self.next = None
@@ -8,57 +8,53 @@ class Node:
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {} # key, value
+        self.hmap = {}
+        self.size = capacity
         self.head = Node(-1)
         self.tail = Node(-1)
         self.head.next = self.tail
         self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        # hashmap -> o(1)
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-            self._add_to_top(node)
-            return node.val
-        return -1
- 
+        if key not in self.hmap:
+            return -1
+        
+        node = self.hmap[key]
+        self._remove(node)
+        self._move_to_top(node)
+        return node.val
+
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
+        if key in self.hmap:
+            node = self.hmap[key]
             node.val = value
             self._remove(node)
-            self._add_to_top(node)
-            return
+            self._move_to_top(node)
+        else:
+            if len(self.hmap) == self.size:
+                if self.tail.prev != self.head:
+                    remove_node = self.tail.prev
+                    self._remove(remove_node)
+                    del self.hmap[remove_node.key]
+            new_node = Node(key, value)
+            self.hmap[key] = new_node
+            self._move_to_top(new_node)
 
-        if len(self.cache) == self.capacity:
-            lru = self.tail.prev
-            if lru == self.head:
-                return
-            del self.cache[lru.key]
-            self._remove(lru)
-
-        self.cache[key] = Node(key, value)
-        self._add_to_top(self.cache[key])
 
     def _remove(self, node):
+        prev_node = node.prev
         next_node = node.next
-        node.prev.next = next_node
-        next_node.prev = node.prev
+        prev_node.next = next_node
+        next_node.prev = prev_node
+        node.prev = None
+        node.next = None
     
-    def _add_to_top(self, node):
+    def _move_to_top(self, node):
         head_next = self.head.next
         self.head.next = node
         node.prev = self.head
         node.next = head_next
         head_next.prev = node
-
-
-
-
-
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
