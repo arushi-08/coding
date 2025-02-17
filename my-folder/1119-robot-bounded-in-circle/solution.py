58e1f730a72c10646b9fcb2871f10ad587497717
Arushi Sharma <arushi.net@outlook.com>
2025-02-17 19:42:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (22.57%)

--- my-folder/1119-robot-bounded-in-circle/solution.py
+++ my-folder/1119-robot-bounded-in-circle/solution.py
@@ -1,56 +1,29 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-         
-        #  return true if there's a circle and robot always returns back to it
-
-        # GGLLGG
-        """
-         --
-        |  |
-        |  |
-         --
         
-        if odd number of L or R return true
-        # if its G
-        GGLGG
-           -
-            |
-            - -
-            LGRGLG
-
-        GLGLGGLGL
-
-
-                -
-               | |
-               |
-                -
-
-
-                |
-                 -
-                 L
-                 (3,-4)
-                 (4, 5)
-        """
-
         l_steps = instructions.count('L')
         r_steps = instructions.count('R')
 
-        if not l_steps and not r_steps:
+        if l_steps == 0 and r_steps == 0:
             return False
         
-        x,y = 0, 0
+        x, y = 0, 0
         dx, dy = 0, 1
-        for i in range(len(instructions)):
-            if instructions[i] == 'G':
-                x += dx
-                y += dy
-            elif instructions[i] == 'L':
-                dx, dy = -dy, dx
-            elif instructions[i] == 'R':
-                dx, dy = dy, -dx
-        # print(x,y, curr_dir)
-        return [x,y] == [0,0] or [dx,dy]!=[0,1]
-
+        period = 4
+        while period:
+            for instruction in instructions:
+                if instruction == 'G':
+                    x += dx
+                    y += dy
+                elif instruction == 'L':
+                    dx, dy = -dy, dx
+                else:
+                    dx, dy = dy, -dx
+            
+            if [x,y] == [0,0]:
+                return True
+            
+            period -= 1
+        
+        return False
         
