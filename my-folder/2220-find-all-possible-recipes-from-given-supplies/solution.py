99b42fe064e3c946048acb4182a67edc5bb687cd
Arushi Sharma <arushi.net@outlook.com>
2025-03-22 16:56:05 +0000

    [LeetCode Sync] Runtime - 75 ms (57.85%), Memory - 19.8 MB (53.23%)

--- my-folder/2220-find-all-possible-recipes-from-given-supplies/solution.py
+++ my-folder/2220-find-all-possible-recipes-from-given-supplies/solution.py
@@ -1,39 +1,51 @@
-from collections import defaultdict
 class Solution:
-    def get_dish_order(self, recipes, ingredients):
-        dish_order = defaultdict(int)
-        for recipe, ing in zip(recipes, ingredients):
-            dish_order[recipe] = len(ing)
-        return dish_order
-    
-    def get_prereq_dish_map(self, recipes, ingredients):
-        prereq_dish_map = defaultdict(list)
-        for recipe, ing in zip(recipes, ingredients):
-            for i in ing:
-                prereq_dish_map[i].append(recipe)
-        return prereq_dish_map
-    
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         
-        # indegree dict
-        dish_order = self.get_dish_order(recipes, 
-                                         ingredients)
-        # graph
-        prereq_dish_map = self.get_prereq_dish_map(recipes, ingredients)
-        
-        queue = supplies.copy()
-        
-        res = []
-        while queue:
-            curr_dish = queue.pop(0)
-            if curr_dish in recipes:
-                res.append(curr_dish)
-            # prereq -> dish
-            for dish in prereq_dish_map[curr_dish]:
-                dish_order[dish] -= 1
-                if dish_order[dish] == 0:
-                    queue.append(dish)
-        
-        return res
-                        
+        # n diff recipes
+
+        # given a string array recipes and a 2d string array ingredients
+        # ith recipe has the name recipes[i] and you can create it
+
+
+        # output recipes we can create
+        # topological sort
+        # start with supplies
+        # create a graph from ingredient to recipe
+        # and hmap of recipe: ingredient count
+
+        graph_ing_to_recipe = defaultdict(list)
+        ing_count_hmap = {}
+
+        for ingredient_list, recipe in zip(ingredients, recipes):
+            for ingredient in ingredient_list:
+                graph_ing_to_recipe[ingredient].append(recipe)
             
+            ing_count_hmap[recipe] = len(ingredient_list)
+        
+        queue = deque()
+        visited = set()
+
+        for ing in supplies:
+            queue.append(ing)
+            visited.add(ing)
+        
+        answer = []
+        while queue:
+            ing = queue.popleft()
+
+            for recipe in graph_ing_to_recipe[ing]:
+                # if ing_count_hmap.get(recipe, 0) == 0:
+                if recipe not in visited:
+                    ing_count_hmap[recipe] -= 1
+
+                    if ing_count_hmap[recipe] == 0:
+                        visited.add(recipe)
+                        queue.append(recipe)
+                        answer.append(recipe)
+        
+        return answer
+        
+
+
+
+
