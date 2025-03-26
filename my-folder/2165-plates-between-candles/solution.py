6c9229c90610e283cddfd331f8d0c39c9b5e944e
Arushi Sharma <arushi.net@outlook.com>
2025-03-26 03:26:52 +0000

    [LeetCode Sync] Runtime - 144 ms (38.50%), Memory - 49.5 MB (20.65%)

--- my-folder/2165-plates-between-candles/solution.py
+++ my-folder/2165-plates-between-candles/solution.py
@@ -1,59 +1,41 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        
-        bar_idx = []
-
-        for i,ch in enumerate(s):
+        # [2,5,9]
+        # [2,5]
+        # [2,5] = closest to bounds
+        # 
+        bar_idx = {}
+        left_traversal = []
+        idx = 0
+        for i, ch in enumerate(s):
             if ch == '|':
-                bar_idx.append(i)
-        
+                bar_idx[i] = idx
+                idx += 1
+                left_traversal.append(i)
+            elif left_traversal:
+                left_traversal.append(left_traversal[-1])
+            else:
+                left_traversal = [len(s)]
+
+        right_traversal = [len(s)] * len(s)
+
+        for i in range(len(s)-1,-1,-1):
+            if s[i] == '|':
+                right_traversal[i] = i
+            elif i < len(s) - 1:
+                right_traversal[i] = right_traversal[i+1]
+
         ans = []
-        for i in range(len(queries)):
-            left, right = queries[i]
-            # print('left', left)
-            # print('right', right)
-
-            first_bar_on_left = self.lower_bound_search(bar_idx, left, 'LEFT')
-            first_bar_on_right = self.lower_bound_search(bar_idx, right, 'RIGHT')
-            
-            # print('first_bar_on_left', first_bar_on_left)
-            # print('first_bar_on_right', first_bar_on_right)
-
-            if first_bar_on_left == -1:
+        for start, end in queries:
+            if (
+                (right_traversal[start] not in bar_idx) or 
+                (left_traversal[end] not in bar_idx) or
+                (bar_idx[right_traversal[start]] >= bar_idx[left_traversal[end]])
+                ):
                 ans.append(0)
-                continue
-                
-            if bar_idx[first_bar_on_right] > right:
-                first_bar_on_right -= 1
-
-            if left <= bar_idx[first_bar_on_right] and right >= bar_idx[first_bar_on_left]:
-                res = bar_idx[first_bar_on_right] - bar_idx[first_bar_on_left] - (first_bar_on_right - first_bar_on_left)
             else:
-                res = 0
+                ans.append(
+                    left_traversal[end] - right_traversal[start] - (bar_idx[left_traversal[end]] - bar_idx[right_traversal[start]]) 
+                )
 
-            ans.append(res)
-        
         return ans
-    
-    def lower_bound_search(self, bar_idx, left_query_bar, BAR_SIDE):
-
-        start = 0
-        end = len(bar_idx) - 1
-        res = len(bar_idx)
-        # print('enter')
-        while start <= end:
-            mid = (start + end) // 2
-            if bar_idx[mid] >= left_query_bar:
-                # print(bar_idx[mid], left_query_bar)
-                res = min(mid, res)
-                # print('res', res)
-                end = mid - 1
-            else:
-                start = mid + 1
-        
-        if res == len(bar_idx):
-            if BAR_SIDE == 'RIGHT':
-                return res - 1
-            return -1
-        return res
-    
