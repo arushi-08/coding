ad94a648f9d2d5cc32f2fb4e0039066fd7f7816d
Arushi Sharma <arushi.net@outlook.com>
2025-04-08 02:07:52 +0000

    [LeetCode Sync] Runtime - 7 ms (79.32%), Memory - 18.8 MB (34.77%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -1,16 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        if len(nums) == 1: return 0
         l = 0
         r = 0
+        n = len(nums)
         n_jumps = 0
-        while r < len(nums)-1:
+
+        while r < n - 1:
+
             farthest = 0
-            for i in range(l, r+1):
-                farthest = max(farthest, i+nums[i])
-            l = r+1
-            r = farthest
             n_jumps += 1
+            for ind in range(l, r+1):
+                farthest = max(farthest, nums[ind] + ind)
+            
+            l = r + 1
+            r = farthest
         
         return n_jumps
