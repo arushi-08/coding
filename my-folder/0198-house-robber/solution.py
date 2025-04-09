9735cca8d9a2ee0a36a0fc42fdc3fd2695583742
Arushi Sharma <arushi.net@outlook.com>
2025-04-09 03:58:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (13.14%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,15 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        # non adj sum
-        if len(nums)==1: return nums[0]
-        prev2 = nums[0]
-        prev = max(nums[0], nums[1])
+        # nums = [1,2,3,1]
+        # goal: return max value from non adj idx
 
-        for i in range(2,len(nums)):
-            curr= max(prev, nums[i] + prev2)
-            prev2 = prev
-            prev = curr
+        after_money = [0] * 2
+        curr_money = [0] * 2
 
-        return prev
+        for i in range(len(nums)-1,-1,-1):
+            curr_money[1] = after_money[0]
+            curr_money[0] = max(after_money[1] + nums[i], after_money[0])
 
+            after_money = curr_money[:]
+        
+        return after_money[0]
