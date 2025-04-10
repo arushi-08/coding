86f41d595382a52e2d1adcaaa4684c2a55d78572
Arushi Sharma <arushi.net@outlook.com>
2025-04-10 18:26:54 +0000

    [LeetCode Sync] Runtime - 279 ms (14.09%), Memory - 32.3 MB (16.90%)

--- my-folder/2681-put-marbles-in-bags/solution.py
+++ my-folder/2681-put-marbles-in-bags/solution.py
@@ -1,82 +1,38 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
         
-        # k bags
-        # given 0 indexed integer array weights
-        # where weights[i] is wt of ith marble
-
-        # given int k
-
-        # divide the marbles into k bags based on rules:
-
+        # return max - min scores
+        # divide marbles into k bags
         # no bag is empty
-        # marbles[i:j+1] in same bag
-        # cost of bag is wts[i] + wts[j]
-        # score = sum of all costs
-        # return diff between max and min scores among marble distributions
+        # ith marble and jth marble are in a bag
+        #   all marbles with an index between ith and jth idx should also be in that same bag
+        # if a bag consists of all marbles with an index from i to j
+        # cost = weights[i] + weights[j]
+        # break points = k+1
+        # start and end are same -> diff break points = k-1
+        # make k-1 breaks in weights arr
+        # how do i make k-1 breaks
+        # 1 break at i is weights[i] + weights[i+1]
+        # find max ( weights[i] + weights[i+1] ) k times
+        # put it in heap
+        if len(weights) <= 2 or k == 1:
+            return 0
 
-        # max score when maxwt is at boundary
-        # min score when maxwt is not at boundary
-        # find subarray end points
-        # if there are k bags
-        # then 2*k boundary points (but when they are overlappings, its actually k+1 points, 2 of these are the corner points that will always be there, so find for k-1 points) _ _ _
-        
-        if len(weights) == k: return 0
-
-        # put k-1 points
-        # assume sum till now is x, if we put another partition at index j, sum is x + A[j] + A[j+1]
-        # top k-1 breaks
-        # 
-
-        # MAX(sum of k-1 adj pairs) - MIN(sum of k-1 adj pairs)
-        # wts[i] + wts[i+1], compute these for each adjacent pair
-        # how to compute this?
-
-        if k == 1: return 0
-
-        max_sigma = self.get_max_sigma(weights, k)
-        min_sigma = self.get_min_sigma(weights, k)
-        return max_sigma - min_sigma
-    
-    def get_max_sigma(self, weights, k):
-        "sum of top k-1 elements"
-        heap = []
+        max_score_heap = []
         for i in range(len(weights)-1):
-            newelem = weights[i] + weights[i+1]
-            if len(heap) == k-1: 
-                if heap[0] < newelem:
-                    heappop(heap)
-                    heappush(heap, newelem)
-            else:
-                heappush(heap, newelem)
+            heappush(max_score_heap, weights[i] + weights[i+1] )
+            if len(max_score_heap) > k-1:
+                heappop(max_score_heap)
         
-        # heap has top k-1 points
-        return sum(heap)
-    
-    def get_min_sigma(self, weights, k):
-        "sum of bottom k numbers"
-        heap = []
+        max_score_val = sum(max_score_heap)
+
+
+        min_score_heap = []
         for i in range(len(weights)-1):
-            newelem = weights[i] + weights[i+1]
-            if len(heap) == k-1: 
-                if -heap[0] > newelem:
-                    heappop(heap)
-                    heappush(heap, -newelem)
-            else:
-                heappush(heap, -newelem)
+            heappush(min_score_heap, -(weights[i] + weights[i+1]) )
+            if len(min_score_heap) > k-1:
+                heappop(min_score_heap)
         
-        # heap has bottom k-1 points
-        return -sum(heap)
+        min_score_val = -sum(min_score_heap)
 
-
-                
-
-                
-
-
-
-
-
-
-
-
+        return max_score_val-min_score_val
