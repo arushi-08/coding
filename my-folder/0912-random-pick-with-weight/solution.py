58cab2da12ebeccb1895cfc175424ec4b2b0e248
Arushi Sharma <arushi.net@outlook.com>
2025-04-10 19:19:28 +0000

    [LeetCode Sync] Runtime - 39 ms (96.94%), Memory - 23.5 MB (17.13%)

--- my-folder/0912-random-pick-with-weight/solution.py
+++ my-folder/0912-random-pick-with-weight/solution.py
@@ -1,26 +1,21 @@
-from random import random
+from itertools import accumulate
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.psum = []
-        self.total_sum = 0
-        for i in range(len(w)):
-            self.total_sum += w[i]
-            self.psum.append(self.total_sum)
-            
+        # 1
+        # 2, 3
+        self.w = w
+        self.psum = list(accumulate(w))
 
     def pickIndex(self) -> int:
-        rand_val = self.total_sum * random()
-        start, end = 0, len(self.psum)-1
-        idx = end
-        while start <= end:
-            mid = (start + end)//2
-            if self.psum[mid] >= rand_val:
-                idx = min(idx, mid)
-                end = mid - 1
-            else:
-                start = mid + 1
-        return idx
+        
+        # find leftmost index where i can insert a random number?
+        return bisect.bisect_left( 
+            self.psum, 
+            random.randint(1, self.psum[-1])
+        )
+
+
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
