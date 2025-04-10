70043d3871e01dd4e1fc91a330a955ab2cdebba6
Arushi Sharma <arushi.net@outlook.com>
2025-04-10 20:15:51 +0000

    [LeetCode Sync] Runtime - 2 ms (99.50%), Memory - 18.9 MB (20.99%)

--- my-folder/1108-analyze-user-website-visit-pattern/solution.py
+++ my-folder/1108-analyze-user-website-visit-pattern/solution.py
@@ -1,15 +1,62 @@
 class Solution:
-
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         
-        users = defaultdict(list)
-    
-        for user, time, site in sorted(zip(username, timestamp, website), key = lambda x: (x[0],x[1])): 
-            users[user].append(site)
+        # given 2 string arrays username and website
+        # integer array timestamp
+        # all the given arrays are of the same length and tuple 
+        # [usernamei, websitei, timestampi]
+        # pattern is a list of 3 websites
 
-        patterns = Counter()
+        # [home, away, love], [leetcode, love, leetcode]
+        # [luffy, luffy, luffy]
+        # 
 
-        for user, sites in users.items():
-            patterns.update(Counter(set(combinations(sites, 3))))
+        # score of pattern = num of users that visited all websites in pattren
+        # in same ordered they appeared in the pattern
 
-        return max(sorted(patterns), key=patterns.get)
+        # [home, away, love] -> score is num of uesrs x, x visited home then away then love
+
+        # return pattern with highest score
+
+        # create tuples
+        # same websites,
+        # home - 1, 4, 7, 8
+        # about - 2, 9,
+        # career - 3, 10
+        # 
+        # joe -> home, about, career
+        # james -> home, cart, maps, home
+        # mary -> home, about, career
+
+        # iterate on people
+        # store possible 3 contiguous arrays
+        # return the most frequent contiguous array
+        webvisits = []
+        for time, user, site in zip(timestamp, username,  website):
+            webvisits.append([time, site, user])
+        webvisits.sort()
+
+        graph = defaultdict(list)
+        for time, site, user in webvisits:
+            graph[user].append(site)
+        
+        pattern_freq = defaultdict(set)
+        for user, sites in graph.items():
+            for i in range(len(sites)):
+                for j in range(i+1, len(sites)):
+                    for k in range(j+1, len(sites)):
+                        pattern_freq[(sites[i], sites[j], sites[k])].add(user)
+            
+        max_users = 0
+        popular_pattern = []
+        for k, v in pattern_freq.items():
+            if len(v) > max_users:
+                max_users = len(v)
+                popular_pattern = list(k)
+            elif len(v) == max_users:
+                popular_pattern = sorted([popular_pattern, list(k)])[0]
+        return popular_pattern
+
+
+        
+
