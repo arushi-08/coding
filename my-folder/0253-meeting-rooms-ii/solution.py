cccb6a1091cbfc0c8e1758026eeb4dc75e2e29a6
Arushi Sharma <arushi.net@outlook.com>
2025-04-10 01:15:17 +0000

    [LeetCode Sync] Runtime - 3 ms (86.57%), Memory - 19.9 MB (75.24%)

--- my-folder/0253-meeting-rooms-ii/solution.py
+++ my-folder/0253-meeting-rooms-ii/solution.py
@@ -1,22 +1,41 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        # given meeting arr where intervals[i] = [starti, endi]
-        # return min num of conference rooms 
+        
+        # given array of meeting time intervals
+        # where intervals[i] = [starti, endi]
+        # return min num of conference rooms required
+
+        # [[0,30],[5,10],[15,20]]
+        # return minimum number of conferences
+
+        # sort - start time
+        # conf can be shared - if non overlapping
+        # max num of overlapping intervals at a time = num of conf rooms
+
+        # iterate on intervals
+        # if next interval start time > curr end time
+        # curr meeting over -> same conf room for next
+        # else: overlapping
+        if not intervals: return 0
 
         intervals.sort()
-        booked_room_endtimes = []
-        heappush(booked_room_endtimes, intervals[0][1])
-        
-        for i in range(1, len(intervals)):
 
-            start_x, end_x = intervals[i]
-            if start_x >= booked_room_endtimes[0]:
-                heappop(booked_room_endtimes)
+        min_end_time_heap = []
 
-            heappush(booked_room_endtimes, end_x)
+        for interval in intervals:
+            if min_end_time_heap and interval[0] >= min_end_time_heap[0]:
+                
+                heappop(min_end_time_heap)
             
-        
-        return len(booked_room_endtimes)
-            
+            heappush(min_end_time_heap, interval[1])
 
+        return len(min_end_time_heap)
+
+        # [[7,50],[10,20],[11,40],[21,40],[42,55]]
+        # min_end_time_heap = 50
+
+        # min_end_time_heap = [20,50]
+        # 
+        # min_end_time_heap = [20,40,50]
+        # n_conf_rooms = 1
