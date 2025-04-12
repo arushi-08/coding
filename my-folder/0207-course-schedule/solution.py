24cf6b282175c3085dec15eb54edb17b3a35057a
Arushi Sharma <arushi.net@outlook.com>
2025-04-12 21:46:33 +0000

    [LeetCode Sync] Runtime - 5 ms (58.48%), Memory - 19 MB (82.45%)

--- my-folder/0207-course-schedule/solution.py
+++ my-folder/0207-course-schedule/solution.py
@@ -1,47 +1,29 @@
-from collections import deque, defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-
-        courses_wo_prereqs = [1] * numCourses
-        for i in range(len(prerequisites)):
-            courses_wo_prereqs[prerequisites[i][0]] = 0
+        # course schedule
+        prereq_to_course_graph = defaultdict(list)
+        course_to_prereq_count_map = {} 
+        for course, prereq in prerequisites:
+            prereq_to_course_graph[prereq].append(course)
+            course_to_prereq_count_map[course] = course_to_prereq_count_map.get(course, 0) + 1
         
-        course_graph = defaultdict(list)
-        for i in range(len(prerequisites)):
-            course_graph[prerequisites[i][0]].append(prerequisites[i][1])
-
-        prereq_graph = defaultdict(list)
-        for i in range(len(prerequisites)):
-            prereq_graph[prerequisites[i][1]].append(prerequisites[i][0])
-
-        visited = set()
         queue = deque()
-        count_courses = 0
-        for i in range(len(courses_wo_prereqs)):
-            if courses_wo_prereqs[i] == 1:
+        for i in range(numCourses):
+            if i not in course_to_prereq_count_map:
                 queue.append(i)
-                visited.add(i)
-                count_courses += 1
-        print(queue)
+        
+        # false: cannot finish
+
         while queue:
-            currcourse = queue.popleft()
-            for course in prereq_graph[currcourse]:
-                if course in visited:
-                    continue
-                # course - course that has currcourse as prereq
-                n_prereqs = 0
-                for prereq in course_graph[course]:
-                    if prereq in visited:
-                        n_prereqs += 1
-                    else:
-                        break
-                if n_prereqs == len(course_graph[course]):
-                    queue.append(course)
-                    visited.add(course)
-                    count_courses += 1
-                # print("queue",queue)
-        if count_courses == numCourses:
-            return True
-        return False
-            
+            curr = queue.popleft()
+
+            for next_course in prereq_to_course_graph[curr]:
+                course_to_prereq_count_map[next_course] -= 1
+
+                if course_to_prereq_count_map[next_course] == 0:
+                    queue.append(next_course)
+                    del course_to_prereq_count_map[next_course]
+
+        return course_to_prereq_count_map == {}
+
