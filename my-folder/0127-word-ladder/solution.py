d01fe2ecf250331f7f8e4720688068846a9d1f92
Arushi Sharma <arushi.net@outlook.com>
2025-04-12 21:15:56 +0000

    [LeetCode Sync] Runtime - 31 ms (99.25%), Memory - 20.8 MB (42.68%)

--- my-folder/0127-word-ladder/solution.py
+++ my-folder/0127-word-ladder/solution.py
@@ -1,24 +1,39 @@
-from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if not beginWord: return 0
-        queue = deque()
-        queue.append((beginWord, 1))
-        visited = set()
-        while len(queue):
-            curr, count = queue.popleft()
-            for word in wordList:
-                if word not in visited:
-                    changes = 0
-                    for c1, c2, in zip(curr, word):
-                        if c1 != c2:
-                            changes += 1
-                    if changes > 1:
-                        continue
-                    visited.add(word)
-                    if word == endWord:
-                        return count + 1
-                    queue.append((word, count + 1))
+
+        wordSet = set(wordList)
+        if endWord not in wordSet: return 0
+
+        if beginWord not in wordSet:
+            wordSet.add(beginWord)
+
+        graph = defaultdict(list)
+        # [*it, h*t]
+        for word in wordSet:
+            for i in range(len(word)):
+                graph[word[:i]+ '*' + word[i+1:]].append(word)
+
+        queue_begin = deque([beginWord])
+        visited_begin = {beginWord:1}
+        queue_end = deque([endWord])
+        visited_end = {endWord:1}
+
+        while queue_begin and queue_end:
+            if len(queue_begin) > len(queue_end):
+                queue_begin, queue_end = queue_end, queue_begin
+                visited_begin, visited_end = visited_end, visited_begin
+
+            queue_len = len(queue_begin)
+            for _ in range(queue_len):
+                
+                curr = queue_begin.popleft()
+            
+                for i in range(len(curr)):
+                    for neigh in graph[curr[:i]+ '*' + curr[i+1:]]:
+                        if neigh in visited_end:
+                            return visited_begin[curr] + visited_end[neigh]
+                        if neigh not in visited_begin:
+                            queue_begin.append(neigh)
+                            visited_begin[neigh] = visited_begin[curr] + 1
         
-        if curr == endWord: return count
         return 0
