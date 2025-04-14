f22c998f5bade13f536ab3bb05abd4af28640722
Arushi Sharma <arushi.net@outlook.com>
2025-04-14 04:41:30 +0000

    [LeetCode Sync] Runtime - 6 ms (20.35%), Memory - 18 MB (59.67%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.py
+++ my-folder/0004-median-of-two-sorted-arrays/solution.py
@@ -1,46 +1,55 @@
-
-
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums2) < len(nums1):
-            nums2, nums1 = nums1, nums2
+        """
+        half_len = length of 1st half of array (if array is odd, left half is longer of the 2 halves) => (totallen + 1)/2
+        l = nums[mid-1]
+        r = nums[mid]
+        arr1 = l1.  r1
+        arr2 = l2.  r2
+        partition the 2 arrays such that all elements in the left half are smaller than all elements in the right half 
+        mid condition:
+            if l1 <= r2 and l2 <= r1:
+                if total is even:
+                    return (max(l1,l2) + min(r1,r2))/2
+                return max(l1,l2)
+            if l1 > r2:
+                we have moved too much to right side, need to move left
+                ed = mid1 - 1
+            else:
+                st = mid1 + 1
+        """
+
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
         
-        total_len = len(nums1) + len(nums2)
-        half_len = (total_len+1)//2
-        # [1,2,3]
-        # [5,6,7]
-        # half len = 2, mid = 1, nums2mid = 0
         st = 0
-        end = len(nums1)
-
-        while st <= end:
-            mid = (st + end) // 2
-            nums2mid = half_len - mid
-
-            l1, r1 = self.get_l_r(nums1, mid)
-            l2, r2 = self.get_l_r(nums2, nums2mid)
+        ed = len(nums1)
+        total_len = len(nums1) + len(nums2)
+        half_len = (total_len+1) // 2
+        
+        while st <= ed:
+            mid1 = (st + ed) // 2
+            mid2 = half_len - mid1
+            l1, r1 = self.get_l_r(nums1, mid1)
+            l2, r2 = self.get_l_r(nums2, mid2)
 
             if l1 <= r2 and l2 <= r1:
-                # print(l1,  r1, l2, r2)
                 if total_len & 1 == 0:
-                    return (max(l1, l2) + min(r1, r2))/2
+                    return (max(l2, l1) + min(r1, r2))/2
                 return max(l1,l2)
             
-            if l2 > r1:
-                # 6 > 4, pick more of nums1
-                st = mid + 1
+            if l1 > r2:
+                ed = mid1 - 1
             else:
-                end = mid - 1
+                st = mid1 + 1
         
-        return 0
-
     
     def get_l_r(self, nums, mid):
-        n = len(nums)
-        l,r = -float('inf'), float('inf')
-        if 0 < mid:
+
+        l = -float('inf')
+        r = float('inf')
+        if 0 <= mid - 1:
             l = nums[mid-1]
-        if mid < n:
+        if mid < len(nums):
             r = nums[mid]
         return l, r
-
