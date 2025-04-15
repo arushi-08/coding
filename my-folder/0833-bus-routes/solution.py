911f98b55e1126879f7fdd42699a54e8e53e7628
Arushi Sharma <arushi.net@outlook.com>
2025-04-15 01:03:39 +0000

    [LeetCode Sync] Runtime - 77 ms (90.94%), Memory - 43.4 MB (63.31%)

--- my-folder/0833-bus-routes/solution.py
+++ my-folder/0833-bus-routes/solution.py
@@ -1,39 +1,43 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        """
+        graph nodes -> buses
+        find next bus by using stops_to_bus_map{}
+            and looping on all buses that go to a stop
+        """
 
         if source == target: return 0
 
-        graph = defaultdict(list)
+        stops_to_bus_map = defaultdict(list)
+        for i in range(len(routes)):
+            for stop in routes[i]:
+                stops_to_bus_map[stop].append(i)
+
         queue = deque()
-        for i, route in enumerate(routes):
-            for bustop in route:
-                graph[bustop].append(i)
+        visited_stops = {source}
+        visited_bus = set()
+        for bus in stops_to_bus_map[source]:
+            queue.append((bus, 1))
+            visited_bus.add(bus)
         
-        visited = set()
-        queue.append(source)
-
-        count = 0
-        visited_bustop = set()
         while queue:
+            curr, nbuses = queue.popleft()
 
-            size = len(queue)
+            if target in routes[curr]:
+                return nbuses
 
-            for i in range(size):
-                stop = queue.popleft()
-                if stop in visited_bustop:
-                    continue
-                
-                visited_bustop.add(stop)
-                if stop == target:
-                    return count
-            
-                for bus in graph[stop]:
-                    if bus not in visited:
-                        visited.add(bus)
-                        for nextstop in routes[bus]:
-                            queue.append(nextstop)
-                        
-                
-            count += 1
-
+            for next_stop in routes[curr]:
+                if next_stop not in visited_stops:
+                    for next_bus in stops_to_bus_map[next_stop]:
+                        if next_bus not in visited_bus:
+                            queue.append([next_bus, nbuses+1])
+                            visited_bus.add(next_bus)
+                    visited_stops.add(next_stop)
+        
         return -1
+
+# O(V+E)
+# 500 + num of stops intersection
+# if all stops are common between 2 buses
+# that's still 1 edge
+# E = V*(V-1) = V^2?
