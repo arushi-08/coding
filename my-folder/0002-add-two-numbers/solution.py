bde529109a04e1cf864f57d99a429b96bf0cd772
Arushi Sharma <arushi.net@outlook.com>
2025-04-18 21:31:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (84.74%)

--- my-folder/0002-add-two-numbers/solution.py
+++ my-folder/0002-add-two-numbers/solution.py
@@ -6,40 +6,38 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        curr1 = l1
-        curr2 = l2
-        ansnode = ListNode()
-        curr = ansnode
-        while curr1 and curr2:
-            curr.val += curr1.val + curr2.val
-            if curr1.next or curr2.next:
-                curr.next = ListNode()
-            if curr.val > 9:
-                value = curr.val
-                curr.val =  value % 10
-                curr.next = ListNode(value // 10)
-            curr1 = curr1.next
-            curr2 = curr2.next
+        # given 2 non empty linked lists representing 2 non-negative integers.
+        # digits are stored in reverse order
+
+        # 2 -> 4 -> 3
+        # 5 -> 6 -> 4
+        # 7 -> 0 -> 8
+
+        # 2 things:
+        # add number, number < 10 -> new node with val
+        # number >= 10 -> store remainder in new node, carry 1 in a varible for next new node
+        # 
+
+        result_head = ListNode(0)
+        curr = result_head
+        carry = 0
+        
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            addval = v1 + v2 + carry
+            carry = 0
+            if addval >= 10:
+                carry = addval // 10
+                addval %= 10
+            
+            curr.next = ListNode(addval)
             curr = curr.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return result_head.next
+            
         
-        def helper(curr, curr1):
-
-            while curr1:
-                curr.val += curr1.val
-                if curr1.next:
-                    curr.next = ListNode()
-                if curr.val > 9:
-                    value = curr.val
-                    curr.val =  value % 10
-                    curr.next = ListNode(value // 10)
-                curr1 = curr1.next
-                curr = curr.next
-            return curr
-
-        curr = helper(curr, curr1)
-        
-        curr = helper(curr, curr2)
-
-
-        return ansnode
-
