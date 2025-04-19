8699402f1d08ac61a07daa5525f27181cb7507f1
Arushi Sharma <arushi.net@outlook.com>
2025-04-19 14:37:35 +0000

    [LeetCode Sync] Runtime - 8 ms (74.36%), Memory - 24.9 MB (37.25%)

--- my-folder/0359-logger-rate-limiter/solution.py
+++ my-folder/0359-logger-rate-limiter/solution.py
@@ -1,16 +1,18 @@
 class Logger:
 
     def __init__(self):
-        self.message_queue = {}
+        # each unique msg printed at most 10 seconds
+        # if same msg comes within 10 seconds, drop it
+        self.logmap = {}
+        
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if (message in self.message_queue 
-            and timestamp < self.message_queue[message] + 10):
-            return False
-        else:
-            self.message_queue[message] = timestamp
-            return True
+        if message in self.logmap:
+            if timestamp - self.logmap[message] < 10:
+                return False
         
+        self.logmap[message] = timestamp
+        return True
 
 
 # Your Logger object will be instantiated and called as such:
