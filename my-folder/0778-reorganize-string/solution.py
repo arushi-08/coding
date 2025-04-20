77680cdf0bc2c3132a64282cdd55be9a1f1f7b47
Arushi Sharma <arushi.net@outlook.com>
2025-04-20 23:05:51 +0000

    [LeetCode Sync] Runtime - 3 ms (48.40%), Memory - 17.9 MB (28.02%)

--- my-folder/0778-reorganize-string/solution.py
+++ my-folder/0778-reorganize-string/solution.py
@@ -1,47 +1,25 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        """
-        goal: put most frequent char first such that same chars are not adjacent
-
-        cannot do all k's then repeat vvvlo ['v', 'l', 'o', 'v'], expected vlvov
-        cannot do k every consecutive aabbcc, output: ababc_ cannot do it, expected abcabc
-        what did i do before?
-        put k every consecutive but don't restart the start pointer => abacbc
-        abc
         
-        """
-
-        s_list = list(s)
-        s_map = Counter(s_list)
-
-        heap = [(-v,k) for k, v in s_map.items()]
-        heapify(heap)
-
-        res = []
+        fmap = Counter(s)
+        fmap_items = [[-v,k] for k,v in fmap.items()]
+        heapify(fmap_items)
         prev = None
-        while heap:
-            
-            maxval, most_freq_char = heappop(heap)
-            res.append(most_freq_char)
-            maxval += 1
+        res = []
+
+        while fmap_items:
+            freq, top_chr = heappop(fmap_items)
+            freq += 1
+            res.append(top_chr)
 
             if prev:
-                heappush(heap, prev)
+                heappush(fmap_items, prev)
                 prev = None
             
-            if maxval < 0:
-                prev = (maxval, most_freq_char)
-
-        if len(res) != len(s):
-            return ""
-
-        return "".join(res)
-
-# aabbcc
-# res = []
-# a:2,b:2,c:2
-# maxval=-2,most_freq=a
-# 
-# 
-
-
+            if freq < 0:
+                prev = [freq, top_chr]
+        
+        if len(res) == len(s):
+            return ''.join(res)
+        return ''
+        
