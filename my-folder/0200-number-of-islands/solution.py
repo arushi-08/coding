dfd571c16dfdba7fe39801a47dca8b937016392f
Arushi Sharma <arushi.net@outlook.com>
2025-04-22 17:42:26 +0000

    [LeetCode Sync] Runtime - 267 ms (34.90%), Memory - 20 MB (82.74%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,38 +1,49 @@
-from collections import deque
 class Solution:
+    def __init__(self):
+        self.dir_rows = [1, 0, -1, 0]
+        self.dir_cols = [0, 1, 0, -1]
+        self.n_dirs = len(self.dir_rows)
+
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        # return numislands
-        numislands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=='1':
+        "count num of islands, bfs"
+        n_rows = len(grid)
+        n_cols = len(grid[0])
+
+        n_islands = 0
+        for i in range(n_rows):
+            for j in range(n_cols):
+                if grid[i][j] == '1':
                     grid[i][j] = '0'
-                    self.bfs(grid, i, j) 
-                    numislands += 1
-        
-        return numislands
-    
-    def bfs(self, grid, i, j):
+                    self.bfs(grid, n_rows, n_cols, i, j)
+                    n_islands += 1
+        return n_islands
+
+    def bfs(self, grid, n_rows, n_cols, i, j):
 
         queue = deque()
         queue.append((i,j))
 
-        rows = [1, 0, -1, 0]
-        cols = [0, -1, 0, 1]
-
         while queue:
             currx, curry = queue.popleft()
 
-            for i in range(len(rows)):
-                if self.isSafe(grid, currx+rows[i], curry+cols[i]):
-                    queue.append((currx+rows[i], curry+cols[i]))
-                    grid[currx+rows[i]][curry+cols[i]] = '0'
-    
-    def isSafe(self, grid, x, y):
-        return (
-            x >= 0 and x < len(grid) and
-            y >= 0 and y < len(grid[0]) and
-            grid[x][y] == '1'
+            for k in range(self.n_dirs):
+                
+                if self.is_safe(
+                    currx + self.dir_rows[k],
+                    curry + self.dir_cols[k],
+                    n_rows,
+                    n_cols,
+                    grid
+                ):
+                    grid[currx + self.dir_rows[k]][curry + self.dir_cols[k]] = '0'
+                    queue.append(
+                        (currx + self.dir_rows[k], curry + self.dir_cols[k])
+                    )
+
+    def is_safe(self, x, y, m, n, grid):
+
+        return ( 
+            0 <= x < m and 0 <= y < n and grid[x][y] == '1'
         )
 
