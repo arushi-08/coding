e8d48f97a9579a921535b15efa3ae2397328b854
Arushi Sharma <arushi.net@outlook.com>
2025-04-27 04:11:17 +0000

    [LeetCode Sync] Runtime - 43 ms (22.54%), Memory - 22.1 MB (11.65%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,18 +1,26 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        # num of subarr with sum k
-        hmap = {}
-        hmap[0] = 1
-        psum = 0
-        ans = 0
-        for num in nums:
-            psum += num
-            if psum - k in hmap:
-                ans += hmap[psum-k]
-            hmap[psum] = hmap.get(psum, 0) + 1
+        """
+        given subarr's find sum = k
+        key : curr_sum - earlier_sum == k
+        earlier_sum == curr_sum - k
+        psum[curr_sum - k] is the count of subarrs whose sum from 0 to subarr length is earlier_sum which means the count of subarrs sum is k
+        """
 
-        return ans
+        # psum[i] stores sum : count of nums[0..i] with that sum
+        # count is not psum[k]
+        # it can include between sum that doesn't include nums[0]
+        psum = defaultdict(int)
+        psum[0] = 1
+        curr_sum = 0
+        count = 0
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            count += psum[curr_sum - k]
+            psum[curr_sum] += 1
+
+        return count
 
 
 
