4db623f1137b0eccd1fbfd02d7cdc1888177d6b4
Arushi Sharma <arushi.net@outlook.com>
2025-04-27 02:45:45 +0000

    [LeetCode Sync] Runtime - 851 ms (34.15%), Memory - 18.1 MB (71.27%)

--- my-folder/0416-partition-equal-subset-sum/solution.py
+++ my-folder/0416-partition-equal-subset-sum/solution.py
@@ -1,28 +1,39 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-    
-        tsum = sum(nums)
-        if tsum % 2 == 1: return False
-        memo = {}
+        
+        """
+        dp
+        """
 
-        def dfs(subsetsum, csum, index):
-            if csum == subsetsum:
-                return True
+        total_sum = sum(nums)
 
-            if csum > subsetsum or index == len(nums):
-                return False
+        if total_sum & 1 : return False
+        
+        subsetsum = total_sum // 2
+        
+        curr = [False] * (subsetsum+1)
+        curr[0] = True
+
+        """
+        dp[i][s] -> can num[i..n-1] form s
+        if i do:
+            curr[j-nums[i]] i.e, can num[i..n-1] form j-nums[i]
+            if j < nums[i], we can't take j
+            so inner loop is from subsetsum to nums[i]-1
+                because inner loop is only when we have 2 choices
+
+            our choices are take or not take
+            take is curr[j-nums[i]]
+            not take is curr[j]
+        """
+
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(subsetsum, nums[i]-1,-1):
+                curr[j] = curr[j - nums[i]] | curr[j]
+        return curr[-1]
             
-            if (csum + nums[index], index) in memo:
-                return memo[(csum + nums[index], index)]
 
-            result =  dfs(
-                subsetsum, csum + nums[index], index+1
-                ) or dfs(
-                    subsetsum, csum, index+1
-                    )
-            memo[(csum + nums[index], index)] = result
 
-            return result
 
-        subsetsum = tsum // 2
-        return dfs(subsetsum, 0, 0)
+
+
