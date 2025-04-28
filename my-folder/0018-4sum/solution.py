28cce8eaa07800c84197321e7f3f5abe96b13690
Arushi Sharma <arushi.net@outlook.com>
2025-04-28 22:03:51 +0000

    [LeetCode Sync] Runtime - 447 ms (26.30%), Memory - 19.5 MB (7.53%)

--- my-folder/0018-4sum/solution.py
+++ my-folder/0018-4sum/solution.py
@@ -5,13 +5,14 @@ class Solution:
         n = len(nums)
         nums.sort()
         dup = set()
+        
         for i in range(n-3):
             for j in range(i+1, n-2):
-                if (nums[i],nums[j]) in dup: continue
-                dup.add((nums[i],nums[j]))
+                if (nums[i], nums[j]) in dup:
+                    continue
+                dup.add((nums[i], nums[j]))
                 k = j+1
                 l = n-1
-                # print('inside',i+1, n-2, dup, nums[j])
                 subtarget = target - (nums[i] + nums[j])
                 while k < l:
                     cursum = nums[k] + nums[l]
@@ -20,6 +21,9 @@ class Solution:
                         k += 1
                         while k < l and nums[k] == nums[k-1]:
                             k += 1
+                        l -= 1
+                        while l > k and nums[l] == nums[l+1]:
+                            l -= 1
                         
                     elif cursum < subtarget:
                         k += 1
@@ -30,3 +34,4 @@ class Solution:
                         while l > k and nums[l] == nums[l+1]:
                             l -= 1
         return ans
+
