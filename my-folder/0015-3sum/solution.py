bba6216137d31b1e097eff4de2b2440b3df0e971
Arushi Sharma <arushi.net@outlook.com>
2025-04-28 21:59:20 +0000

    [LeetCode Sync] Runtime - 2205 ms (5.01%), Memory - 20.5 MB (89.11%)

--- my-folder/0015-3sum/solution.py
+++ my-folder/0015-3sum/solution.py
@@ -1,26 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
+        # given int array nums
+        # return all triplets [nums[i], n] sum to 0
+
+        # -4,-1,-1,0,1,2
+        """
+        if num[i] < 0 -> nums[ed] > 0
+
+        """
         nums.sort()
+        result = set()
+        print('nums', nums)
+        for i in range(len(nums)-1):
 
-        ans = set()
+            st = i+1
+            ed = len(nums)-1
+            print(i)
 
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            
-            start = i+1
-            end = len(nums)-1
-
-            while start < end:
-                sumval = nums[i] + nums[start] + nums[end]
-                if sumval == 0:
-                    ans.add((nums[i],nums[start], nums[end]))
-                    start += 1
-                    end -= 1
-                elif sumval < 0:
-                    start += 1
+            while st < ed:
+                currsum = nums[i] + nums[st] + nums[ed]
+                if currsum == 0:
+                    result.add(tuple(sorted( [nums[i], nums[st], nums[ed]] )))
+                    st += 1
+                    ed -= 1
+                elif currsum > 0:
+                    ed -= 1
                 else:
-                    end -= 1
+                    st += 1
         
-        return list(ans)
+        return list(result)
+
