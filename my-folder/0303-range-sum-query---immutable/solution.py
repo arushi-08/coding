88d071c5552554aa334ac2ab557484ba312a38b1
Arushi Sharma <arushi.net@outlook.com>
2025-04-28 20:10:14 +0000

    [LeetCode Sync] Runtime - 4 ms (61.22%), Memory - 21.7 MB (61.86%)

--- my-folder/0303-range-sum-query---immutable/solution.py
+++ my-folder/0303-range-sum-query---immutable/solution.py
@@ -1,15 +1,20 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
+        # handle multiple queries
+        # calculate sum between left and right inclusive
         self.nums = nums
-        self.psum = [nums[0]]
-        for i in range(1, len(nums)):
-            self.psum.append(nums[i] + self.psum[i-1])
-        
+        self.psum = [0]
+        for i in range(len(nums)):
+            self.psum.append(nums[i] + self.psum[-1])
+
+        # 0,-2,-2,1
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.psum[right] - self.psum[left] + self.nums[left]
-
+        
+        # 0,2
+        # -2+0+3
+        return self.psum[right+1] - self.psum[left]
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
