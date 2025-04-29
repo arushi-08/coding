7eeebeca37a6377279df89c6b953d48dfff7558d
Arushi Sharma <arushi.net@outlook.com>
2025-04-29 03:14:34 +0000

    [LeetCode Sync] Runtime - 96 ms (94.21%), Memory - 78.7 MB (13.54%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,42 +1,59 @@
-import threading
+class Node:
+    def __init__(self, key, val=0, nextnode=None, prev=None):
+        self.key = key
+        self.val = val
+        self.next = nextnode
+        self.prev = prev
+
 class LRUCache:
-    """
-    how to do LRU?
-        track:
-            doubly LL -> push element to beg of LL
-                to pop element when capacity reached in O(1)
-            hashmap to get & put in cache in O(1)
-    """
 
     def __init__(self, capacity: int):
-        self.cachemap = OrderedDict()
+        self.cachemap = {}
+        self.cache_dll = Node(-1)
+        self.tail = Node(-1)
+        self.cache_dll.next = self.tail
+        self.tail.prev = self.cache_dll
         self.capacity = capacity
-        self.lock = threading.Lock()
 
     def get(self, key: int) -> int:
-        with self.lock:
-            if key in self.cachemap:
-                value = self.cachemap.pop(key)
-                self.cachemap[key] = value
-                return self.cachemap[key]
+        if key in self.cachemap:
+            self.move_to_top(self.cachemap[key])
+            return self.cachemap[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
+        if key in self.cachemap:
+            self.cachemap[key].val = value
+            self.move_to_top(self.cachemap[key])
+            return
         
-        with self.lock:
-            if key in self.cachemap:
-                self.cachemap.pop(key)
-            
-            elif len(self.cachemap) == self.capacity:
-                self.cachemap.popitem(last=False)
+        if self.capacity == len(self.cachemap):
+            key_removed = self.get_last_removed()
+            del self.cachemap[key_removed]
+        
+        self.cachemap[key] = Node(key, value)
+        self.add(self.cachemap[key])
+    
+    def add(self, node):
+        headnext = self.cache_dll.next
+        self.cache_dll.next = node
+        node.prev = self.cache_dll
+        node.next = headnext
+        headnext.prev = node
 
-            self.cachemap[key] = value
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
 
-    """
-    How would you make your cache safe for concurrent get / put calls?
-
-
-    """
+    def move_to_top(self, node):
+        self.remove(node)
+        self.add(node)
+    
+    def get_last_removed(self):
+        node_to_remove = self.tail.prev
+        self.remove(node_to_remove)
+        return node_to_remove.key
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
