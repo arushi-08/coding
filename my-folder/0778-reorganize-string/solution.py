50ce8b3d278d08553d1c7772a530a23078ae4ede
Arushi Sharma <arushi.net@outlook.com>
2025-04-29 02:43:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (74.43%)

--- my-folder/0778-reorganize-string/solution.py
+++ my-folder/0778-reorganize-string/solution.py
@@ -2,24 +2,29 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
         
         fmap = Counter(s)
-        fmap_items = [[-v,k] for k,v in fmap.items()]
+        fmap_items = [[-v, k] for k, v in fmap.items()]
         heapify(fmap_items)
+
         prev = None
-        res = []
+        result = []
 
         while fmap_items:
-            freq, top_chr = heappop(fmap_items)
-            freq += 1
-            res.append(top_chr)
+            
+            maxval, topchar = heappop(fmap_items)
+            if result and topchar == result[-1]:
+                return ""
+            result.append(topchar)
+
+            maxval += 1
 
             if prev:
                 heappush(fmap_items, prev)
                 prev = None
             
-            if freq < 0:
-                prev = [freq, top_chr]
+            if maxval < 0:
+                prev = [maxval, topchar]
         
-        if len(res) == len(s):
-            return ''.join(res)
+        if len(result) == len(s):
+            return ''.join(result)
         return ''
-        
+
