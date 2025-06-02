c5c66f067170451cdaf730afcedaad638f98b03d
Arushi Sharma <arushi.net@outlook.com>
2025-06-02 21:12:56 +0000

    [LeetCode Sync] Runtime - 27 ms (21.59%), Memory - 18.1 MB (12.09%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,25 +1,30 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
-        # given string s, find length of longest substring without duplicate chars
 
-        # s = 'abcabcbb'
-        # find length of longest substring without dups
+        # given s, find length of longest substring without dup chars
 
-        start = 0
-        end = 0
-        lastseen = {}
-        maxlength = 0
+        # without dups
+        # abcabcbb
+        # abc
 
-        for end in range(len(s)):
-
-            if s[end] in lastseen and lastseen[s[end]] >= start:
-                start = lastseen[s[end]] + 1
-            
-            lastseen[s[end]] = end
-            maxlength = max(maxlength, end - start + 1)
+        # if s[ed] == s[st]
+        # move st forward
         
-        
-        return maxlength
+        st = 0
+        ed = 0
+        maxlen = 0
+        fmap = {}
 
+        while ed < len(s):
+
+            while st < ed and s[ed] in fmap and fmap[s[ed]] > 0:
+                fmap[ s[st] ] -= 1
+                st += 1
+
+            maxlen = max(maxlen, ed - st + 1)
+            fmap[ s[ed] ] = fmap.get(s[ed], 0) + 1
+            ed += 1
+        
+        return maxlen
 
