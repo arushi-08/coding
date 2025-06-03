68c715c7b6f161ff29c2f6cf9a5c0445e0052b69
Arushi Sharma <arushi.net@outlook.com>
2025-06-03 20:46:32 +0000

    [LeetCode Sync] Runtime - 4 ms (14.91%), Memory - 17.8 MB (38.11%)

--- my-folder/0202-happy-number/solution.py
+++ my-folder/0202-happy-number/solution.py
@@ -1,15 +1,22 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+
+        def next_val(n):
+            nextn = 0
+
+            while n:
+                n, rem = divmod(n, 10)
+                nextn += rem ** 2
+
+            return nextn
+
         
-        memory = set()
-        while n != 1:
-            n_copy = n
-            digits_n = 0
-            sum_digits = [pow(int(i),2) for i in str(n_copy)]
-            sum_digits = sum(sum_digits)
-            if sum_digits in memory:
-                return False
-            memory.add(sum_digits)
-            n = sum_digits
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = next_val(n)
+            
         
-        return True
+        return n == 1
+
