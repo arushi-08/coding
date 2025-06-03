125ceaa99b78fef707b3d70f56762eb90edc2135
Arushi Sharma <arushi.net@outlook.com>
2025-06-03 23:12:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (29.93%)

--- my-folder/0066-plus-one/solution.py
+++ my-folder/0066-plus-one/solution.py
@@ -1,20 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        """loop from end of array
-           keep a variable : carry
-           add carry to each arr[i], if arr[i] > 9: carry = arr[i]%10
-           arr[i] = arr[i]//10
-        """
-        carry = 1
-        for i in range(len(digits)-1, -1, -1):
-            digits[i] += carry
-            if digits[i] > 9:
-                carry = digits[i] // 10
-                digits[i] %= 10
-            else:
-                carry = 0
         
-        if carry:
-            digits.insert(0, carry)
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
         
-        return digits
+        return [1] + digits
+
+            
