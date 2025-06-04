c66c3e7448ed1f6bea1a023af18763c22f663f38
Arushi Sharma <arushi.net@outlook.com>
2025-06-04 15:39:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (49.86%)

--- my-folder/0050-powx-n/solution.py
+++ my-folder/0050-powx-n/solution.py
@@ -1,13 +1,32 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        result = 1
-        if n < 0: 
+        res = 1
+        if n < 0:
             x = 1/x
             n = -n
-        while n > 0:
-            if n % 2==1:
-                result = result*x
+
+        while n:
+            if n & 1:
+                res *= x
+                n -= 1
             x = x*x
-            n//=2
-        return result
+            n >>= 1
+            
+        return res
+
+        """
+        x=1/2, n=2
+            res = 1/4
+            n // 2 -> n = 1
+        
+        x=2, n=10
+            res = 2*2 = 4
+            n // 2 -> n = 5
+            res = 4*2*2*2 = 32
+            n // 2 -> n = 2
+            res = 32*4 = 128
+            n // 2 -> n = 1
+            res = 128*4*2 = 1024
+        
+        """
