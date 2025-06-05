a9fd9825784b0fb95da0a9c925835addd07aed08
Arushi Sharma <arushi.net@outlook.com>
2025-06-05 19:04:33 +0000

    [LeetCode Sync] Runtime - 19 ms (99.93%), Memory - 19.5 MB (44.14%)

--- my-folder/2139-detect-squares/solution.py
+++ my-folder/2139-detect-squares/solution.py
@@ -1,22 +1,38 @@
 class DetectSquares:
+        # count num of squares after adding points
+        # know if any point is in same x (x,0), same dist y (0,x)
+        # and same dist from (x,0) and (0,x) to (-x,-x) or positive coords
 
     def __init__(self):
-        self.points = []
-        self.point_counts = {}
+        # adds new points from stream into a data structure
+        self.pointscount = defaultdict(lambda : defaultdict(int))
 
     def add(self, point: List[int]) -> None:
-        self.points.append(point)
-        self.point_counts[tuple(point)] = self.point_counts.get(tuple(point), 0) + 1
-        
+        x,y = point
+        self.pointscount[x][y] += 1
+
 
     def count(self, point: List[int]) -> int:
-        px,py = point
-        res = 0
-        for x,y in self.points:
-            if abs(px - x) != abs(py - y) or px == x or py == y:
+        x,y = point
+        count = 0
+        for cj, count_cj  in self.pointscount[x].items():
+            d = abs(y-cj)
+            if d == 0:
                 continue
-            res += self.point_counts.get((px,y), 0) * self.point_counts.get((x,py), 0)
-        return res
+            count_a = self.pointscount[x+d][y]
+            count_b = self.pointscount[x+d][cj]
+            count += count_cj * count_a * count_b
+            
+            count_a = self.pointscount[x-d][y]
+            count_b = self.pointscount[x-d][cj]
+            count += count_cj * count_a * count_b
+
+        return count    
+
+
+"""
+
+"""
 
 
 # Your DetectSquares object will be instantiated and called as such:
