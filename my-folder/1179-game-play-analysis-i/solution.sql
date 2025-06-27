e6b73d21f618c89ccb0a55ed5a560f65de4696ad
Arushi Sharma <arushi.net@outlook.com>
2025-06-27 17:09:38 +0000

    [LeetCode Sync] Runtime - 621 ms (20.45%), Memory - 0.0B (100.00%)

--- my-folder/1179-game-play-analysis-i/solution.sql
+++ my-folder/1179-game-play-analysis-i/solution.sql
@@ -1,6 +1,10 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+
+
+-- find first login date for each player
 
 select player_id, min(event_date) as first_login
 from activity
-group by player_id 
+group by player_id
+
 
