221b4449ed9a0111b67411fe0e0b95ee987c6835
Arushi Sharma <arushi.net@outlook.com>
2025-06-27 18:20:00 +0000

    [LeetCode Sync] Runtime - 1070 ms (21.22%), Memory - 0.0B (100.00%)

--- my-folder/1155-product-sales-analysis-iii/solution.sql
+++ my-folder/1155-product-sales-analysis-iii/solution.sql
@@ -1,14 +1,9 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
+
+-- find sales first year each product was sold
 
 
--- select product id, year, quantity, and price for 1st year of every product sold
-
-with t as (
-    select product_id, min(year) as year
-    from sales
-    group by product_id
-)
-select product_id, year as first_year, quantity, price
-from sales
-where (year, product_id) in (select year, product_id from t)
-
+select s.product_id, year as first_year, quantity, price
+from sales as s inner join (select product_id, min(year) as min_yr
+                            from sales group by product_id) as t
+on s.product_id = t.product_id and year = min_yr
