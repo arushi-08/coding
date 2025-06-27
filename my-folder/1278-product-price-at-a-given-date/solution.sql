3262ed953a2bf28738f2d5463e1e617346b3acb6
Arushi Sharma <arushi.net@outlook.com>
2025-06-27 03:19:52 +0000

    [LeetCode Sync] Runtime - 859 ms (12.02%), Memory - 0.0B (100.00%)

--- my-folder/1278-product-price-at-a-given-date/solution.sql
+++ my-folder/1278-product-price-at-a-given-date/solution.sql
@@ -1,25 +1,18 @@
 # Write your MySQL query statement below
 
--- find price of products on 2019-08-16
--- original 10 
 
-
-with p1 as (
-    select product_id, new_price as price
-    from (
-        select product_id, new_price, row_number() over( partition by product_id order by
-        change_date desc) as rn
-        from (
-            select product_id, new_price, change_date
-            from products
-            where change_date <= date('2019-08-16')
-        ) as t ) as p
-    where rn = 1
-)
-
-select * from p1
-union
-select product_id, 10 as price
-from products
-where product_id not in (select product_id from p1)
-
+select product_id,
+coalesce(
+    (select new_price 
+    from products as ph
+    where ph.product_id = p.product_id
+    and ph.change_date <= '2019-08-16'
+    order by ph.change_date desc
+    limit 1),
+    10
+) as price
+from
+(
+    select distinct product_id
+    from products
+) as p
