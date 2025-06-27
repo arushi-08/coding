c3cd31a221137c53f71308973bc4ad84cdf0c3f8
Arushi Sharma <arushi.net@outlook.com>
2025-06-27 23:50:35 +0000

    [LeetCode Sync] Runtime - 216 ms (51.93%), Memory - 0.0B (100.00%)

--- my-folder/1390-average-selling-price/solution.sql
+++ my-folder/1390-average-selling-price/solution.sql
@@ -1,12 +1,12 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-select product_id, ifnull(round(sum(tprice)/sum(units), 2), 0) as average_price
-from (
-select p.product_id, ifnull(price * units, 0) as tprice, ifnull(units, 0) as units
-from prices as p left join unitssold as u
-on p.product_id = u.product_id
-where purchase_date >= start_date and purchase_date <= end_date 
-or purchase_date is null
-) as t
-group by product_id
+-- find avg = price / units
 
+
+-- find units in start, end time ( units * price + units * price) / total units
+
+select p.product_id, coalesce(round(sum(price * units)::numeric / sum(units), 2), 0) as average_price
+from prices p left join unitssold u
+on p.product_id = u.product_id and 
+p.start_date <= u.purchase_date and  u.purchase_date <= p.end_date
+group by p.product_id
