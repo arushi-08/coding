b13e448514e84c9b8d4e3c45636dde513332f24f
Arushi Sharma <arushi.net@outlook.com>
2025-06-28 23:51:16 +0000

    [LeetCode Sync] Runtime - 330 ms (99.77%), Memory - 0.0B (100.00%)

--- my-folder/1278-product-price-at-a-given-date/solution.sql
+++ my-folder/1278-product-price-at-a-given-date/solution.sql
@@ -1,18 +1,12 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
 
-select product_id,
-coalesce(
-    (select new_price 
-    from products as ph
-    where ph.product_id = p.product_id
-    and ph.change_date <= '2019-08-16'
-    order by ph.change_date desc
-    limit 1),
-    10
-) as price
-from
-(
-    select distinct product_id
-    from products
-) as p
+select p.product_id, coalesce(new_price, 10) as price
+from (select distinct on(product_id) product_id, new_price
+from products
+where change_date <= to_date('2019-08-16', 'YYYY-mm-dd')
+order by 1, change_date desc) as t
+full outer join (select distinct product_id from products) as p
+on t.product_id = p.product_id
+
+
