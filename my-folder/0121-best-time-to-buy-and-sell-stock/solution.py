18c5bcb5c4c89989e45e5256603bf5fd196b7a0c
Arushi Sharma <arushi.net@outlook.com>
2025-07-08 22:38:42 +0000

    [LeetCode Sync] Runtime - 128 ms (26.05%), Memory - 26.9 MB (32.22%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,11 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        max_profit = 0
-        min_buy = float('inf')
+
+
+        # array of prices given
+        # max profit by choosing a single day to buy 1 stock and choose next day in future to sell
+        if not prices: return 0 
+
+        profit = 0
+        bought = prices[0]
         for price in prices:
-            min_buy = min(min_buy, price)
-            max_profit = max(max_profit, price - min_buy)
-        
-        return max_profit
+            profit = max(profit, price - bought)
+            bought = min(bought, price)
+
+        return profit
 
