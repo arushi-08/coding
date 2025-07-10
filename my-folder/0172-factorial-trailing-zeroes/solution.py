2db846dfd19daf3edf8275bd8e397c946d88bc63
Arushi Sharma <arushi.net@outlook.com>
2025-07-10 19:19:33 +0000

    [LeetCode Sync] Runtime - 436 ms (18.16%), Memory - 19.4 MB (6.70%)

--- my-folder/0172-factorial-trailing-zeroes/solution.py
+++ my-folder/0172-factorial-trailing-zeroes/solution.py
@@ -1,9 +1,35 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         
-        if not n:
-            return 0
-        # Since there are a lot more 2s than 5s in any n!, we just need to find the
-        # number of 5s in n! and that will be the answer
-        return n//5 + self.trailingZeroes(n//5)
-    
+        # 5 - 5*4*3*2*1
+        # 10
+        # 
+        count_two = 0
+        count_five = 0
+
+        def helper(n):
+            nonlocal count_two, count_five
+
+            if n in (0,1):
+                return 0
+
+            if n % 5 == 0:
+                n_copy = n
+                while n_copy % 5 == 0:
+                    n_copy //= 5
+                    count_five += 1
+            
+            elif n & 1 == 0:
+                n_copy = n
+                while n_copy & 1 == 0:
+                    count_two += 1
+                    n_copy >>= 1
+            
+            # print(n, count_two, count_five)
+            return helper(n-1)
+        
+        helper(n)
+
+        return min(count_five, count_two)
+
+
