cecca7dec4357ced560db4064a8e57a7303d4a84
Arushi Sharma <arushi.net@outlook.com>
2025-07-13 04:27:44 +0000

    [LeetCode Sync] Runtime - 207 ms (96.99%), Memory - 0.0B (100.00%)

--- my-folder/0197-rising-temperature/solution.sql
+++ my-folder/0197-rising-temperature/solution.sql
@@ -1,6 +1,16 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
+-- find all dates id with > temps comp to prev dates
 
-select w1.Id
-from weather w1, weather w2
-where w1.temperature > w2.temperature and dateDiff(w1.recordDate, w2.recordDate) = 1
+WITH data_w_prev_date as (
+    SELECT
+        *,
+        LAG(temperature) OVER(ORDER BY recordDate) as prev_temp,
+        LAG(recordDate) OVER(ORDER BY recordDate) as prev_date
+    FROM weather
+)
+
+SELECT id
+FROM data_w_prev_date
+WHERE temperature > prev_temp
+AND recordDate - 1 = prev_date
