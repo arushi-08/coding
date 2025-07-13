e0057002d557a699142aff1d07ab10e5737da269
Arushi Sharma <arushi.net@outlook.com>
2025-07-13 05:14:04 +0000

    [LeetCode Sync] Runtime - 582 ms (24.85%), Memory - 0.0B (100.00%)

--- my-folder/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ my-folder/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,8 +1,20 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
+-- find ids of users who visited without any trans
+-- number of times they visited
 
-select customer_id, count(customer_id) as count_no_trans
-from visits as v left join transactions as t
-on v.visit_id = t.visit_id
-where t.transaction_id is null
-group by customer_id
+WITH customer_counts AS (
+    SELECT
+        customer_id
+    FROM visits v
+    LEFT JOIN transactions t
+        ON v.visit_id = t.visit_id
+    GROUP BY v.visit_id, v.customer_id
+    HAVING sum(amount) IS NULL
+)
+
+SELECT
+    customer_id,
+    COUNT(*) as count_no_trans
+FROM customer_counts
+GROUP BY customer_id
