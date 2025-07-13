5685959f10df119b180dcdd5598ad4c150adb1be
Arushi Sharma <arushi.net@outlook.com>
2025-07-13 14:23:59 +0000

    [LeetCode Sync] Runtime - 285 ms (41.09%), Memory - 0.0B (100.00%)

--- my-folder/0180-consecutive-numbers/solution.sql
+++ my-folder/0180-consecutive-numbers/solution.sql
@@ -1,21 +1,13 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
+-- 
 
--- find all nos appear atleast 3 times consecutively
-
--- select distinct num as ConsecutiveNums
--- from (
---     select row_number() over(partition by num) as rn, rank() over(partition by num) as rnk, num
---     from logs
--- ) as t
--- where rn >= 3 and rnk = 1
-
-
--- select row_number() over(partition by num) as rn, rank() over(partition by num order by id) as rnk, num
---     from logs
-
-
-select distinct l1.num as ConsecutiveNums
-from logs l1, logs l2, logs l3
-where l1.id = l2.id + 1 and l1.id = l3.id + 2
-and l1.num = l2.num and l1.num = l3.num
+SELECT DISTINCT
+    l1.num as ConsecutiveNums
+FROM logs l1
+JOIN logs l2
+    ON l1.id + 1 = l2.id
+    AND l1.num = l2.num
+JOIN logs l3
+    ON l1.id + 2 = l3.id
+    AND l2.num = l3.num
