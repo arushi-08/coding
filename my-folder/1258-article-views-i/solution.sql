ea4b2acff3fceb89cb0f59aa593b17dbcb8bf6e7
Arushi Sharma <arushi.net@outlook.com>
2025-07-13 04:33:07 +0000

    [LeetCode Sync] Runtime - 360 ms (62.17%), Memory - 0.0B (100.00%)

--- my-folder/1258-article-views-i/solution.sql
+++ my-folder/1258-article-views-i/solution.sql
@@ -1,16 +1,10 @@
 -- Write your PostgreSQL query statement below
 
--- author = viewer
 
-select distinct v.author_id as id
-from views v inner join views v2
-on v.author_id = v2.viewer_id and v.article_id = v2.article_id
+-- find all aithors that viewed >= 1 own articles
 
--- select *
--- from views 
--- where viewer_id = 1
-
--- select *
--- from views v inner join views v2
--- on v.author_id = v2.viewer_id
--- where v.author_id = 1
+SELECT DISTINCT
+    author_id AS id
+FROM views 
+WHERE author_id = viewer_id
+ORDER BY author_id
