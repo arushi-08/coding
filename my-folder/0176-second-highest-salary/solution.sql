3ea72f788377566507757007c09e58148de0a834
Arushi Sharma <arushi.net@outlook.com>
2025-07-13 04:12:56 +0000

    [LeetCode Sync] Runtime - 267 ms (12.50%), Memory - 0.0B (100.00%)

--- my-folder/0176-second-highest-salary/solution.sql
+++ my-folder/0176-second-highest-salary/solution.sql
@@ -3,8 +3,8 @@
 -- 2nd highest distinct
 
 select (
-select distinct salary 
-from employee
-order by salary desc
-limit 1 offset 1
-) as SecondHighestSalary
+    select distinct salary
+    from employee
+    order by salary desc
+    limit 1 offset 1
+)  as SecondHighestSalary
