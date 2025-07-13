df9092fe82f3f249e850a3553bd9242a540dc216
Arushi Sharma <arushi.net@outlook.com>
2025-07-13 05:26:44 +0000

    [LeetCode Sync] Runtime - 203 ms (73.38%), Memory - 0.0B (100.00%)

--- my-folder/1390-average-selling-price/solution.sql
+++ my-folder/1390-average-selling-price/solution.sql
@@ -1,12 +1,21 @@
 -- Write your PostgreSQL query statement below
 
--- find avg = price / units
 
+-- avg selling price round(2)
+-- no sold -> coalesce(0)
 
--- find units in start, end time ( units * price + units * price) / total units
-
-select p.product_id, coalesce(round(sum(price * units)::numeric / sum(units), 2), 0) as average_price
-from prices p left join unitssold u
-on p.product_id = u.product_id and 
-p.start_date <= u.purchase_date and  u.purchase_date <= p.end_date
-group by p.product_id
+WITH prices_days AS (
+    SELECT
+    pd.product_id,
+    units * price as price,
+    units
+    FROM prices pd
+    LEFT JOIN unitssold u
+        ON pd.product_id = u.product_id
+        AND pd.start_date <= u.purchase_date AND u.purchase_date <= pd.end_date
+)
+SELECT
+    product_id,
+    coalesce(round(SUM(price)::numeric/SUM(units), 2), 0) as average_price
+FROM prices_days
+GROUP BY product_id
