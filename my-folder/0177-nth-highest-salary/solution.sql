b1f88c6b36bd9e77c702c728ba9c9c5c2106de32
Arushi Sharma <arushi.net@outlook.com>
2025-07-14 04:47:31 +0000

    [LeetCode Sync] Runtime - 463 ms (34.31%), Memory - 0.0B (100.00%)

--- my-folder/0177-nth-highest-salary/solution.sql
+++ my-folder/0177-nth-highest-salary/solution.sql
@@ -1,13 +1,13 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SET N = N-1;
-  RETURN (
-      # Write your MySQL query statement below.
-      select distinct salary
-        from employee
-        order by salary desc
-        limit 1 offset N
-  );
+RETURN (
+-- #Write your MySQL query statement below.
+    
+    SELECT DISTINCT salary
+    FROM   (SELECT *,
+        DENSE_RANK() OVER (ORDER BY salary desc) as rnk
+    FROM employee) t
+    WHERE rnk = n
+
+);
 END
-
-
