01f3a862b3dfd5c28938859668e8e45bba09c941
Arushi Sharma <arushi.net@outlook.com>
2025-07-14 21:08:35 +0000

    [LeetCode Sync] Runtime - 356 ms (72.19%), Memory - 0.0B (100.00%)

--- my-folder/1278-product-price-at-a-given-date/solution.sql
+++ my-folder/1278-product-price-at-a-given-date/solution.sql
@@ -1,12 +1,25 @@
--- Write your PostgreSQL query statement below
 
 
-select p.product_id, coalesce(new_price, 10) as price
-from (select distinct on(product_id) product_id, new_price
-from products
-where change_date <= to_date('2019-08-16', 'YYYY-mm-dd')
-order by 1, change_date desc) as t
-full outer join (select distinct product_id from products) as p
-on t.product_id = p.product_id
 
+WITH products_filtered AS (
+    SELECT *
+    FROM products
+    WHERE change_date <= '2019-08-16'
+),
 
+products_filtered_idx AS (
+    SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY change_date DESC ) AS rn
+    FROM products_filtered
+)
+
+(SELECT
+    product_id,
+    new_price as price
+FROM products_filtered_idx
+WHERE rn = 1)
+UNION
+(SELECT product_id, 10 as price
+FROM products 
+WHERE product_id not in (SELECT product_id from products_filtered))
