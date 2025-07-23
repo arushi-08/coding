7849240d1a2fbfceca0968c46e45d16809cc9db0
Arushi Sharma <arushi.net@outlook.com>
2025-07-23 06:13:43 +0000

    [LeetCode Sync] Runtime - 384 ms (95.40%), Memory - 0.0B (100.00%)

---
+++ my-folder/1811-fix-names-in-a-table/solution.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+
+SELECT
+    user_id,
+    CONCAT(UPPER(SUBSTRING("name", 1, 1)),  LOWER(SUBSTRING("name", 2, LENGTH("name"))) ) AS "name"
+FROM users
+ORDER BY user_id
