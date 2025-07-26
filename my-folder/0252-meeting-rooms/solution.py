c9f9cbc7b02cb478c79447535e1f214ec69ac4df
Arushi Sharma <arushi.net@outlook.com>
2025-07-26 19:59:04 +0000

    [LeetCode Sync] Runtime - 4 ms (31.28%), Memory - 19.6 MB (83.23%)

--- my-folder/0252-meeting-rooms/solution.py
+++ my-folder/0252-meeting-rooms/solution.py
@@ -1,8 +1,16 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         
-        intervals.sort(key= lambda x: x[0])
-        for i in range(len(intervals)-1):
-            if intervals[i][1] > intervals[i+1][0]:
+
+        # intervals = [start, end]
+
+        # if person can attend all meetings
+
+        intervals.sort()
+        current_end_time = 0
+        for start, end in intervals:
+            if current_end_time > start:
                 return False
+            current_end_time = end
+        
         return True
