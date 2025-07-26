44979aa93e9e688af46b3c77fb1b61ba5769c316
Arushi Sharma <arushi.net@outlook.com>
2025-07-26 20:03:47 +0000

    [LeetCode Sync] Runtime - 7 ms (31.81%), Memory - 19.9 MB (48.72%)

--- my-folder/0253-meeting-rooms-ii/solution.py
+++ my-folder/0253-meeting-rooms-ii/solution.py
@@ -1,41 +1,21 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        
-        # given array of meeting time intervals
-        # where intervals[i] = [starti, endi]
-        # return min num of conference rooms required
 
-        # [[0,30],[5,10],[15,20]]
-        # return minimum number of conferences
-
-        # sort - start time
-        # conf can be shared - if non overlapping
-        # max num of overlapping intervals at a time = num of conf rooms
-
-        # iterate on intervals
-        # if next interval start time > curr end time
-        # curr meeting over -> same conf room for next
-        # else: overlapping
-        if not intervals: return 0
+        # min conf rooms needed
 
         intervals.sort()
+        # active smallest end time meeting
+        # the max size of heap - is answer
 
-        min_end_time_heap = []
-
-        for interval in intervals:
-            if min_end_time_heap and interval[0] >= min_end_time_heap[0]:
-                
-                heappop(min_end_time_heap)
+        rooms = []
+        max_rooms = 0
+        for start, end in intervals:
+            if rooms and rooms[0] <= start:
+                # rooms stores end
+                heappop(rooms)
             
-            heappush(min_end_time_heap, interval[1])
-
-        return len(min_end_time_heap)
-
-        # [[7,50],[10,20],[11,40],[21,40],[42,55]]
-        # min_end_time_heap = 50
-
-        # min_end_time_heap = [20,50]
-        # 
-        # min_end_time_heap = [20,40,50]
-        # n_conf_rooms = 1
+            heappush(rooms, end)
+            max_rooms = max(len(rooms), max_rooms)
+            
+        return max_rooms
